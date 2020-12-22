5d408a030b43b58c948d3c3764bbaa252d379057
Manish <manish.thakur@agoda.com>
2020-12-22 11:09:01 +0700

    Finish revrse array logic

--- src/main/java/RotateArray.java
+++ src/main/java/RotateArray.java
@@ -1,8 +1,12 @@
 public class RotateArray {
-	public static int[] rotate(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+	public static int[] reverse(int[] arr, int i, int j) {
+		while(i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
 		return arr;
 	}
 }
--- src/test/java/RotateArrayTest.java
+++ src/test/java/RotateArrayTest.java
@@ -3,16 +3,31 @@ import org.junit.Test;
 
 public class RotateArrayTest {
     @Test
-    public void RotateEmptyArray() {
+    public void ReverseEmptyArray() {
         int[] arr = new int[]{1};
-        RotateArray.rotate(arr, 0, 0);
+        RotateArray.reverse(arr, 0, 0);
         Assert.assertArrayEquals("empty array comparison failed", arr, arr);
     }
 
     @Test
-    public void RotateTwoElemsArray() {
+    public void ReverseTwoElemsArray() {
         int[] arr = new int[]{1, 2};
-        RotateArray.rotate(arr, 0, 1);
+        RotateArray.reverse(arr, 0, 1);
         Assert.assertArrayEquals("two elem array comparison failed", new int[]{2, 1}, arr);
     }
+ 
+    @Test
+    public void ReverseThreeElemsArray() {
+        int[] arr = new int[]{1, 2, 3};
+        RotateArray.reverse(arr, 0, 2);
+        Assert.assertArrayEquals("three elem array comparison failed", new int[]{3, 2, 1}, arr);
+    }
+
+    @Test
+    public void ReverseFourElemsArray() {
+        int[] arr = new int[]{1, 2, 3, 4};
+        RotateArray.reverse(arr, 0, 3);
+        Assert.assertArrayEquals("three elem array comparison failed", new int[]{4, 3, 2, 1}, arr);
+    }
+    
 }
