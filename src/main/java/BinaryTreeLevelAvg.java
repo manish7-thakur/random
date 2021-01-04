86d71e0b704ba047af9580c8c204a10019fb8882
Manish <manish.thakur@agoda.com>
2021-01-04 23:36:56 +0700

    use linked list as queue

--- src/main/java/BinaryTreeLevelAvg.java
+++ src/main/java/BinaryTreeLevelAvg.java
@@ -1,39 +1,31 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 class BinaryTreeLevelAvg {
     public static List<Integer> calcAvgs(TreeNode node) {
         List<Integer> list = new ArrayList<>();
-        Queue<List<TreeNode>> arrayQueue = new ArrayDeque<>();
-        List<TreeNode> nodeList = new ArrayList<>();
-        nodeList.add(node);
-        arrayQueue.add(nodeList);
-        calc(arrayQueue, list);
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(node);
+        calc(queue, list);
         return list;
     }
 
-    private static void calc(Queue<List<TreeNode>> queue, List<Integer> list) {
+    private static void calc(LinkedList<TreeNode> queue, List<Integer> list) {
         int sum = 0;
-        List<TreeNode> nodeList;
+        int count = 0;
         while (!queue.isEmpty()) {
             sum = 0;
-            nodeList = queue.remove();
-            List<TreeNode> newLevelNodeList = new ArrayList<>();
-            for (TreeNode node : nodeList) {
+            count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
                 sum += node.value;
                 if (node.left != null) {
-                    newLevelNodeList.add(node.left);
+                    queue.add(node.left);
                 }
                 if (node.right != null) {
-                    newLevelNodeList.add(node.right);
+                    queue.add(node.right);
                 }
             }
-            list.add(sum / nodeList.size());
-            if (!newLevelNodeList.isEmpty()) {
-                queue.add(newLevelNodeList);
-            }
+            list.add(sum / count);
         }
     }
 }
