2e99113d12e34985cb3af2fd096d86fec9b88cf0
Manish <manish.thakur@agoda.com>
2021-01-06 21:29:12 +0700

    initialize hashmap size

--- src/main/java/LongestSubstring.java
+++ src/main/java/LongestSubstring.java
@@ -3,15 +3,17 @@ import java.util.Map;
 
 public class LongestSubstring {
     public static String nonRepeatingSubstring(String str) {
-        Map<Character, Integer> idxMap = new HashMap<>(str.length());
+        int strLen = str.length();
+        Map<Character, Integer> idxMap = new HashMap<>(strLen);
         String res = "";
         int l = 0;
-        for (int r = 0; r < str.length(); r++) {
+        for (int r = 0; r < strLen; r++) {
             char c = str.charAt(r);
             int idx = idxMap.getOrDefault(c, -1);
             if (idx == -1) {
-                if (str.substring(l, r + 1).length() > res.length()) {
-                    res = str.substring(l, r + 1);
+                int currStr = str.substring(l, r + 1);
+                if (currStr.length() > res.length()) {
+                    res = currStr;
                 }
             } else {
                 while (l < idx + 1) {
