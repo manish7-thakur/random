93a9e33b2f57831904dca845241d50e6c57196a8
Manish <manish.thakur@agoda.com>
2021-01-06 15:18:47 +0700

    longest substring with non repeating cahrs

--- src/test/java/LongestSubstringTest.java
+++ src/test/java/LongestSubstringTest.java
@@ -8,4 +8,10 @@ public class LongestSubstringTest {
 		String expected = "abc";
 		Assert.assertEquals(expected, actual);
 	}
+	@Test
+	public void Substring5CharsRepeating() {
+		String actual = LongestSubstring.nonRepeatingSubstring("abca");
+		String expected = "abc";
+		Assert.assertEquals(expected, actual);
+	}
 }
