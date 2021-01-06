407e6d7920d4b2053222216bc7e1b59454843ac0
Manish <manish.thakur@agoda.com>
2021-01-06 16:03:16 +0700

    5 chars string with repeating chars

--- src/main/java/LongestSubstring.java
+++ src/main/java/LongestSubstring.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstring {
-	public static String nonRepeatingSubstring(String str) {
-		return str;
-	}
+    public static String nonRepeatingSubstring(String str) {
+        Map<Character, Integer> occMap = new HashMap<>();
+        String res = "";
+        int l = 0;
+        for (int r = 0; r < str.length(); r++) {
+            char c = str.charAt(r);
+            int occ = occMap.getOrDefault(c, 0);
+            if (occMap.containsKey(c)) {
+                if (str.substring(l, r).length() > res.length()) {
+                    res = str.substring(l, r);
+                }
+                l++;
+            } else {
+                if (str.substring(l, r).length() > res.length()) {
+                    res = str.substring(l, r + 2);
+                }
+                occMap.put(c, ++occ);
+            }
+        }
+        return res;
+    }
 }
--- src/test/java/LongestSubstringTest.java
+++ src/test/java/LongestSubstringTest.java
@@ -1,17 +1,18 @@
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class LongestSubstringTest {
-	@Test
-	public void Substring3CharsNonRepeating() {
-		String actual = LongestSubstring.nonRepeatingSubstring("abc");
-		String expected = "abc";
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void Substring5CharsRepeating() {
-		String actual = LongestSubstring.nonRepeatingSubstring("abca");
-		String expected = "abc";
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void Substring3CharsNonRepeating() {
+        String actual = LongestSubstring.nonRepeatingSubstring("abc");
+        String expected = "abc";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void Substring5CharsRepeating() {
+        String actual = LongestSubstring.nonRepeatingSubstring("abca");
+        String expected = "abc";
+        Assert.assertEquals(expected, actual);
+    }
 }
