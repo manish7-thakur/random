2b5a77db758b7a429f4ee7bd9dda095c862e994c
Manish <manish.thakur@agoda.com>
2021-01-07 21:41:04 +0700

    longest palindrome string with all repeating chars

--- src/main/java/LongestPalindrome.java
+++ src/main/java/LongestPalindrome.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
 public class LongestPalindrome {
-	
-	public static String longestPlaindrome(String s) {
-		Map<Character, ArrayList<Integer>> idxMap = new HashMap<>(); 
-		String res = "" + s.charAt(0);
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			ArrayList<Integer> idxList = idxMap.getOrDefault(c, new ArrayList<Integer>());
-			idxList.add(i);
-			idxMap.put(c, idxList);
-		}
-		for(Map.Entry<Character, ArrayList<Integer>> entry : idxMap.entrySet()) {
-			ArrayList<Integer> idxList = entry.getValue();
-			int idxListSize = idxList.size();
-			if(idxListSize > 1) {
-				for(int i = 0; i < idxListSize; i++) {
-					for(int j = i+1; j < idxListSize; j++) {
-						int l = idxList.get(i);
-						int r = idxList.get(j);
- 						if(isPalindrome(s, l, r) && r - l + 1 > res.length()) {
-							res = s.substring(l, r +1);
-						}
-					}
-				}
-			}
-		}
-		return res;
-	}
 
-	public static boolean isPalindrome(String s, int i, int j) {
-		while(i < j) {
-			if(s.charAt(i) != s.charAt(j)) {
-				return false;
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
+    public static String longestPlaindrome(String s) {
+        Map<Character, ArrayList<Integer>> idxMap = new HashMap<>();
+        String res = "" + s.charAt(0);
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ArrayList<Integer> idxList = idxMap.getOrDefault(c, new ArrayList<>());
+            idxList.add(i);
+            idxMap.put(c, idxList);
+        }
+        for (Map.Entry<Character, ArrayList<Integer>> entry : idxMap.entrySet()) {
+            ArrayList<Integer> idxList = entry.getValue();
+            int idxListSize = idxList.size();
+            if (idxListSize > 1) {
+                for (int i = 0; i < idxListSize; i++) {
+                    for (int j = i + 1; j < idxListSize; j++) {
+                        int l = idxList.get(i);
+                        int r = idxList.get(j);
+                        if (isPalindrome(s, l, r) && r - l + 1 > res.length()) {
+                            res = s.substring(l, r + 1);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
--- src/test/java/LongestPalindromeTest.java
+++ src/test/java/LongestPalindromeTest.java
@@ -2,63 +2,63 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LongestPalindromeTest {
-	@Test
-	public void isPalindromeNonRepeatingChars() {
-	boolean actual = LongestPalindrome.isPalindrome("abcd", 0, 3);
-	Assert.assertFalse(actual);
-	}
+    @Test
+    public void isPalindromeNonRepeatingChars() {
+        boolean actual = LongestPalindrome.isPalindrome("abcd", 0, 3);
+        Assert.assertFalse(actual);
+    }
 
-	@Test
-	public void isPalindromeRepeatingCharEvenPalindromeLen2() {
-	boolean actual = LongestPalindrome.isPalindrome("bb", 0, 1);
-	Assert.assertTrue(actual);
-	}
+    @Test
+    public void isPalindromeRepeatingCharEvenPalindromeLen2() {
+        boolean actual = LongestPalindrome.isPalindrome("bb", 0, 1);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void isPalindromeRepeatingCharEvenPalindrome() {
-	boolean actual = LongestPalindrome.isPalindrome("abba", 0, 3);
-	Assert.assertTrue(actual);
-	}
+    @Test
+    public void isPalindromeRepeatingCharEvenPalindrome() {
+        boolean actual = LongestPalindrome.isPalindrome("abba", 0, 3);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void isPalindromeRepeatingCharOddPalindrome() {
-	boolean actual = LongestPalindrome.isPalindrome("aba", 0, 2);
-	Assert.assertTrue(actual);
-	}
+    @Test
+    public void isPalindromeRepeatingCharOddPalindrome() {
+        boolean actual = LongestPalindrome.isPalindrome("aba", 0, 2);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void longestPalindromeNonRepeatingChars() {
-	String actual = LongestPalindrome.longestPlaindrome("abcd");
-	String expected = "a";
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void longestPalindromeNonRepeatingChars() {
+        String actual = LongestPalindrome.longestPlaindrome("abcd");
+        String expected = "a";
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void longestPalindromeRepeatingCharsLen4() {
-	String actual = LongestPalindrome.longestPlaindrome("abbd");
-	String expected = "bb";
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void longestPalindromeRepeatingCharsLen4() {
+        String actual = LongestPalindrome.longestPlaindrome("abbd");
+        String expected = "bb";
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void longestPalindromeRepeatingCharsLen5() {
-	String actual = LongestPalindrome.longestPlaindrome("abcbd");
-	String expected = "bcb";
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void longestPalindromeRepeatingCharsLen5() {
+        String actual = LongestPalindrome.longestPlaindrome("abcbd");
+        String expected = "bcb";
+        Assert.assertEquals(expected, actual);
+    }
 
 
-	@Test
-	public void longestPalindromeRepeatingCharsLenEntireString() {
-	String actual = LongestPalindrome.longestPlaindrome("abcba");
-	String expected = "abcba";
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void longestPalindromeRepeatingCharsLenEntireString() {
+        String actual = LongestPalindrome.longestPlaindrome("abcba");
+        String expected = "abcba";
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void longestPalindromeRepeatingCharsLenAll() {
-	String actual = LongestPalindrome.longestPlaindrome("bbbbbbb");
-	String expected = "bbbbbbb";
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void longestPalindromeRepeatingCharsLenAll() {
+        String actual = LongestPalindrome.longestPlaindrome("bbbbbbb");
+        String expected = "bbbbbbb";
+        Assert.assertEquals(expected, actual);
+    }
 }
