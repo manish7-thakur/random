4b7a2af75cf75c3271cc62802da29c290fc0ba18
Manish <manish.thakur@agoda.com>
2021-01-07 15:50:55 +0700

    format files & assert for values

--- src/main/java/BinaryTreeInverter.java
+++ src/main/java/BinaryTreeInverter.java
@@ -1,12 +1,12 @@
 public class BinaryTreeInverter {
-	public static void invert(TreeNode root) {
-		if(root == null) {
-			return;
-		}
-		TreeNode left = root.left;
-		root.left = root.right;
-		root.right = left;
-		invert(root.left);
-		invert(root.right);
-	}
+    public static void invert(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        invert(root.left);
+        invert(root.right);
+    }
 }
--- src/test/java/BinaryTreeInverterTest.java
+++ src/test/java/BinaryTreeInverterTest.java
@@ -2,36 +2,36 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BinaryTreeInverterTest {
-	@Test
-	public void Invert2Nodes() {
-		TreeNode root = new TreeNode(5, null, new TreeNode(6, null, null));
-		BinaryTreeInverter.invert(root);
-		Assert.assertNull(root.right);
-		Assert.assertEquals(6, root.left.value);
-	}
+    @Test
+    public void Invert2Nodes() {
+        TreeNode root = new TreeNode(5, null, new TreeNode(6, null, null));
+        BinaryTreeInverter.invert(root);
+        Assert.assertNull(root.right);
+        Assert.assertEquals(6, root.left.value);
+    }
 
-	@Test
-	public void Invert5Nodes() {
-		TreeNode root = new TreeNode(5, new TreeNode(4, new TreeNode(3, null, null), null), new TreeNode(6, null, new TreeNode(7, null, null)));
-		BinaryTreeInverter.invert(root);
-		Assert.assertEquals(4, root.right.value);
-		Assert.assertEquals(6, root.left.value);
-		Assert.assertEquals(7, root.left.left.value);
-		Assert.assertNull(root.left.right);
-		Assert.assertEquals(3, root.right.right.value);
-		Assert.assertNull(root.right.left);
-	}
+    @Test
+    public void Invert5Nodes() {
+        TreeNode root = new TreeNode(5, new TreeNode(4, new TreeNode(3, null, null), null), new TreeNode(6, null, new TreeNode(7, null, null)));
+        BinaryTreeInverter.invert(root);
+        Assert.assertEquals(4, root.right.value);
+        Assert.assertEquals(6, root.left.value);
+        Assert.assertEquals(7, root.left.left.value);
+        Assert.assertNull(root.left.right);
+        Assert.assertEquals(3, root.right.right.value);
+        Assert.assertNull(root.right.left);
+    }
 
-	@Test
-	public void Invert5Nodes() {
-		TreeNode root = new TreeNode(5, new TreeNode(4, new TreeNode(3, null, null), new TreeNode(2, null, null)), 
-			new TreeNode(6, new TreeNode(8, null, null), new TreeNode(7, null, null)));
-		BinaryTreeInverter.invert(root);
-		Assert.assertEquals(4, root.right.value);
-		Assert.assertEquals(6, root.left.value);
-		Assert.assertEquals(7, root.left.left.value);
-		Assert.assertEquals(8, root.left.right);
-		Assert.assertEquals(3, root.right.right.value);
-		Assert.assertEquals(2, root.right.left.value);
-	}
+    @Test
+    public void Invert7Nodes() {
+        TreeNode root = new TreeNode(5, new TreeNode(4, new TreeNode(3, null, null), new TreeNode(2, null, null)),
+                new TreeNode(6, new TreeNode(8, null, null), new TreeNode(7, null, null)));
+        BinaryTreeInverter.invert(root);
+        Assert.assertEquals(4, root.right.value);
+        Assert.assertEquals(6, root.left.value);
+        Assert.assertEquals(7, root.left.left.value);
+        Assert.assertEquals(8, root.left.right.value);
+        Assert.assertEquals(3, root.right.right.value);
+        Assert.assertEquals(2, root.right.left.value);
+    }
 }
