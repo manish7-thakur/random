a8d8fb15592e27feaa5862aca6007aa87a906e03
Manish <manish.thakur@agoda.com>
2021-01-07 15:39:34 +0700

    binary tree invert 5 nodes

---
+++ src/test/java/BinaryTreeInverterTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinaryTreeInverterTest {
+	@Test
+	public void Invert2Nodes() {
+		TreeNode root = new TreeNode(5, null, new TreeNode(6, null, null));
+		BinaryTreeInverter.invert(root);
+		Assert.assertNull(root.right);
+		Assert.assertEquals(6, root.left.value);
+	}
+	@Test
+	public void Invert5Nodes() {
+		TreeNode root = new TreeNode(5, new TreeNode(4, new TreeNode(3, null, null), null), new TreeNode(6, null, new TreeNode(7, null, null)));
+		BinaryTreeInverter.invert(root);
+		Assert.assertEquals(4, root.right.value);
+		Assert.assertEquals(6, root.left.value);
+		Assert.assertEquals(7, root.left.left.value);
+		Assert.assertNull(root.left.right);
+		Assert.assertEquals(3, root.right.right.value);
+		Assert.assertNull(root.right.left);
+	}
+}
