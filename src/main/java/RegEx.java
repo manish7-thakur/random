d09c2bea2c0fa237d3ab9e606774b873cbb7fb1e
Manish <manish.thakur@agoda.com>
2021-01-10 20:29:32 +0700

    regex matches for 5 chars

--- src/main/java/RegEx.java
+++ src/main/java/RegEx.java
@@ -1,21 +1,21 @@
 public class RegEx {
-	public static boolean matches(String s, String p, int i , int j) {
-		if(i == -1) {
-			return true;
-		}
-		if(i != -1 && j == -1) {
-			return false;
-		}
-		if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
-			return matches(s, p, i-1, j-1);
-		}
-		if(p.charAt(j) == '*') {
-			if(s.charAt(i) == p.charAt(j-1) || p.charAt(j-1) == '.') {
-				return matches(s,p, i-1, j);
-			} else {
-				return matches(s,p, i, j-2);
-			}
-		}
-		return false;
-	}
+    public static boolean matches(String s, String p, int i, int j) {
+        if ((i == -1 && j == -1) || (i == -1 && j == 1 && p.charAt(j) == '*')) {
+            return true;
+        }
+        if (i == -1 || j == -1) {
+            return false;
+        }
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            return matches(s, p, i - 1, j - 1);
+        }
+        if (p.charAt(j) == '*') {
+            if (s.charAt(i) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                return matches(s, p, i - 1, j) || matches(s, p, i, j - 2);
+            } else {
+                return matches(s, p, i, j - 2);
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/RegExTest.java
+++ src/test/java/RegExTest.java
@@ -1,53 +1,58 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class RegExTest {
-	@Test
-	public void matchEmptyString() {
-		boolean actual = RegEx.matches("","", -1, -1);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchEmptyString() {
+        boolean actual = RegEx.matches("", "", -1, -1);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void matchStringSingleCharNoWildCard() {
-		boolean actual = RegEx.matches("a","a", 0, 0);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchStringSingleCharNoWildCard() {
+        boolean actual = RegEx.matches("a", "a", 0, 0);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void matchStringSingleCharWildCard() {
-		boolean actual = RegEx.matches("a","a*", 0, 1);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchStringSingleCharWildCard() {
+        boolean actual = RegEx.matches("a", "a*", 0, 1);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void matchStringDoubleCharWildCard() {
-		boolean actual = RegEx.matches("ab","a*", 1, 1);
-		Assert.assertFalse(actual);
-	}
+    @Test
+    public void matchStringDoubleCharWildCard() {
+        boolean actual = RegEx.matches("ab", "a*", 1, 1);
+        Assert.assertFalse(actual);
+    }
 
-	@Test
-	public void matchStringTripleCharWildCard() {
-		boolean actual = RegEx.matches("acb","a*b", 2, 2);
-		Assert.assertFalse(actual);
-	}
+    @Test
+    public void matchStringTripleCharWildCard() {
+        boolean actual = RegEx.matches("acb", "a*b", 2, 2);
+        Assert.assertFalse(actual);
+    }
 
-	@Test
-	public void matchStringTripleCharWildCardOnlyDot() {
-		boolean actual = RegEx.matches("bcy","b.y", 2, 2);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchStringTripleCharWildCardOnlyDot() {
+        boolean actual = RegEx.matches("bcy", "b.y", 2, 2);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void matchStringFourCharWildCard() {
-		boolean actual = RegEx.matches("bcdy","b.*y", 3, 3);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchStringFourCharWildCard() {
+        boolean actual = RegEx.matches("bcdy", "b.*y", 3, 3);
+        Assert.assertTrue(actual);
+    }
 
-	@Test
-	public void matchStringFiveCharWildCard() {
-		boolean actual = RegEx.matches("bcdy","a*b.*y", 3, 5);
-		Assert.assertTrue(actual);
-		boolean actual = RegEx.matches("ablmy","a*b.*y", 4, 5);
-		Assert.assertTrue(actual);
-	}
+    @Test
+    public void matchStringFiveCharWildCard() {
+        boolean actual = RegEx.matches("bcdy", "a*b.*y", 3, 5);
+        Assert.assertTrue(actual);
+        actual = RegEx.matches("ablmy", "a*b.*y", 4, 5);
+        Assert.assertTrue(actual);
+        actual = RegEx.matches("ay", "a*b.*y", 1, 5);
+        Assert.assertFalse(actual);
+        actual = RegEx.matches("ab", "a*b.*y", 1, 5);
+        Assert.assertFalse(actual);
+    }
 }
