c59b7d96ad9fd439538aab06fd64a98a2bba582e
Manish <manish.thakur@agoda.com>
2021-01-14 21:40:36 +0700

    format code

--- src/main/java/NoOfIslands.java
+++ src/main/java/NoOfIslands.java
@@ -1,42 +1,39 @@
 public class NoOfIslands {
-	
-public static int noOfIslands(int[][] grid) {
-	boolean[][] visited = new boolean[grid.length][grid[0].length];
-	return find(grid, visited);
-}
 
-static int find(int[][] grid, boolean[][] visited) {
-	int count = 0;
-	for(int i = 0; i < grid.length; i++) {
-	for(int j = 0; j < grid[0].length; j++) {
-	if(grid[i][j] == 1 && !visited[i][j]) {
-		visit(grid, visited, i, j, grid.length, grid[0].length);
-		count++;
-	}
-	}
-	}
-	return count;
-}
+    public static int noOfIslands(int[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        return find(grid, visited);
+    }
 
-static void visit(int[][] grid, boolean[][] visited, int  i , int j, int l, int b) {
-	if(!isSafe(grid, i, j, l, b, visited)) {
-	return;
-	}
-	visited[i][j] = true;
-	visit(grid, visited, i+1, j, l ,b);
-	visit(grid, visited, i, j+1, l,b);
-	visit(grid, visited, i-1, j,l, b);
-	visit(grid, visited, i, j-1,l ,b);
-	visit(grid, visited, i-1, j-1,l ,b);
-	visit(grid, visited, i-1, j+1,l ,b);
-	visit(grid, visited, i+1, j+1,l ,b);
-	visit(grid, visited, i+1, j-1,l ,b);
-}
+    static int find(int[][] grid, boolean[][] visited) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    visit(grid, visited, i, j, grid.length, grid[0].length);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
-static boolean isSafe(int[][] grid, int i, int j, int l, int b, boolean[][] visited) {
-	if(i >= 0 && i < l && j >=0 && j < b && !visited[i][j] && grid[i][j] == 1) {
-	return true;
-	}
-	return false;
-}
+    static void visit(int[][] grid, boolean[][] visited, int i, int j, int l, int b) {
+        if (!isSafe(grid, i, j, l, b, visited)) {
+            return;
+        }
+        visited[i][j] = true;
+        visit(grid, visited, i + 1, j, l, b);
+        visit(grid, visited, i, j + 1, l, b);
+        visit(grid, visited, i - 1, j, l, b);
+        visit(grid, visited, i, j - 1, l, b);
+        visit(grid, visited, i - 1, j - 1, l, b);
+        visit(grid, visited, i - 1, j + 1, l, b);
+        visit(grid, visited, i + 1, j + 1, l, b);
+        visit(grid, visited, i + 1, j - 1, l, b);
+    }
+
+    static boolean isSafe(int[][] grid, int i, int j, int l, int b, boolean[][] visited) {
+        return i >= 0 && i < l && j >= 0 && j < b && !visited[i][j] && grid[i][j] == 1;
+    }
 }
--- src/test/java/NoOfIslandsTest.java
+++ src/test/java/NoOfIslandsTest.java
@@ -1,90 +1,91 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class NoOfIslandsTest {
-	@Test
-	public void oneCrossOneGrid() {
-	int[][] grid = new int[][] {{1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 1;
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void oneCrossOneGrid() {
+        int[][] grid = new int[][]{{1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
     @Test
-	public void twoCrossTwoGrid() {
-	int[][] grid = new int[][] {
-		{1, 0}, 
-		{0, 1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 1;
-	Assert.assertEquals(expected, actual);
-	}
+    public void twoCrossTwoGrid() {
+        int[][] grid = new int[][]{
+                {1, 0},
+                {0, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void twoCrossThreeGrid() {
-	int[][] grid = new int[][] {
-		{1, 0, 0}, 
-		{0, 0, 1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 2;
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void twoCrossThreeGrid() {
+        int[][] grid = new int[][]{
+                {1, 0, 0},
+                {0, 0, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void twoCrossFourGrid() {
-	int[][] grid = new int[][] {
-		{1, 0, 0, 1}, 
-		{1, 1, 0, 1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 2;
-	Assert.assertEquals(expected, actual);
-	grid = new int[][] {
-		{1, 1, 1, 1}, 
-		{1, 1, 0, 1}};
-		 actual = NoOfIslands.noOfIslands(grid);
-	 expected = 1;
-	Assert.assertEquals(expected, actual);
-}
-	@Test
-	public void threeCrossTwoGrid() {
-	int[][] grid = new int[][] {
-		{1, 0}, 
-		{0, 0},
-		{1,1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 2;
-	Assert.assertEquals(expected, actual);
-	grid = new int[][] {
-		{1, 1}, 
-		{1, 1},
-		{1,1}};
-	 actual = NoOfIslands.noOfIslands(grid);
-	 expected = 1;
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void twoCrossFourGrid() {
+        int[][] grid = new int[][]{
+                {1, 0, 0, 1},
+                {1, 1, 0, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+        grid = new int[][]{
+                {1, 1, 1, 1},
+                {1, 1, 0, 1}};
+        actual = NoOfIslands.noOfIslands(grid);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void fourCrossFourGrid() {
-	int[][] grid = new int[][] {
-		{1, 0, 0, 1}, 
-		{0, 0, 1, 1},
-		{1, 0, 0, 0},
-		{0, 0, 1, 1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 4;
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void threeCrossTwoGrid() {
+        int[][] grid = new int[][]{
+                {1, 0},
+                {0, 0},
+                {1, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+        grid = new int[][]{
+                {1, 1},
+                {1, 1},
+                {1, 1}};
+        actual = NoOfIslands.noOfIslands(grid);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-		@Test
-	public void fiveCrossFiveGrid() {
-	int[][] grid = new int[][] {
-				   {1, 1, 0, 0, 0},
-                   {0, 1, 0, 0, 1},
-                   {1, 0, 0, 1, 1},
-                   {0, 0, 0, 0, 0},
-                   {1, 0, 1, 0, 1}};
-	int actual = NoOfIslands.noOfIslands(grid);
-	int expected = 5;
-	Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void fourCrossFourGrid() {
+        int[][] grid = new int[][]{
+                {1, 0, 0, 1},
+                {0, 0, 1, 1},
+                {1, 0, 0, 0},
+                {0, 0, 1, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 4;
+        Assert.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void fiveCrossFiveGrid() {
+        int[][] grid = new int[][]{
+                {1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 1},
+                {1, 0, 0, 1, 1},
+                {0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 1}};
+        int actual = NoOfIslands.noOfIslands(grid);
+        int expected = 5;
+        Assert.assertEquals(expected, actual);
+    }
 }
