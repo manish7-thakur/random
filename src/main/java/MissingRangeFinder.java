7f62d1f6e578728a9fd9cc69d7732d9a464c7339
Manish <manish.thakur@agoda.com>
2021-01-15 21:40:45 +0700

    find missing ranges for upto 3 range length

---
+++ src/main/java/MissingRangeFinder.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class MissingRangeFinder {
+    public static ArrayList<String> find(int[] ranges, int l, int h) {
+        ArrayList<String> res = new ArrayList<>();
+        if (ranges.length == 0) {
+            res.add(l + "->" + h);
+            return res;
+        }
+        int i = 0;
+        while (i < ranges.length) {
+            if (l < ranges[i]) {
+                int curr = ranges[i] - 1;
+                if (l == curr) {
+                    res.add("" + l);
+                } else {
+                    res.add("" + l + "->" + curr);
+                }
+            }
+            l = ranges[i] + 1;
+            i++;
+        }
+        if (l < h) {
+            res.add("" + l + "->" + h);
+        } else if (l == h) {
+            res.add("" + l);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/MissingRangeFinderTest.java
@@ -0,0 +1,70 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class MissingRangeFinderTest {
+    @Test
+    public void emptyRanges() {
+        int l = -1;
+        int h = 2;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(l + "->" + h);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void singleRange() {
+        int l = -1;
+        int h = 2;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{0}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(l + "");
+        expected.add(1 + "->" + 2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void singleRangeSameAsLow() {
+        int l = 0;
+        int h = 2;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{0}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(1 + "->" + 2);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void twoRange() {
+        int l = -1;
+        int h = 2;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{0, 1}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(-1 + "");
+        expected.add(2 + "");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void twoRangeSameAsLowHigh() {
+        int l = 0;
+        int h = 1;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{0, 1}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void threeRanges() {
+        int l = -2;
+        int h = 6;
+        ArrayList<String> actual = MissingRangeFinder.find(new int[]{0, 1, 3}, l, h);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add(-2 + "->" + -1);
+        expected.add(2 + "");
+        expected.add(4 + "->" + 6);
+        Assert.assertEquals(expected, actual);
+    }
+}
