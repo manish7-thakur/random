84dd88b0c28952c400c3478d8c3253fcf80acb34
Manish <manish.thakur@agoda.com>
2021-01-16 17:33:22 +0700

    4 array pair sum

--- src/test/java/TripletSumTest.java
+++ src/test/java/TripletSumTest.java
@@ -9,9 +9,16 @@ public class TripletSumTest {
     }
 
     @Test
-    public void ArrayWithTheeElemTest() {
+    public void ArrayWithThreeElemTest() {
         int actual = TripletSum.countTriplets(new int[]{2, 3, 1}, 9);
         int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
+
+   @Test
+    public void ArrayWithFourElemTest() {
+        int actual = TripletSum.countTriplets(new int[]{-1, 5, 4, 1}, 6);
+        int expected = 2;
         Assert.assertEquals(expected, actual);
     }
 }
