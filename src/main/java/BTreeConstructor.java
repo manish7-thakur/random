81e115ff4cad025e2b07cf66352b218204869c8b
Manish <manish.thakur@agoda.com>
2021-01-19 23:46:44 +0700

    simplify BtreeConstructercode

--- src/main/java/BTreeConstructor.java
+++ src/main/java/BTreeConstructor.java
@@ -1,27 +1,17 @@
 public class BTreeConstructor {
     public static TreeNode fromPreorder(int[] elems) {
-        TreeNode root = new TreeNode(elems[0], null, null);
-        build(elems, root, 0, elems.length - 1);
-        return root;
+        return build(elems, 0, elems.length - 1);
     }
 
-    private static void build(int[] elems, TreeNode root, int i, int j) {
-        if (i >= j) {
-            return;
+    private static TreeNode build(int[] elems, int i, int j) {
+        if (i > j) {
+            return null;
         }
+        TreeNode root = new TreeNode(elems[i], null, null);
         int idx = findRightSubtreeRoot(elems, i, j);
-        if (idx <= j) {
-            root.right = new TreeNode(elems[idx], null, null);
-        }
-        if (i + 1 != idx) {
-            root.left = new TreeNode(elems[i + 1], null, null);
-        }
-        if (root.right != null) {
-            build(elems, root.right, idx, j);
-        }
-        if (root.left != null) {
-            build(elems, root.left, i + 1, idx - 1);
-        }
+        root.right = build(elems, idx, j);
+        root.left = build(elems, i + 1, idx - 1);
+        return root;
     }
 
     static int findRightSubtreeRoot(int[] elems, int i, int j) {
