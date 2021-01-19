239e2340c77979f12422fc4317fbe387c45c2c1a
Manish <manish.thakur@agoda.com>
2021-01-19 22:58:27 +0700

    optimizyte cand format code

--- src/main/java/BTreeConstructor.java
+++ src/main/java/BTreeConstructor.java
@@ -1,32 +1,32 @@
 public class BTreeConstructor {
-	public static TreeNode fromPreorder(int[] elems) {
-		TreeNode root = new TreeNode(elems[0], null, null);
-		build(elems, root, 0, elems.length -1);
-		return root;
-	}
+    public static TreeNode fromPreorder(int[] elems) {
+        TreeNode root = new TreeNode(elems[0], null, null);
+        build(elems, root, 0, elems.length - 1);
+        return root;
+    }
 
-	private static void build(int[] elems, TreeNode root, int i, int j) {
-		if(i >= j) {
-			return;
-		}
-		int idx = findRightSubtreeRoot(elems, i, j);
-		if(!(idx > j)) {
-			root.right = new TreeNode(elems[idx], null, null);
-		}
-		if(i +1 != idx) {
-			root.left = new TreeNode(elems[i+1], null, null);
-		}
-		if(root.right != null) {
-			build(elems, root.right, idx, j);
-		}
-		if(root.left != null) {
-			build(elems, root.left, i + 1, idx - 1);
-		}
-	}
+    private static void build(int[] elems, TreeNode root, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        int idx = findRightSubtreeRoot(elems, i, j);
+        if (idx <= j) {
+            root.right = new TreeNode(elems[idx], null, null);
+        }
+        if (i + 1 != idx) {
+            root.left = new TreeNode(elems[i + 1], null, null);
+        }
+        if (root.right != null) {
+            build(elems, root.right, idx, j);
+        }
+        if (root.left != null) {
+            build(elems, root.left, i + 1, idx - 1);
+        }
+    }
 
-	static int findRightSubtreeRoot(int[] elems, int i, int j) {
-		int k = i+1;
-		for(; k <= j && elems[k] < elems[i]; k++);
-		return k;
-	}
+    static int findRightSubtreeRoot(int[] elems, int i, int j) {
+        int k = i + 1;
+        for (; k <= j && elems[k] < elems[i]; k++) ;
+        return k;
+    }
 }
--- src/test/java/BTreeConstructorTest.java
+++ src/test/java/BTreeConstructorTest.java
@@ -1,93 +1,94 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BTreeConstructorTest {
-	@Test
-	public void singleNode() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {3});
-		Assert.assertEquals(3, root.value);
-	}
+    @Test
+    public void singleNode() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{3});
+        Assert.assertEquals(3, root.value);
+    }
 
-	@Test
-	public void doubleNodesRightSubtree() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {3, 5});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(5, root.right.value);
-		Assert.assertNull(root.left);
-	}
+    @Test
+    public void doubleNodesRightSubtree() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{3, 5});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(5, root.right.value);
+        Assert.assertNull(root.left);
+    }
 
-	@Test
-	public void doubleNodesLeftSubtree() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {3, 2});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(2, root.left.value);
-	}
+    @Test
+    public void doubleNodesLeftSubtree() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{3, 2});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(2, root.left.value);
+    }
 
-	@Test
-	public void tripleNodesRightSkewed() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {3, 4, 5});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(4, root.right.value);
-		Assert.assertEquals(5, root.right.right.value);
-	}
+    @Test
+    public void tripleNodesRightSkewed() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{3, 4, 5});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(4, root.right.value);
+        Assert.assertEquals(5, root.right.right.value);
+    }
 
-	@Test
-	public void tripleNodesLeftSkewed() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {5, 4, 3});
-		Assert.assertEquals(5, root.value);
-		Assert.assertEquals(4, root.left.value);
-		Assert.assertEquals(3, root.left.left.value);
-	}
+    @Test
+    public void tripleNodesLeftSkewed() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{5, 4, 3});
+        Assert.assertEquals(5, root.value);
+        Assert.assertEquals(4, root.left.value);
+        Assert.assertEquals(3, root.left.left.value);
+    }
 
-	@Test
-	public void fourNodes() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {3, 2, 4, 5});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(2, root.left.value);
-		Assert.assertEquals(4, root.right.value);
-		Assert.assertEquals(5, root.right.right.value);
-		root = BTreeConstructor.fromPreorder(new int[] {3, 2, 5, 4});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(2, root.left.value);
-		Assert.assertEquals(5, root.right.value);
-		Assert.assertEquals(4, root.right.left.value);
-		root = BTreeConstructor.fromPreorder(new int[] {3, 4, 5, 6});
-		Assert.assertEquals(3, root.value);
-		Assert.assertEquals(4, root.right.value);
-		Assert.assertEquals(5, root.right.right.value);
-		Assert.assertEquals(6, root.right.right.right.value);
-		root = BTreeConstructor.fromPreorder(new int[] {6, 5, 4, 3});
-		Assert.assertEquals(6, root.value);
-		Assert.assertEquals(5, root.left.value);
-		Assert.assertEquals(4, root.left.left.value);
-		Assert.assertEquals(3, root.left.left.left.value);
-	}
+    @Test
+    public void fourNodes() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{3, 2, 4, 5});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(2, root.left.value);
+        Assert.assertEquals(4, root.right.value);
+        Assert.assertEquals(5, root.right.right.value);
+        root = BTreeConstructor.fromPreorder(new int[]{3, 2, 5, 4});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(2, root.left.value);
+        Assert.assertEquals(5, root.right.value);
+        Assert.assertEquals(4, root.right.left.value);
+        root = BTreeConstructor.fromPreorder(new int[]{3, 4, 5, 6});
+        Assert.assertEquals(3, root.value);
+        Assert.assertEquals(4, root.right.value);
+        Assert.assertEquals(5, root.right.right.value);
+        Assert.assertEquals(6, root.right.right.right.value);
+        root = BTreeConstructor.fromPreorder(new int[]{6, 5, 4, 3});
+        Assert.assertEquals(6, root.value);
+        Assert.assertEquals(5, root.left.value);
+        Assert.assertEquals(4, root.left.left.value);
+        Assert.assertEquals(3, root.left.left.left.value);
+    }
 
-	@Test
-	public void sixNodes() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {10,13, 12,11, 14, 15});
-		Assert.assertEquals(10, root.value);
-		Assert.assertNull(root.left);
-		Assert.assertEquals(13, root.right.value);
-		Assert.assertEquals(14, root.right.right.value);
-		Assert.assertEquals(12, root.right.left.value);
-		Assert.assertEquals(15, root.right.right.right.value);
-		Assert.assertEquals(11, root.right.left.left.value);
-	}
+    @Test
+    public void sixNodes() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{10, 13, 12, 11, 14, 15});
+        Assert.assertEquals(10, root.value);
+        Assert.assertNull(root.left);
+        Assert.assertEquals(13, root.right.value);
+        Assert.assertEquals(14, root.right.right.value);
+        Assert.assertEquals(12, root.right.left.value);
+        Assert.assertEquals(15, root.right.right.right.value);
+        Assert.assertEquals(11, root.right.left.left.value);
+    }
 
-	@Test
-	public void tenNodes() {
-		TreeNode root = BTreeConstructor.fromPreorder(new int[] {10,6,5,4,7,8,13, 12,11, 14, 15});
-		Assert.assertEquals(10, root.value);
-		Assert.assertEquals(13, root.right.value);
-		Assert.assertEquals(14, root.right.right.value);
-		Assert.assertEquals(12, root.right.left.value);
-		Assert.assertEquals(15, root.right.right.right.value);
-		Assert.assertEquals(11, root.right.left.left.value);
-		Assert.assertEquals(6, root.left.value);
-		Assert.assertEquals(5, root.left.left.value);
-		Assert.assertEquals(7, root.left.right.value);
-		Assert.assertEquals(4, root.left.left.left.value);
-		Assert.assertEquals(8, root.left.right.right.value);
-	}
+    @Test
+    public void tenNodes() {
+        TreeNode root = BTreeConstructor.fromPreorder(new int[]{10, 6, 5, 4, 7, 8, 13, 12, 11, 14, 15});
+        Assert.assertEquals(10, root.value);
+        Assert.assertEquals(13, root.right.value);
+        Assert.assertEquals(14, root.right.right.value);
+        Assert.assertEquals(12, root.right.left.value);
+        Assert.assertEquals(15, root.right.right.right.value);
+        Assert.assertEquals(11, root.right.left.left.value);
+        Assert.assertEquals(6, root.left.value);
+        Assert.assertEquals(5, root.left.left.value);
+        Assert.assertEquals(7, root.left.right.value);
+        Assert.assertEquals(4, root.left.left.left.value);
+        Assert.assertEquals(8, root.left.right.right.value);
+    }
 
 }
