701684ee0610866a155ec435b50b24574bb0ec96
Manish <manish.thakur@agoda.com>
2021-01-23 22:42:22 +0700

    twiddle + kill

--- src/main/java/EditDistance.java
+++ src/main/java/EditDistance.java
@@ -1,21 +1,21 @@
 public class EditDistance {
-	public static int find(String s1, String s2, int i, int j) {
-		if(i < 0 && j < 0) {
-			return 0;
-		}
-		if(i < 0) {
-			return 1+ find(s1, s2, i, j-1); // Insert
-		}
-		if(j < 0) {
-			return 1 + find(s1, s2, i-1, j); // Delete
-		}
-		if(s1.charAt(i) != s2.charAt(j)) {
-			if(i > 0 && j > 0 && s1.charAt(i) == s2.charAt(j-1) && s1.charAt(i-1) == s2.charAt(j)) {
-				return 1 + find(s1,s2,i-2,j-2);
-			}
-			return Math.min(Math.min(1+ find(s1,s2,i-1,j-1), 1 + find(s1, s2, i-1, j)), 1+ find(s1, s2, i, j-1));
-		} 
-		return 1 + find(s1, s2, i-1, j-1); // Copy
-
-	}
+    public static int find(String s1, String s2, int i, int j) {
+        if (i < 0 && j < 0) {
+            return 0;
+        }
+        if (i < 0) {
+            return 1 + find(s1, s2, i, j - 1); // Insert
+        }
+        if (j < 0) {
+            return 1; // Kill as target is achieved
+        }
+        if (s1.charAt(i) != s2.charAt(j)) {
+            if (i > 0 && j > 0 && s1.charAt(i) == s2.charAt(j - 1) && s1.charAt(i - 1) == s2.charAt(j)) {
+                return 1 + find(s1, s2, i - 2, j - 2);
+            }
+            //Min (replace, insert->delete, delete -> insert)
+            return Math.min(Math.min(1 + find(s1, s2, i - 1, j - 1), 1 + find(s1, s2, i - 1, j)), 1 + find(s1, s2, i, j - 1));
+        }
+        return 1 + find(s1, s2, i - 1, j - 1); // Copy
+    }
 }
--- src/test/java/EditDistanceTest.java
+++ src/test/java/EditDistanceTest.java
@@ -1,58 +1,90 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-//cost
-//copy(c) -> 1
-//replace(r) -> 1
-//insert(i) -> 1
-//delete(d) -> 1 
-//twiddle(t) -> 1
+/**
+ * cost
+ * copy(c) -> 1
+ * replace(r) -> 1
+ * insert(i) -> 1
+ * delete(d) -> 1
+ * twiddle(t) -> 1
+ */
+
 public class EditDistanceTest {
-	@Test
-	public void singleCharStringEmptySource() {
-		int actual = EditDistance.find("", "n", -1, 0);
-		int expected = 1;//insert
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void singleCharStringEmptySource() {
+        int actual = EditDistance.find("", "n", -1, 0);
+        int expected = 1;//insert
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void singleCharStringTargetEmpty() {
-		int actual = EditDistance.find("a", "", 0, -1);
-		int expected = 1;//delete
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void singleCharStringTargetEmpty() {
+        int actual = EditDistance.find("a", "", 0, -1);
+        int expected = 1;//kill
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void singleCharIdenticalString() {
-		int actual = EditDistance.find("a", "a", 0, 0);
-		int expected = 1; //copy
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void singleCharIdenticalString() {
+        int actual = EditDistance.find("a", "a", 0, 0);
+        int expected = 1; //copy
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void singleCharNonIdenticalString() {
-		int actual = EditDistance.find("a", "b", 0, 0);
-		int expected = 1; //Min(replace, delete + insert)
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void singleCharNonIdenticalString() {
+        int actual = EditDistance.find("a", "b", 0, 0);
+        int expected = 1; //Min(replace, delete + insert)
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void doubleCharNonIdenticalString() {
-		int actual = EditDistance.find("ab", "bc", 1, 1);
-		int expected = 2; //Min(replace + replace, insert + copy + delete)
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void doubleCharNonIdenticalString() {
+        int actual = EditDistance.find("ab", "bc", 1, 1);
+        int expected = 2; //Min(replace + replace, insert + copy + delete)
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void doubleCharReversedString() {
-		int actual = EditDistance.find("ab", "ba", 1, 1);
-		int expected = 1; //twiddle
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void doubleCharReversedString() {
+        int actual = EditDistance.find("ab", "ba", 1, 1);
+        int expected = 1; //twiddle
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void doubleCharSTripleCharT() {
-		int actual = EditDistance.find("ab", "bac", 1, 2);
-		int expected = 2; //insert + twiddle
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void doubleCharSTripleCharT() {
+        int actual = EditDistance.find("ab", "bac", 1, 2);
+        int expected = 2; //insert + twiddle
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tripleCharSTripleCharT() {
+        int actual = EditDistance.find("abc", "bac", 2, 2);
+        int expected = 2; //copy + twiddle
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fourCharSTripleCharT() {
+        int actual = EditDistance.find("abcd", "bac", 3, 2);
+        int expected = 3; //delete + copy + twiddle
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fiveCharSFiveCharT() {
+        int actual = EditDistance.find("abcde", "bacef", 4, 4);
+        int expected = 4; //delete + copy + twiddle
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fiveCharStwoCharT() {
+        int actual = EditDistance.find("cdeab", "ba", 4, 1);
+        int expected = 2; // twiddle + kill
+        Assert.assertEquals(expected, actual);
+    }
 }
