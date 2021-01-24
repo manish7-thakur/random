f4e347e1cebab752a842d6fe1437add55ec19539
Manish <manish.thakur@agoda.com>
2021-01-24 16:45:34 +0700

    edit distance based on defined cost

--- src/main/java/EditDistance.java
+++ src/main/java/EditDistance.java
@@ -1,51 +1,76 @@
-import java.util.*;
+import java.util.Arrays;
+
 public class EditDistance {
-    public static int find(String s1, String s2, int i, int j) {
+    public static String find(String s1, String s2, int i, int j) {
+        StringBuilder b = new StringBuilder();
+        findNow(s1, s2, i, j);
+        return b.toString();
+    }
+
+    static int findNow(String s1, String s2, int i, int j) {
         if (i < 0 && j < 0) {
             return 0;
         }
         if (i < 0) {
-            return 1 + find(s1, s2, i, j - 1); // Insert
+            return 2 * (j + 1); // Cost of Inserting rem chars in s2
         }
         if (j < 0) {
-            return 1; // Kill as target is achieved
+            return Math.min(2 * (i + 1), Integer.MAX_VALUE); // min of delete, kill
         }
-        if (s1.charAt(i) != s2.charAt(j)) {
-            if (i > 0 && j > 0 && s1.charAt(i) == s2.charAt(j - 1) && s1.charAt(i - 1) == s2.charAt(j)) {
-                return 1 + find(s1, s2, i - 2, j - 2);
-            }
-            //Min (replace, insert->delete, delete -> insert)
-            return 1 +  Math.min(Math.min(find(s1, s2, i - 1, j - 1), find(s1, s2, i - 1, j)), find(s1, s2, i, j - 1));
+        int twiddle = Integer.MAX_VALUE;
+        int copy = Integer.MAX_VALUE;
+        if (i > 0 && j > 0 && s1.charAt(i) == s2.charAt(j - 1) && s1.charAt(i - 1) == s2.charAt(j)) {
+            twiddle = Integer.MAX_VALUE;
         }
-        return 1 + find(s1, s2, i - 1, j - 1); // Copy
+        if (s1.charAt(i) == s2.charAt(j)) {
+            copy = -1 + findNow(s1, s2, i - 1, j - 1);
+        }
+        int replace = 1 + findNow(s1, s2, i - 1, j - 1);
+        int insert = 2 + findNow(s1, s2, i - 1, j);
+        int delete = 2 + findNow(s1, s2, i, j - 1);
+        int min = findMin(twiddle, replace, insert, delete, copy);
+        if (min == replace) {
+            return replace;
+        } else if (min == copy) {
+            return copy;
+        } else if (min == insert) {
+            return insert;
+        } else if (min == delete) {
+            return delete;
+        }
+        return twiddle;
+    }
+
+    static int findMin(int... values) {
+        Arrays.sort(values);
+        return values[0];
     }
 
     static String getOps(char[][] path, int i, int j, StringBuilder b) {
-        if(i < 0) {
+        if (i < 0) {
             return b.toString();
-            }
-        if(j< 0) {
-            if(i < 0) {
-            return b.toString();
+        }
+        if (j < 0) {
+            if (i < 0) {
+                return b.toString();
             }
             b.append('k');
             return b.toString();
         }
-        if(path[i][j] == 'i') {
+        if (path[i][j] == 'i') {
             b.append('i');
-            return getOps(path, i, j-1, b);
-        } else if(path[i][j] == 'c' || path[i][j] == 'r') {
+            return getOps(path, i, j - 1, b);
+        } else if (path[i][j] == 'c' || path[i][j] == 'r') {
             b.append(path[i][j]);
-            return getOps(path, i - 1, j-1, b);
-        } else if(path[i][j] == 'd') {
+            return getOps(path, i - 1, j - 1, b);
+        } else if (path[i][j] == 'd') {
             b.append('d');
-            return getOps(path, i -1, j, b);
-        } else if(path[i][j] == 't') {
+            return getOps(path, i - 1, j, b);
+        } else if (path[i][j] == 't') {
             b.append('t');
-            return getOps(path, i -2, j-2, b);
+            return getOps(path, i - 2, j - 2, b);
         }
         return b.toString();
-        
     }
 
 }
--- src/test/java/EditDistanceTest.java
+++ src/test/java/EditDistanceTest.java
@@ -1,91 +1,91 @@
 import org.junit.Assert;
 import org.junit.Test;
-import java.util.*;
 
 /**
  * cost
- * copy(c) -> 1
+ * copy(c) -> -1
  * replace(r) -> 1
- * insert(i) -> 1
- * delete(d) -> 1
- * twiddle(t) -> 1
+ * insert(i) -> 2
+ * delete(d) -> 2
+ * twiddle(t) -> Inf
+ * kill(k) -> Inf
  */
 
 public class EditDistanceTest {
     @Test
     public void singleCharStringEmptySource() {
-        int actual = EditDistance.find("", "n", -1, 0);
-        int expected = 1;//insert
+        int actual = EditDistance.findNow("", "n", -1, 0);
+        int expected = 2;//insert
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void singleCharStringTargetEmpty() {
-        int actual = EditDistance.find("a", "", 0, -1);
-        int expected = 1;//kill
+        int actual = EditDistance.findNow("a", "", 0, -1);
+        int expected = 2;//delete
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void singleCharIdenticalString() {
-        int actual = EditDistance.find("a", "a", 0, 0);
-        int expected = 1; //copy
+        int actual = EditDistance.findNow("a", "a", 0, 0);
+        int expected = -1; //copy
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void singleCharNonIdenticalString() {
-        int actual = EditDistance.find("a", "b", 0, 0);
+        int actual = EditDistance.findNow("a", "b", 0, 0);
         int expected = 1; //Min(replace, delete + insert)
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void doubleCharNonIdenticalString() {
-        int actual = EditDistance.find("ab", "bc", 1, 1);
+        int actual = EditDistance.findNow("ab", "bc", 1, 1);
         int expected = 2; //Min(replace + replace, insert + copy + delete)
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void doubleCharReversedString() {
-        int actual = EditDistance.find("ab", "ba", 1, 1);
-        int expected = 1; //twiddle
+        int actual = EditDistance.findNow("ab", "ba", 1, 1);
+        int expected = 2;
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void doubleCharSTripleCharT() {
-        int actual = EditDistance.find("ab", "bac", 1, 2);
-        int expected = 2; //insert + twiddle
+        int actual = EditDistance.findNow("ab", "bac", 1, 2);
+        int expected = 2;
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void tripleCharSTripleCharT() {
-        int actual = EditDistance.find("abc", "bac", 2, 2);
-        int expected = 2; //copy + twiddle
+        int actual = EditDistance.findNow("abc", "bac", 2, 2);
+        int expected = 1; //copy + twiddle
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void fourCharSTripleCharT() {
-        int actual = EditDistance.find("abcd", "bac", 3, 2);
-        int expected = 3; //delete + copy + twiddle
+        int actual = EditDistance.findNow("abcd", "bac", 3, 2);
+        int expected = 3;
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void fiveCharSFiveCharT() {
-        int actual = EditDistance.find("abcde", "bacef", 4, 4);
-        int expected = 4; //delete + copy + twiddle
+        int actual = EditDistance.findNow("abcde", "bacef", 4, 4);
+        int expected = 3;
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void fiveCharStwoCharT() {
-        int actual = EditDistance.find("cdeab", "ba", 4, 1);
-        int expected = 2; // twiddle + kill
+        int actual = EditDistance.findNow("cdeab", "ba", 4, 1);
+        int expected = 6;
         Assert.assertEquals(expected, actual);
     }
 
@@ -102,8 +102,8 @@ public class EditDistanceTest {
     @Test
     public void opsDoubleCopy() {
         char[][] path = new char[][]{
-            {'c', 'd'},
-            {'k', 'c'}};
+                {'c', 'd'},
+                {'k', 'c'}};
         StringBuilder b = new StringBuilder();
         String actual = EditDistance.getOps(path, 1, 1, b);
         String expected = "cc";
@@ -113,8 +113,8 @@ public class EditDistanceTest {
     @Test
     public void opsDeleteCopy() {
         char[][] path = new char[][]{
-            {'r', 'c'},
-            {'k', 'd'}};
+                {'r', 'c'},
+                {'k', 'd'}};
         StringBuilder b = new StringBuilder();
         String actual = EditDistance.getOps(path, 1, 1, b);
         String expected = "dc";
@@ -124,8 +124,8 @@ public class EditDistanceTest {
     @Test
     public void opsReplaceCopy() {
         char[][] path = new char[][]{
-            {'c', 'c'},
-            {'k', 'r'}};
+                {'c', 'c'},
+                {'k', 'r'}};
         StringBuilder b = new StringBuilder();
         String actual = EditDistance.getOps(path, 1, 1, b);
         String expected = "rc";
@@ -135,9 +135,9 @@ public class EditDistanceTest {
     @Test
     public void opsTwiddleCopy() {
         char[][] path = new char[][]{
-            {'c', 'c', 'd'},
-            {'k', 'r', 'i'},
-            {'k', 'i', 't'}};
+                {'c', 'c', 'd'},
+                {'k', 'r', 'i'},
+                {'k', 'i', 't'}};
         StringBuilder b = new StringBuilder();
         String actual = EditDistance.getOps(path, 2, 2, b);
         String expected = "tc";
@@ -147,8 +147,8 @@ public class EditDistanceTest {
     @Test
     public void opsDoubleInsertKill() {
         char[][] path = new char[][]{
-            {'c', 'c'},
-            {'i', 'i'}};
+                {'c', 'c'},
+                {'i', 'i'}};
         StringBuilder b = new StringBuilder();
         String actual = EditDistance.getOps(path, 1, 1, b);
         String expected = "iik";
