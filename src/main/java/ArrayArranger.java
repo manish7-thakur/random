2aa3fe3f808c1b97f28a0a50471cde5b2eb044d3
Manish <manish.thakur@agoda.com>
2021-01-25 22:47:21 +0700

    extract swap method

--- src/main/java/ArrayArranger.java
+++ src/main/java/ArrayArranger.java
@@ -1,25 +1,27 @@
 
 /**
-* Arrange elements in such a way that in any pair of 3 the middle element greater than both left & right
-* 7,5,3,6,2,4 -> 5<=7>=3<=6>=2<=4
-*/
+ * Arrange elements in such a way that in any pair of 3 the middle element greater than both left & right
+ * 7,5,3,6,2,4 -> 5<=7>=3<=6>=2<=4
+ */
 public class ArrayArranger {
-	public static int[] arrange(int[] arr) {
-		for(int i = 0; i< arr.length -1; i++) {
-			if(i %2 != 0) {
-				if(arr[i] < arr[i+1]) {
-					int temp = arr[i];
-					arr[i] = arr[i+1];
-					arr[i+1] = temp;
-				}
-			} else {
-				if(arr[i] > arr[i+1]) {
-					int temp = arr[i];
-					arr[i] = arr[i+1];
-					arr[i+1] = temp;
-				}
-			}
-		}
-		return arr;
-	}
+    public static int[] arrange(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (i % 2 != 0) {
+                if (arr[i] < arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            } else {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
--- src/test/java/ArrayArrangerTest.java
+++ src/test/java/ArrayArrangerTest.java
@@ -1,55 +1,55 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ArrayArrangerTest {
-	@Test
-	public void arrangeSingleElem() {
-		int[] actual = ArrayArranger.arrange(new int[]{3});
-		int[] expected = new int[]{3};
-		Assert.assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void arrangeSingleElem() {
+        int[] actual = ArrayArranger.arrange(new int[]{3});
+        int[] expected = new int[]{3};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-	@Test
-	public void arrangeTwoElems() {
-		int[] actual = ArrayArranger.arrange(new int[]{3, 1});
-		int[] expected = new int[]{1, 3};
-		Assert.assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void arrangeTwoElems() {
+        int[] actual = ArrayArranger.arrange(new int[]{3, 1});
+        int[] expected = new int[]{1, 3};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-	@Test
-	public void arrangeThreeElems() {
-		int[] actual = ArrayArranger.arrange(new int[]{1, 2, 3});
-		int[] expected = new int[]{1, 3, 2};
-		Assert.assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void arrangeThreeElems() {
+        int[] actual = ArrayArranger.arrange(new int[]{1, 2, 3});
+        int[] expected = new int[]{1, 3, 2};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-	@Test
-	public void arrangeFourElems() {
-		int[] actual = ArrayArranger.arrange(new int[]{1, 3, 4, 2});
-		int[] expected = new int[]{1, 4, 2, 3};
-		Assert.assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void arrangeFourElems() {
+        int[] actual = ArrayArranger.arrange(new int[]{1, 3, 4, 2});
+        int[] expected = new int[]{1, 4, 2, 3};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-	@Test
-	public void arrangeFiveElems() {
-		int[] actual = ArrayArranger.arrange(new int[]{1, 5, 3, 4, 2});
-		int[] expected = new int[]{1, 5, 3, 4, 2};
-		Assert.assertArrayEquals(expected, actual);
-		actual = ArrayArranger.arrange(new int[]{1,2,3,4,5});
-		expected = new int[]{1, 3, 2, 5, 4};
-		Assert.assertArrayEquals(expected, actual);
-		actual = ArrayArranger.arrange(new int[]{5,4,3,2,1});
-		expected = new int[]{4, 5, 2, 3, 1};
-		Assert.assertArrayEquals(expected, actual);
-	}
+    @Test
+    public void arrangeFiveElems() {
+        int[] actual = ArrayArranger.arrange(new int[]{1, 5, 3, 4, 2});
+        int[] expected = new int[]{1, 5, 3, 4, 2};
+        Assert.assertArrayEquals(expected, actual);
+        actual = ArrayArranger.arrange(new int[]{1, 2, 3, 4, 5});
+        expected = new int[]{1, 3, 2, 5, 4};
+        Assert.assertArrayEquals(expected, actual);
+        actual = ArrayArranger.arrange(new int[]{5, 4, 3, 2, 1});
+        expected = new int[]{4, 5, 2, 3, 1};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-	@Test
-	public void arrangeSixElems() {
-		int[] actual = ArrayArranger.arrange(new int[]{1, 2, 3, 4, 5, 6});
-		int[] expected = new int[]{1, 3, 2, 5, 4, 6};
-		Assert.assertArrayEquals(expected, actual);
-		actual = ArrayArranger.arrange(new int[]{6, 5, 4, 3, 2, 1});
-		expected = new int[]{5, 6, 3, 4, 1, 2};
-		Assert.assertArrayEquals(expected, actual);
-	}
-
+    @Test
+    public void arrangeSixElems() {
+        int[] actual = ArrayArranger.arrange(new int[]{1, 2, 3, 4, 5, 6});
+        int[] expected = new int[]{1, 3, 2, 5, 4, 6};
+        Assert.assertArrayEquals(expected, actual);
+        actual = ArrayArranger.arrange(new int[]{6, 5, 4, 3, 2, 1});
+        expected = new int[]{5, 6, 3, 4, 1, 2};
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
