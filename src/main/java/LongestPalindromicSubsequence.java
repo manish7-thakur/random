e324baa7bc48de92d4e4991fa7ab9f269ee46a78
Manish <manish.thakur@agoda.com>
2021-01-27 22:07:49 +0700

    optimize code & specs

--- src/main/java/LongestPalindromicSubsequence.java
+++ src/main/java/LongestPalindromicSubsequence.java
@@ -1,19 +1,20 @@
 public class LongestPalindromicSubsequence {
-	public static int find(String s, int i) {
-		return findNow(s, 0, i);
-	}
-	static int findNow(String s, int i, int j) {
-		if(i == j) {
-			return 1;
-		}
-		if(i > j) {
-			return 0;
-		}
-		int k = i ;
-		for(; k < j && s.charAt(k) != s.charAt(j); k++);
-			if(k == j) {
-				return findNow(s, i, j-1);
-			}
-			return Math.max(2 + findNow(s, k+1, j-1), findNow(s, i, j-1));
-	}
+    public static int find(String s, int i) {
+        return findNow(s, 0, i);
+    }
+
+    static int findNow(String s, int i, int j) {
+        if (i == j) {
+            return 1;
+        }
+        if (i > j) {
+            return 0;
+        }
+        int k = i;
+        for (; k < j && s.charAt(k) != s.charAt(j); k++) ;
+        if (k == j) {
+            return findNow(s, i, j - 1);
+        }
+        return Math.max(2 + findNow(s, k + 1, j - 1), findNow(s, i, j - 1));
+    }
 }
--- src/test/java/LongestPalindromicSubsequenceTest.java
+++ src/test/java/LongestPalindromicSubsequenceTest.java
@@ -1,98 +1,85 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class LongestPalindromicSubsequenceTest {
-	@Test
-	public void singleCharString() {
-		String s = "a";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 1;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void singleCharString() {
+        String s = "a";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void doubleCharStringPalindrome() {
-		String s = "aa";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void doubleCharStringPalindrome() {
+        String s = "aa";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void doubleCharStringNonpalindrome() {
-		String s = "ab";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 1;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void doubleCharStringNonpalindrome() {
+        String s = "ab";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void tripleCharStringNonpalindrome() {
-		String s = "abc";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 1;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void tripleCharStringLast2CharPalindrome() {
+        String s = "baa";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+        s = "abac";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+        s = "aab";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+        s = "aba";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+        s = "abc";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void tripleCharStringPalindrome() {
-		String s = "aba";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 3;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void fourCharString() {
+        String s = "bcac";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 3;
+        Assert.assertEquals(expected, actual);
+        s = "abcd";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void tripleCharStringFirst2CharPalindrome() {
-		String s = "aab";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void fiveCharString() {
+        String s = "abcda";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 3;
+        Assert.assertEquals(expected, actual);
 
-	@Test
-	public void tripleCharStringLast2CharPalindrome() {
-		String s = "baa";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+        s = "abcba";
+        actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        expected = 5;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void fourCharStringfirst3CharPalindrome() {
-		String s = "abac";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 3;
-		Assert.assertEquals(expected, actual);
-	}
-
-	@Test
-	public void fourCharStringLast3CharPalindrome() {
-		String s = "bcac";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 3;
-		Assert.assertEquals(expected, actual);
-	}
-
-	@Test
-	public void fourCharStringNoPalindrome() {
-		String s = "abcd";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 1;
-		Assert.assertEquals(expected, actual);
-	}
-
-
-	@Test
-	public void fiveCharString() {
-		String s = "abcda";
-		int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		int expected = 3;
-		Assert.assertEquals(expected, actual);
-
-		s = "abcba";
-		 actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
-		 expected = 5;
-		Assert.assertEquals(expected, actual);
-	}
-
+    @Test
+    public void multiCharString() {
+        String s = "character";
+        int actual = LongestPalindromicSubsequence.find(s, s.length() - 1);
+        int expected = 5;
+        Assert.assertEquals(expected, actual);
+    }
 }
