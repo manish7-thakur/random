0fd1755267e766ecd707a6aaad1ffdd2be00e1fa
Manish <manish.thakur@agoda.com>
2021-01-31 12:52:16 +0700

    build rod cutting soln

--- src/main/java/RodCutter.java
+++ src/main/java/RodCutter.java
@@ -1,12 +1,42 @@
+import java.util.*;
+
 public class RodCutter {
     public static int maxProfit(int[] prices, int n) {
-        return findProfit(prices, n, 1);
+    	char[][] path = new char[n+1][n+1];
+        return findProfit(prices, n, 1, path);
     }
 
-    static int findProfit(int[] prices, int n, int i) {
+    static int findProfit(int[] prices, int n, int i, char[][] path) {
         if (n == 0 || i > n) {
             return 0;
         }
-        return Math.max(prices[i] + findProfit(prices, n - i, i), findProfit(prices, n, i + 1));
+        int v1 = prices[i] + findProfit(prices, n - i, i, path);
+        int v2 = findProfit(prices, n, i + 1, path);
+        if(v1 > v2) {
+        	path[n][i] = 'c'; //cut here
+        	return v1;
+        } else {
+        	path[n][i] = 'n'; //no cut
+        	return v2;
+        }
+    }
+
+    static ArrayList<Integer> buildSoln(char[][] path, ArrayList<Integer> soln, int n, int i) {
+    	if(n == 0 || i > n) {
+    		return soln;
+    	}
+    	if(path[n][i] == 'c') {
+    		soln.add(i);
+    		return buildSoln(path, soln, n-i, i);
+    	} else {
+    		return buildSoln(path, soln, n, i+1);
+    	}
+    }
+
+    public static ArrayList<Integer> maxProfitSoln(int[] prices, int n) {
+    	char[][] path = new char[n+1][n+1];
+    	findProfit(prices, n, 1, path);
+    	ArrayList soln = new ArrayList<Integer>();
+    	return buildSoln(path, soln, n, 1);
     }
 }	
--- src/test/java/RodCutterTest.java
+++ src/test/java/RodCutterTest.java
@@ -1,11 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.*;
 
 public class RodCutterTest {
     @Test
     public void zeroLengthRod() {
         int actual = RodCutter.maxProfit(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 0);
         int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zeroLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 0);
+        ArrayList expected = new ArrayList();
         Assert.assertEquals(expected, actual);
     }
 
@@ -17,6 +25,14 @@ public class RodCutterTest {
     }
 
     @Test
+    public void oneLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 1);
+        ArrayList expected = new ArrayList();
+        expected.add(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void twoLengthRod() {
         int actual = RodCutter.maxProfit(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 2);
         int expected = 5;
@@ -24,9 +40,26 @@ public class RodCutterTest {
     }
 
     @Test
+    public void twoLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 2);
+        ArrayList expected = new ArrayList();
+        expected.add(2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void threeLengthRod() {
         int actual = RodCutter.maxProfit(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 3);
         int expected = 8;
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void threeLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 3);
+        ArrayList expected = new ArrayList();
+        expected.add(3);
         Assert.assertEquals(expected, actual);
     }
 
@@ -38,6 +71,15 @@ public class RodCutterTest {
     }
 
     @Test
+    public void fourLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 4);
+        ArrayList expected = new ArrayList();
+        expected.add(2);
+        expected.add(2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void tenLengthRod() {
         int actual = RodCutter.maxProfit(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 10);
         int expected = 30;
@@ -45,9 +87,26 @@ public class RodCutterTest {
     }
 
     @Test
+    public void tenLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30}, 10);
+        ArrayList expected = new ArrayList();
+        expected.add(10);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void thirteenLengthRod() {
         int actual = RodCutter.maxProfit(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 31, 32, 33}, 13);
         int expected = 38;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void thirteenLengthRodSoln() {
+        ArrayList actual = RodCutter.maxProfitSoln(new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 31, 32, 33}, 13);
+        ArrayList expected = new ArrayList();
+        expected.add(3);
+        expected.add(10);
         Assert.assertEquals(expected, actual);
     }
 }
