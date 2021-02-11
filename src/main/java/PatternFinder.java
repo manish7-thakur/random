6cd9fe0939776332bd600568e688ccf86d625e36
Manish <manish.thakur@agoda.com>
2021-02-11 22:08:09 +0700

    store pat first char in a var

--- src/main/java/PatternFinder.java
+++ src/main/java/PatternFinder.java
@@ -5,18 +5,19 @@ public class PatternFinder {
 	static int find(String text, String pat) {
 		char patFirstChar = pat.charAt(0);
 		for(int i = 0; i < text.length() - pat.length() + 1; i++) {
-			if(patFirstChar == text.charAt(i)) {
 				int j = i;
 				int k = 0;
-				while(k < pat.length() && pat.charAt(k) == text.charAt(j)) {
+			while(k < pat.length() && pat.charAt(k) == text.charAt(j)) {
+
+				//while(k < pat.length() && pat.charAt(k) == text.charAt(j)) {
 					k++;
 					j++;
+				//}
 				}
 				if(k >= pat.length()) {
 					return i;
 				}
 				i += k-1;
-			}
 		}
 		return -1;
 	}
