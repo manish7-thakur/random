ddfa5551f37e137d6cec2901fa45fde5070b1c05
Manish <manish.thakur@agoda.com>
2021-02-11 22:38:30 +0700

    format code

--- src/main/java/PatternFinder.java
+++ src/main/java/PatternFinder.java
@@ -1,19 +1,19 @@
 public class PatternFinder {
-	/**
-	* Find the first occ of pattern in the given text, all characters in the pattern must be unique
-	*/
-	static int find(String text, String pat) {
-		char patFirstChar = pat.charAt(0);
-		for(int i = 0; i < text.length() - pat.length() + 1; i++) {
-			if(patFirstChar == text.charAt(i)) {
-				int k = 0;
-				for(k = 0; k < pat.length() && pat.charAt(k) == text.charAt(i+k); k++);
-				if(k >= pat.length()) {
-					return i;
-				}
-				i += k-1;
-			}
-		}
-		return -1;
-	}
+    /**
+     * Find the first occ of pattern in the given text, all characters in the pattern must be unique
+     */
+    static int find(String text, String pat) {
+        char patFirstChar = pat.charAt(0);
+        for (int i = 0; i < text.length() - pat.length() + 1; i++) {
+            if (patFirstChar == text.charAt(i)) {
+                int k;
+                for (k = 0; k < pat.length() && pat.charAt(k) == text.charAt(i + k); k++) ;
+                if (k >= pat.length()) {
+                    return i;
+                }
+                i += k - 1;
+            }
+        }
+        return -1;
+    }
 }
--- src/test/java/PatternFinderTest.java
+++ src/test/java/PatternFinderTest.java
@@ -1,85 +1,88 @@
 import org.junit.*;
 
 public class PatternFinderTest {
-	@Test
-	public void findPatternEmptyText() {
-		int actual = PatternFinder.find("", "a");
-		int expected = -1;
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void findPatternSingleCharText() {
-		int actual = PatternFinder.find("a", "a");
-		int expected = 0;
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void findPatternDoubleCharText() {
-		int actual = PatternFinder.find("ab", "b");
-		int expected = 1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("ab", "ab");
-		expected = 0;
-		Assert.assertEquals(expected, actual);
-	}
-	@Test
-	public void findPatternTripleCharText() {
-		int actual = PatternFinder.find("abc", "c");
-		int expected = 2;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("cab", "ab");
-		expected = 1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("abc", "ab");
-		expected = 0;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("abc", "ac");
-		expected = -1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("aac", "ac");
-		expected = 1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("aaa", "ac");
-		expected = -1;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void findPatternEmptyText() {
+        int actual = PatternFinder.find("", "a");
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
 
-		@Test
-	public void findPatternFourCharText() {
-		int actual = PatternFinder.find("abcd", "d");
-		int expected = 3;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("cdab", "ab");
-		expected = 2;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("abdc", "acb");
-		expected = -1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("adbc", "db");
-		expected = 1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("adabc", "db");
-		expected = -1;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("dddbc", "db");
-		expected = 2;
-		Assert.assertEquals(expected, actual);
-		actual = PatternFinder.find("dbdbc", "dbc");
-		expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void findPatternSingleCharText() {
+        int actual = PatternFinder.find("a", "a");
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void findPatternFiveCharText() {
-		int actual = PatternFinder.find("ababe", "abe");
-		int expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void findPatternDoubleCharText() {
+        int actual = PatternFinder.find("ab", "b");
+        int expected = 1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("ab", "ab");
+        expected = 0;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void findPatternMultiCharText() {
-		int actual = PatternFinder.find("abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392j", "abd@htr(kyus%#392j");
-		int expected = 340;
-		Assert.assertEquals(expected, actual);
-	}
+    @Test
+    public void findPatternTripleCharText() {
+        int actual = PatternFinder.find("abc", "c");
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("cab", "ab");
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("abc", "ab");
+        expected = 0;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("abc", "ac");
+        expected = -1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("aac", "ac");
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("aaa", "ac");
+        expected = -1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findPatternFourCharText() {
+        int actual = PatternFinder.find("abcd", "d");
+        int expected = 3;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("cdab", "ab");
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("abdc", "acb");
+        expected = -1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("adbc", "db");
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("adabc", "db");
+        expected = -1;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("dddbc", "db");
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+        actual = PatternFinder.find("dbdbc", "dbc");
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findPatternFiveCharText() {
+        int actual = PatternFinder.find("ababe", "abe");
+        int expected = 2;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findPatternMultiCharText() {
+        int actual = PatternFinder.find("abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392abd@htr(kyus%#392j", "abd@htr(kyus%#392j");
+        int expected = 340;
+        Assert.assertEquals(expected, actual);
+    }
 }
