bc4ed0fce23179777e4bc1e27024f8eac649b5bb
Manish <manish.thakur@agoda.com>
2021-02-11 22:06:53 +0700

    store pat first char in a var

--- src/main/java/PatternFinder.java
+++ src/main/java/PatternFinder.java
@@ -3,8 +3,9 @@ public class PatternFinder {
 	* Find the first occ of pattern in the given text, all characters in the pattern must be unique
 	*/
 	static int find(String text, String pat) {
+		char patFirstChar = pat.charAt(0);
 		for(int i = 0; i < text.length() - pat.length() + 1; i++) {
-			if(pat.charAt(0) == text.charAt(i)) {
+			if(patFirstChar == text.charAt(i)) {
 				int j = i;
 				int k = 0;
 				while(k < pat.length() && pat.charAt(k) == text.charAt(j)) {
