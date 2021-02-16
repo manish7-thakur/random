bdffcb6d56c343d6fa0d059232faebf61facb3af
Manish <manish.thakur@agoda.com>
2021-02-16 22:51:31 +0700

    euclid's coefficient

--- src/main/java/GCDCalculator.java
+++ src/main/java/GCDCalculator.java
@@ -1,8 +1,39 @@
 public class GCDCalculator {
+    static class Triplets {
+        private int d;
+        private int x;
+        private int y;
+
+        public Triplets(int d, int x, int y) {
+            this.d = d;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            Triplets o = (Triplets) other;
+            return o.d == this.d && o.x == this.x && o.y == this.y;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + d + "," + x + "," + y + "]";
+        }
+    }
+
     static int euclidCalc(int a, int b) {
         if (b == 0) {
             return a;
         }
         return euclidCalc(b, a % b);
+    }
+
+    static Triplets euclidCoefficients(int a, int b) {
+        if (b == 0) {
+            return new Triplets(a, 1, 0);
+        }
+        Triplets t = euclidCoefficients(b, a % b);
+        return new Triplets(t.d, t.y, t.x - (int) (Math.floor(a / b) * t.y));
     }
 }
--- src/test/java/GCDCalculatorTest.java
+++ src/test/java/GCDCalculatorTest.java
@@ -6,16 +6,33 @@ public class GCDCalculatorTest {
     public void gcdWithZero() {
         int actual = GCDCalculator.euclidCalc(28, 0);
         int expected = 28;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
         actual = GCDCalculator.euclidCalc(0, 23);
         expected = 23;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void gcdWithNonZero() {
         int actual = GCDCalculator.euclidCalc(24, 30);
         int expected = 6;
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void euclidCoefficientWithZero() {
+        GCDCalculator.Triplets actual = GCDCalculator.euclidCoefficients(28, 0);
+        GCDCalculator.Triplets expected = new GCDCalculator.Triplets(28, 1, 0);
+        Assert.assertEquals(expected, actual);
+        actual = GCDCalculator.euclidCoefficients(0, 28);
+        expected = new GCDCalculator.Triplets(28, 0, 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void euclidCoefficientWithNonZero() {
+        GCDCalculator.Triplets actual = GCDCalculator.euclidCoefficients(99, 78);
+        GCDCalculator.Triplets expected = new GCDCalculator.Triplets(3, -11, 14);
+        Assert.assertEquals(expected, actual);
     }
 }
