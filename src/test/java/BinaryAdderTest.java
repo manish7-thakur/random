d766a3c25964693e9d9a76f0be7e8a20256be505
Manish <manish.thakur@agoda.com>
2021-02-20 15:58:07 +0700

    binary adder for 2 bits

--- src/test/java/BinaryAdderTest.java
+++ src/test/java/BinaryAdderTest.java
@@ -1,52 +1,73 @@
-import java.util.*;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class BinaryAdderTest {
-	@Test 
-	public void addSingleBit() {
-		int[] a = new int[]{1};
-		int[] b = new int[]{1};
-		int[] actual = BinaryAdder.add(a, b);
-		int[] expected = new int[]{1, 0};
-		Assert.assertArrayEquals(expected, actual);
+    @Test
+    public void addSingleBit() {
+        int[] a = new int[]{1};
+        int[] b = new int[]{1};
+        int[] actual = BinaryAdder.add(a, b);
+        int[] expected = new int[]{1, 0};
+        Assert.assertArrayEquals(expected, actual);
 
-		a = new int[]{0};
-		b = new int[]{1};
-		actual = BinaryAdder.add(a, b);
-		expected = new int[]{0, 1};
-		Assert.assertArrayEquals(expected, actual);
-	}
+        a = new int[]{0};
+        b = new int[]{1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{0, 1};
+        Assert.assertArrayEquals(expected, actual);
+    }
 
-		@Test 
-	public void addDoubleBits() {
-		int[] a = new int[]{1, 0};
-		int[] b = new int[]{1, 0};
-		int[] actual = BinaryAdder.add(a, b);
-		int[] expected = new int[]{1, 0, 0};
-		Assert.assertArrayEquals(expected, actual);
+    @Test
+    public void addDoubleBits() {
+        int[] a = new int[]{1, 0};
+        int[] b = new int[]{1, 0};
+        int[] actual = BinaryAdder.add(a, b);
+        int[] expected = new int[]{1, 0, 0};
+        Assert.assertArrayEquals(expected, actual);
 
-		a = new int[]{1, 0};
-		b = new int[]{1, 1};
-		actual = BinaryAdder.add(a, b);
-		expected = new int[]{1, 0, 1};
-		Assert.assertArrayEquals(expected, actual);
+        a = new int[]{1, 0};
+        b = new int[]{1, 1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{1, 0, 1};
+        Assert.assertArrayEquals(expected, actual);
 
-		a = new int[]{1, 1};
-		b = new int[]{1, 1};
-		actual = BinaryAdder.add(a, b);
-		expected = new int[]{1, 1, 0};
-		Assert.assertArrayEquals(expected, actual);
+        a = new int[]{1, 1};
+        b = new int[]{1, 1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{1, 1, 0};
+        Assert.assertArrayEquals(expected, actual);
 
-		a = new int[]{0, 1};
-		b = new int[]{0, 1};
-		actual = BinaryAdder.add(a, b);
-		expected = new int[]{0, 1, 0};
-		Assert.assertArrayEquals(expected, actual);
+        a = new int[]{0, 1};
+        b = new int[]{0, 1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{0, 1, 0};
+        Assert.assertArrayEquals(expected, actual);
 
-		a = new int[]{0, 1};
-		b = new int[]{1, 0};
-		actual = BinaryAdder.add(a, b);
-		expected = new int[]{0, 1, 1};
-		Assert.assertArrayEquals(expected, actual);
-	}
+        a = new int[]{0, 1};
+        b = new int[]{1, 0};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{0, 1, 1};
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addTripleBits() {
+        int[] a = new int[]{1, 0, 1};
+        int[] b = new int[]{1, 0, 1};
+        int[] actual = BinaryAdder.add(a, b);
+        int[] expected = new int[]{1, 0, 1, 0};
+        Assert.assertArrayEquals(expected, actual);
+
+        a = new int[]{0, 1, 1};
+        b = new int[]{1, 0, 1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{1, 0, 0, 0};
+        Assert.assertArrayEquals(expected, actual);
+
+        a = new int[]{1, 1, 1};
+        b = new int[]{1, 1, 1};
+        actual = BinaryAdder.add(a, b);
+        expected = new int[]{1, 1, 1, 0};
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
