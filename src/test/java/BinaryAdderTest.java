3bfe7524eb055a85b54f5967f8d18d61279b23e6
Manish <manish.thakur@agoda.com>
2021-02-20 15:53:43 +0700

    bonary adder for 2 bits

--- src/test/java/BinaryAdderTest.java
+++ src/test/java/BinaryAdderTest.java
@@ -16,4 +16,37 @@ public class BinaryAdderTest {
 		expected = new int[]{0, 1};
 		Assert.assertArrayEquals(expected, actual);
 	}
+
+		@Test 
+	public void addDoubleBits() {
+		int[] a = new int[]{1, 0};
+		int[] b = new int[]{1, 0};
+		int[] actual = BinaryAdder.add(a, b);
+		int[] expected = new int[]{1, 0, 0};
+		Assert.assertArrayEquals(expected, actual);
+
+		a = new int[]{1, 0};
+		b = new int[]{1, 1};
+		actual = BinaryAdder.add(a, b);
+		expected = new int[]{1, 0, 1};
+		Assert.assertArrayEquals(expected, actual);
+
+		a = new int[]{1, 1};
+		b = new int[]{1, 1};
+		actual = BinaryAdder.add(a, b);
+		expected = new int[]{1, 1, 0};
+		Assert.assertArrayEquals(expected, actual);
+
+		a = new int[]{0, 1};
+		b = new int[]{0, 1};
+		actual = BinaryAdder.add(a, b);
+		expected = new int[]{0, 1, 0};
+		Assert.assertArrayEquals(expected, actual);
+
+		a = new int[]{0, 1};
+		b = new int[]{1, 0};
+		actual = BinaryAdder.add(a, b);
+		expected = new int[]{0, 1, 1};
+		Assert.assertArrayEquals(expected, actual);
+	}
 }
