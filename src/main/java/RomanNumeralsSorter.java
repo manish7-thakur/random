af057b57a3127bf833f0a4ddde4adfdbad3483f2
Manish <manish.thakur@agoda.com>
2021-02-21 17:14:03 +0700

    refactor methods

--- src/main/java/RomanNumeralsSorter.java
+++ src/main/java/RomanNumeralsSorter.java
@@ -1,9 +1,11 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+
 /**
-Sort names first alphabetically then by roman numeral suffix(separated by space), roman numerals range from 1 to 50 (I to L)
-*/
+ * Sort names first alphabetically then by roman numeral suffix(separated by space), roman numerals range from 1 to 50 (I to L)
+ */
 public class RomanNumeralsSorter {
-	    public static List<String> sortRoman(List<String> names) {
+    public static List<String> sortRoman(List<String> names) {
         names.sort(new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
@@ -17,44 +19,34 @@ public class RomanNumeralsSorter {
         String[] name1 = s1.split(" ");
         String[] name2 = s2.split(" ");
         int nameOrder = name1[0].compareTo(name2[0]);
-        if(nameOrder == 0) {
-            return compareRoman(name1[1], name2[1]);
+        if (nameOrder == 0) {
+            return getDecimalVal(name1[1]) - getDecimalVal(name2[1]);
         } else {
             return nameOrder;
         }
     }
 
-    private static int compareRoman(String s1, String s2) {
-        int val1 = 0, val2 = 0;
-        for(int i = 0; i< s1.length(); i++) {
+    private static int getDecimalVal(String s1) {
+        int val = 0;
+        for (int i = 0; i < s1.length(); i++) {
             int value = getValue(s1.charAt(i));
-            if(i > 0 && getValue(s1.charAt(i-1)) <  value) {
-                val1 = value - getValue(s1.charAt(i-1)) + val1 - getValue(s1.charAt(i-1));
+            if (i > 0 && getValue(s1.charAt(i - 1)) < value) {
+                val += value - 2 * getValue(s1.charAt(i - 1));
             } else {
-                val1 += value;
+                val += value;
             }
         }
-
-        for(int i = 0; i< s2.length(); i++) {
-            int value = getValue(s2.charAt(i));
-            if(i > 0 && getValue(s2.charAt(i-1)) <  value) {
-                val2 = value - getValue(s2.charAt(i-1)) + val2 - getValue(s2.charAt(i-1));
-            } else {
-                val2 += value;
-            }
-        }
-        
-        return val1 - val2;
+        return val;
     }
 
     private static int getValue(char s) {
-        if(s == 'L') {
+        if (s == 'L') {
             return 50;
-        } else if(s == 'X') {
+        } else if (s == 'X') {
             return 10;
-        }else if(s == 'I') {
+        } else if (s == 'I') {
             return 1;
-        } else if(s == 'V') {
+        } else if (s == 'V') {
             return 5;
         }
         return 0;
