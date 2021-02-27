629c7c1745bee22840095b3b751b84f7c812da02
Manish <manish.thakur@agoda.com>
2021-02-27 14:06:12 +0700

    min coin finder

---
+++ src/main/java/MinCoinFinder.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class MinCoinFinder {
+	    public static int CoinDeterminerRec(int num, int count) {
+        if (num <= 0) {
+            return count;
+        }
+        int coin11C = Integer.MAX_VALUE;
+        int coin9C = Integer.MAX_VALUE;
+        int coin7C = Integer.MAX_VALUE;
+        int coin5C = Integer.MAX_VALUE;
+        int coin1C = Integer.MAX_VALUE;
+        if (num >= 11) {
+            coin11C = CoinDeterminerRec(num - 11, count + 1);
+        }
+        if (num >= 9) {
+            coin9C = CoinDeterminerRec(num - 9, count + 1);
+        }
+        if (num >= 7) {
+            coin7C = CoinDeterminerRec(num - 7, count + 1);
+        }
+        if (num >= 5) {
+            coin5C = CoinDeterminerRec(num - 5, count + 1);
+        }
+        coin1C = CoinDeterminerRec(num - 1, count + 1);
+        return min(coin11C, coin9C, coin7C, coin5C, coin1C);
+    }
+
+    private static int min(int... values) {
+        Arrays.sort(values);
+        return values[0];
+    }
+}
---
+++ src/test/java/MinCoinFinderTest.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+
+public class MinCoinFinderTest {
+	@Test
+	public void findValue() {
+		int actual = MinCoinFinder.CoinDeterminerRec(1, 0);
+		int expected = 1;
+		Assert.assertEquals(expected, actual);
+
+		actual = MinCoinFinder.CoinDeterminerRec(5, 0);
+		expected = 1;
+		Assert.assertEquals(expected, actual );
+
+		actual = MinCoinFinder.CoinDeterminerRec(6, 0);
+		expected = 2;
+		Assert.assertEquals(expected,actual);
+
+		actual = MinCoinFinder.CoinDeterminerRec(11, 0);
+		expected = 1;
+		Assert.assertEquals(expected,actual);
+
+		actual = MinCoinFinder.CoinDeterminerRec(16, 0);
+		expected = 2;
+		Assert.assertEquals(expected, actual);
+
+		actual = MinCoinFinder.CoinDeterminerRec(25, 0);
+		expected = 3;
+		Assert.assertEquals(expected, actual);
+
+		actual = MinCoinFinder.CoinDeterminerRec(30, 0);
+		expected = 4;
+		Assert.assertEquals(expected, actual);
+	}
+}
