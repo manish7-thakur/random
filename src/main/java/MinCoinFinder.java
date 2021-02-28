989510eafc0740db703a88ee823576c6d5cd5f2e
Manish <manish.thakur@agoda.com>
2021-02-28 12:00:06 +0700

    use memoization for min coin finder

--- src/main/java/MinCoinFinder.java
+++ src/main/java/MinCoinFinder.java
@@ -1,7 +1,13 @@
 import java.util.Arrays;
 
 public class MinCoinFinder {
-    public static int CoinDeterminerRec(int num, int count) {
+
+    public static int minCoinFinder(int num) {
+        int[] mem = new int[num + 1];
+        return CoinDeterminerRec(num, 0, mem);
+    }
+
+    static int CoinDeterminerRec(int num, int count, int[] mem) {
         if (num <= 0) {
             return count;
         }
@@ -11,19 +17,30 @@ public class MinCoinFinder {
         int coin5C = Integer.MAX_VALUE;
         int coin1C = Integer.MAX_VALUE;
         if (num >= 11) {
-            coin11C = CoinDeterminerRec(num - 11, count + 1);
+            if (mem[num] == 0) {
+                coin11C = CoinDeterminerRec(num - 11, count + 1, mem);
+            }
         }
         if (num >= 9) {
-            coin9C = CoinDeterminerRec(num - 9, count + 1);
+            if (mem[num] == 0) {
+                coin9C = CoinDeterminerRec(num - 9, count + 1, mem);
+            }
         }
         if (num >= 7) {
-            coin7C = CoinDeterminerRec(num - 7, count + 1);
+            if (mem[num] == 0) {
+                coin7C = CoinDeterminerRec(num - 7, count + 1, mem);
+            }
         }
         if (num >= 5) {
-            coin5C = CoinDeterminerRec(num - 5, count + 1);
+            if (mem[num] == 0) {
+                coin5C = CoinDeterminerRec(num - 5, count + 1, mem);
+            }
         }
-        coin1C = CoinDeterminerRec(num - 1, count + 1);
-        return min(coin11C, coin9C, coin7C, coin5C, coin1C);
+        if (mem[num] == 0) {
+            coin1C = CoinDeterminerRec(num - 1, count + 1, mem);
+        }
+        mem[num] = min(coin11C, coin9C, coin7C, coin5C, coin1C);
+        return mem[num];
     }
 
     private static int min(int... values) {
--- src/test/java/MinCoinFinderTest.java
+++ src/test/java/MinCoinFinderTest.java
@@ -4,32 +4,36 @@ import org.junit.Test;
 public class MinCoinFinderTest {
     @Test
     public void findValue() {
-        int actual = MinCoinFinder.CoinDeterminerRec(1, 0);
+        int actual = MinCoinFinder.minCoinFinder(1);
         int expected = 1;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(5, 0);
+        actual = MinCoinFinder.minCoinFinder(5);
         expected = 1;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(6, 0);
+        actual = MinCoinFinder.minCoinFinder(6);
         expected = 2;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(11, 0);
+        actual = MinCoinFinder.minCoinFinder(11);
         expected = 1;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(16, 0);
+        actual = MinCoinFinder.minCoinFinder(16);
         expected = 2;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(25, 0);
+        actual = MinCoinFinder.minCoinFinder(25);
         expected = 3;
         Assert.assertEquals(expected, actual);
 
-        actual = MinCoinFinder.CoinDeterminerRec(30, 0);
+        actual = MinCoinFinder.minCoinFinder(30);
         expected = 4;
+        Assert.assertEquals(expected, actual);
+
+        actual = MinCoinFinder.minCoinFinder(55);
+        expected = 5;
         Assert.assertEquals(expected, actual);
     }
 }
