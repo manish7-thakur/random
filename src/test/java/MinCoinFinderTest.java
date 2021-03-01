8f9249b747b7d65dfd6992cec38da9fb434f67bb
Manish <manish.thakur@agoda.com>
2021-03-01 21:24:14 +0700

    format test file

--- src/test/java/MinCoinFinderTest.java
+++ src/test/java/MinCoinFinderTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.util.*;
 
 public class MinCoinFinderTest {
@@ -10,7 +11,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         Map<Integer, Integer> actualCoins = MinCoinFinder.minCoinFinderVal(1);
-        Map<Integer, Integer> expectedCoins = Map.of(1,1);
+        Map<Integer, Integer> expectedCoins = Map.of(1, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(2);
@@ -18,7 +19,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(2);
-        expectedCoins = Map.of(1,2);
+        expectedCoins = Map.of(1, 2);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(5);
@@ -26,7 +27,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(5);
-        expectedCoins = Map.of(5,1);
+        expectedCoins = Map.of(5, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(6);
@@ -34,7 +35,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(6);
-        expectedCoins = Map.of(5,1,1,1);
+        expectedCoins = Map.of(5, 1, 1, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(11);
@@ -42,7 +43,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(11);
-        expectedCoins = Map.of(11,1);
+        expectedCoins = Map.of(11, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(16);
@@ -50,7 +51,7 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(16);
-        expectedCoins = Map.of(11,1, 5,1);
+        expectedCoins = Map.of(11, 1, 5, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(25);
@@ -58,22 +59,23 @@ public class MinCoinFinderTest {
         Assert.assertEquals(expected, actual);
 
         actualCoins = MinCoinFinder.minCoinFinderVal(25);
-        expectedCoins = Map.of(11,1,9,1,5,1);
+        expectedCoins = Map.of(11, 1, 9, 1, 5, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(55);
         expected = 5;
         Assert.assertEquals(expected, actual);
+
         actualCoins = MinCoinFinder.minCoinFinderVal(55);
-        expectedCoins = Map.of(11,5);
+        expectedCoins = Map.of(11, 5);
         Assert.assertEquals(expectedCoins, actualCoins);
 
         actual = MinCoinFinder.minCoinFinder(65);
         expected = 7;
         Assert.assertEquals(expected, actual);
-        
+
         actualCoins = MinCoinFinder.minCoinFinderVal(65);
-        expectedCoins = Map.of(11, 5, 9,1,1,1);
+        expectedCoins = Map.of(11, 5, 9, 1, 1, 1);
         Assert.assertEquals(expectedCoins, actualCoins);
     }
 }
