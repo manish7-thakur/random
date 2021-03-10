13c097ba53c069cb68d0b39f38b4f42d0c93ce7d
Manish <manish.thakur@agoda.com>
2021-03-10 21:34:37 +0700

    max subarray sum finder

--- src/main/java/MaximumSubarrayFinder.java
+++ src/main/java/MaximumSubarrayFinder.java
@@ -1,38 +1,40 @@
 public class MaximumSubarrayFinder {
 
-	static int maxSubarraySum(int[] arr) {
-		if(arr.length == 1) {
-			return arr[0];
-		}
-		return maxSubarraySum(arr, 0, arr.length -1);
-	}
-	static int maxSubarraySum(int[] arr, int l, int h) {
-		if(l == h) {
-			return arr[l];
-		}
-		int m = (l+h)/2;
-		int left_sum = maxSubarraySum(arr, l , m);
-		int right_sum = maxSubarraySum(arr, m + 1, h);
-		int cross_sum = findMaxCrossOverSum(arr, l, m , h);
-		return Math.max(Math.max(left_sum, right_sum), cross_sum);
-	}
-	static int findMaxCrossOverSum(int[] arr, int l, int m, int h) {
-		int left_sum = Integer.MIN_VALUE;
-		int right_sum = Integer.MIN_VALUE;
-		int sum = 0;
-		for(int i = m; i >= l; i--) {
-			sum += arr[i];
-			if(sum > left_sum) {
-				left_sum = sum;
-			}
-		}
-		sum = 0;
-		for(int i = m+1; i <= h; i++) {
-			sum += arr[i];
-			if(sum > right_sum) {
-				right_sum = sum;
-			}
-		}
-		return Math.max(Math.max(left_sum, right_sum), left_sum+right_sum);
-	}
+    static int maxSubarraySum(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        return maxSubarraySum(arr, 0, arr.length - 1);
+    }
+
+    static int maxSubarraySum(int[] arr, int l, int h) {
+        if (l == h) {
+            return arr[l];
+        }
+        int m = (l + h) / 2;
+        int left_sum = maxSubarraySum(arr, l, m);
+        int right_sum = maxSubarraySum(arr, m + 1, h);
+        int cross_sum = maxCrossOverSum(arr, l, m, h);
+        return Math.max(Math.max(left_sum, right_sum), cross_sum);
+    }
+
+    static int maxCrossOverSum(int[] arr, int l, int m, int h) {
+        int left_sum = Integer.MIN_VALUE;
+        int right_sum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = m; i >= l; i--) {
+            sum += arr[i];
+            if (sum > left_sum) {
+                left_sum = sum;
+            }
+        }
+        sum = 0;
+        for (int i = m + 1; i <= h; i++) {
+            sum += arr[i];
+            if (sum > right_sum) {
+                right_sum = sum;
+            }
+        }
+        return Math.max(Math.max(left_sum, right_sum), left_sum + right_sum);
+    }
 }
--- src/test/java/MaximumSubarrayFinderTest.java
+++ src/test/java/MaximumSubarrayFinderTest.java
@@ -1,50 +1,78 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximumSubarrayFinderTest {
-	@Test
-	public void maxCrossOverSum() {
-		int actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{5}, 0,0,0);
-		int expected = 5;
-		Assert.assertEquals(expected, actual);
+    @Test
+    public void maxCrossOverSum() {
+        int actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{5}, 0, 0, 0);
+        int expected = 5;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{5, -1}, 0,0,1);
-		expected = 5;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{5, -1}, 0, 0, 1);
+        expected = 5;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{5, 6}, 0,1,1);
-		expected = 11;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{5, 6}, 0, 1, 1);
+        expected = 11;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{-5, 6}, 0,0,1);
-		expected = 6;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{-5, 6}, 0, 0, 1);
+        expected = 6;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{-5, 6}, 0,1,1);
-		expected = 6;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{-5, 6}, 0, 1, 1);
+        expected = 6;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{-5, 6, 7}, 0,1,2);
-		expected = 13;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{-5, 6, 7}, 0, 1, 2);
+        expected = 13;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{6, -2, 3}, 0,1,2);
-		expected = 7;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{6, -2, 3}, 0, 1, 2);
+        expected = 7;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{1,3, 6, -2, 3}, 0,2,4);
-		expected = 11;
-		Assert.assertEquals(expected, actual);
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{1, 3, 6, -2, 3}, 0, 2, 4);
+        expected = 11;
+        Assert.assertEquals(expected, actual);
 
-		actual = MaximumSubarrayFinder.findMaxCrossOverSum(new int[]{1,3, -6, -2, 3}, 0,2,4);
-		expected = 1;
-		Assert.assertEquals(expected, actual);
-	}
+        actual = MaximumSubarrayFinder.maxCrossOverSum(new int[]{1, 3, -6, -2, 3}, 0, 2, 4);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void maxSubarraySum() {
-		int actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{4});
-		int expected = 5;
-		Assert.assertEquals(expected, actual);
+    @Test
+    public void maxSubarraySum() {
+        int actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{4});
+        int expected = 4;
+        Assert.assertEquals(expected, actual);
 
-	}
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{4, 5});
+        expected = 9;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{4, -2, 5});
+        expected = 7;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{-1, -2, 2});
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{3, -2, 2, 5});
+        expected = 8;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{3, -2, 2, -5});
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{3, -2, 1, 2, -5});
+        expected = 4;
+        Assert.assertEquals(expected, actual);
+
+        actual = MaximumSubarrayFinder.maxSubarraySum(new int[]{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7});
+        expected = 43;
+        Assert.assertEquals(expected, actual);
+    }
 }
