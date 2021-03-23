54616d62067b24ad6455eff01d07b3672a59e50f
Manish <manish.thakur@agoda.com>
2021-03-23 22:54:47 +0700

    format code

--- src/main/java/MinMaxFinder.java
+++ src/main/java/MinMaxFinder.java
@@ -1,71 +1,75 @@
 public class MinMaxFinder {
-	static class Pair {
-		int min;
-		int max;
-		public Pair(int min, int max){
-			this.min = min;
-			this.max = max;
-		}
-		@Override
-		public boolean equals(Object other) {
-			Pair p = (Pair) other;
-			return this.min == p.min && this.max == p.max;
-		}
-		@Override
-		public String toString() {
-			return "["+this.min+","+this.max+"]";
-		}
-	}
-	static Pair find(int[] arr) {
-		int min;
-		int max;
-		if(arr.length%2 == 0) {
-			if(arr[0] < arr[1]) {
-				min = arr[0];
-				max = arr[1];
-			} else {
-				min = arr[1];
-				max = arr[0];
-			}
-			for(int i = 2 ; i < arr.length; i+=2) {
-				if(arr[i] < arr[i+1]) {
-					if(arr[i] < min) {
-						min = arr[i];
-					}
-					if(arr[i+1] > max) {
-						max = arr[i+1];
-					}
-				} else {
-					if(arr[i+1] < min) {
-						min = arr[i+1];
-					}
-					if(arr[i] > max) {
-						max = arr[i];
-					}
-				}
-			}
-		} else {
-			min = arr[0];
-			max = arr[0];
-			for(int i = 1 ; i < arr.length; i+=2) {
-				if(arr[i] < arr[i+1]) {
-					if(arr[i] < min) {
-						min = arr[i];
-					}
-					if(arr[i+1] > max) {
-						max = arr[i+1];
-					}
-				} else {
-					if(arr[i+1] < min) {
-						min = arr[i+1];
-					}
-					if(arr[i] > max) {
-						max = arr[i];
-					}
-				}
-			}
-		}
+    static class Pair {
+        int min;
+        int max;
 
-		return new Pair(min, max);
-	}
+        public Pair(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            Pair p = (Pair) other;
+            return this.min == p.min && this.max == p.max;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.min + "," + this.max + "]";
+        }
+    }
+
+    static Pair find(int[] arr) {
+        int min;
+        int max;
+        if (arr.length % 2 == 0) {
+            if (arr[0] < arr[1]) {
+                min = arr[0];
+                max = arr[1];
+            } else {
+                min = arr[1];
+                max = arr[0];
+            }
+            for (int i = 2; i < arr.length; i += 2) {
+                if (arr[i] < arr[i + 1]) {
+                    if (arr[i] < min) {
+                        min = arr[i];
+                    }
+                    if (arr[i + 1] > max) {
+                        max = arr[i + 1];
+                    }
+                } else {
+                    if (arr[i + 1] < min) {
+                        min = arr[i + 1];
+                    }
+                    if (arr[i] > max) {
+                        max = arr[i];
+                    }
+                }
+            }
+        } else {
+            min = arr[0];
+            max = arr[0];
+            for (int i = 1; i < arr.length; i += 2) {
+                if (arr[i] < arr[i + 1]) {
+                    if (arr[i] < min) {
+                        min = arr[i];
+                    }
+                    if (arr[i + 1] > max) {
+                        max = arr[i + 1];
+                    }
+                } else {
+                    if (arr[i + 1] < min) {
+                        min = arr[i + 1];
+                    }
+                    if (arr[i] > max) {
+                        max = arr[i];
+                    }
+                }
+            }
+        }
+
+        return new Pair(min, max);
+    }
 }
--- src/test/java/MinMaxFinderTest.java
+++ src/test/java/MinMaxFinderTest.java
@@ -1,46 +1,47 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MinMaxFinderTest {
-	@Test
-	public void findMinMax() {
-		MinMaxFinder.Pair actual = MinMaxFinder.find(new int[]{1});
-		MinMaxFinder.Pair expected = new MinMaxFinder.Pair(1,1);
-		Assert.assertEquals(expected, actual);
+    @Test
+    public void findMinMax() {
+        MinMaxFinder.Pair actual = MinMaxFinder.find(new int[]{1});
+        MinMaxFinder.Pair expected = new MinMaxFinder.Pair(1, 1);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{1, 2});
-		expected = new MinMaxFinder.Pair(1,2);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{1, 2});
+        expected = new MinMaxFinder.Pair(1, 2);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{2, 1});
-		expected = new MinMaxFinder.Pair(1,2);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{2, 1});
+        expected = new MinMaxFinder.Pair(1, 2);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{2, 3, 1});
-		expected = new MinMaxFinder.Pair(1,3);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{2, 3, 1});
+        expected = new MinMaxFinder.Pair(1, 3);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{1, 2, 3});
-		expected = new MinMaxFinder.Pair(1,3);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{1, 2, 3});
+        expected = new MinMaxFinder.Pair(1, 3);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{1, 2, 3, 4});
-		expected = new MinMaxFinder.Pair(1,4);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{1, 2, 3, 4});
+        expected = new MinMaxFinder.Pair(1, 4);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{4, 1, 2, 2});
-		expected = new MinMaxFinder.Pair(1,4);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{4, 1, 2, 2});
+        expected = new MinMaxFinder.Pair(1, 4);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{4, 1, 2, 5, 6});
-		expected = new MinMaxFinder.Pair(1,6);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{4, 1, 2, 5, 6});
+        expected = new MinMaxFinder.Pair(1, 6);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{4, -1, 2, 5, 6});
-		expected = new MinMaxFinder.Pair(-1,6);
-		Assert.assertEquals(expected, actual);
+        actual = MinMaxFinder.find(new int[]{4, -1, 2, 5, 6});
+        expected = new MinMaxFinder.Pair(-1, 6);
+        Assert.assertEquals(expected, actual);
 
-		actual = MinMaxFinder.find(new int[]{4, -1, 2, 5, -23,83,274,925,34,37,-26, 6});
-		expected = new MinMaxFinder.Pair(-26,925);
-		Assert.assertEquals(expected, actual);
-	}
+        actual = MinMaxFinder.find(new int[]{4, -1, 2, 5, -23, 83, 274, 925, 34, 37, -26, 6});
+        expected = new MinMaxFinder.Pair(-26, 925);
+        Assert.assertEquals(expected, actual);
+    }
 }
