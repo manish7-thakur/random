81c726f8f8a95b389711dfc99928868039bcf499
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-04-06 11:28:09 +0700

    quicksort with bigger array and format code

--- src/main/java/Quicksort.java
+++ src/main/java/Quicksort.java
@@ -1,28 +1,28 @@
 public class Quicksort {
-	static void sort(int[] arr, int l, int h) {
-		if(l >= h) {
-			return;
-		}
-		int p = partition(arr);
-		sort(arr, l, p - 1);
-		sort(arr, p + 1, h);
-	}
+    static void sort(int[] arr, int l, int h) {
+        if (l >= h) {
+            return;
+        }
+        int p = partition(arr, l, h);
+        sort(arr, l, p - 1);
+        sort(arr, p + 1, h);
+    }
 
-	static int partition(int[] arr) {
-		int pivot = arr[0];
-		int pos = 0;
-		for(int i = 1; i < arr.length; i++) {
-			if(arr[i] <= pivot) {
-				pos++;
-				int temp = arr[pos];
-				arr[pos] = arr[i];
-				arr[i] = temp;
-			}
-		}
+    static int partition(int[] arr, int l, int h) {
+        int pivot = arr[l];
+        int pos = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= pivot) {
+                pos++;
+                int temp = arr[pos];
+                arr[pos] = arr[i];
+                arr[i] = temp;
+            }
+        }
 
-		int temp = arr[pos];
-		arr[pos] = pivot;
-		arr[0] = temp;
-		return pos;
-	}
+        int temp = arr[pos];
+        arr[pos] = pivot;
+        arr[l] = temp;
+        return pos;
+    }
 }
--- src/test/java/QuickSortTest.java
+++ src/test/java/QuickSortTest.java
@@ -1,53 +1,79 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class QuickSortTest {
-	@Test
-	public void partitonArray() {
-		int actual = Quicksort.partition(new int[]{1});
-		int expected = 0;
-		Assert.assertEquals(expected, actual);
+    @Test
+    public void partitonArray() {
+        int actual = Quicksort.partition(new int[]{1}, 0, 0);
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
 
-		actual = Quicksort.partition(new int[]{2, 1});
-		expected = 1;
-		Assert.assertEquals(expected, actual);
+        actual = Quicksort.partition(new int[]{2, 1}, 0, 1);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
 
-		actual = Quicksort.partition(new int[]{3, 2, 1});
-		expected = 2;
-		Assert.assertEquals(expected, actual);
+        actual = Quicksort.partition(new int[]{3, 2, 1}, 0, 2);
+        expected = 2;
+        Assert.assertEquals(expected, actual);
 
-		actual = Quicksort.partition(new int[]{1, 2, 3});
-		expected = 0;
-		Assert.assertEquals(expected, actual);
+        actual = Quicksort.partition(new int[]{1, 2, 3}, 0, 2);
+        expected = 0;
+        Assert.assertEquals(expected, actual);
 
-		actual = Quicksort.partition(new int[]{2, 1, 3});
-		expected = 1;
-		Assert.assertEquals(expected, actual);
+        actual = Quicksort.partition(new int[]{2, 1, 3}, 0, 2);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
 
-		actual = Quicksort.partition(new int[]{3, 2, 1, 4});
-		expected = 2;
-		Assert.assertEquals(expected, actual);
-	}
+        actual = Quicksort.partition(new int[]{3, 2, 1, 4}, 0, 3);
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+    }
 
-	@Test
-	public void sortArray() {
-		int[] actual = new int[]{1};
-		Quicksort.sort(actual, 0, 0);
-		int[] expected = new int[]{1};
-		Assert.assertArrayEquals(expected, actual);
+    @Test
+    public void sortArray() {
+        int[] actual = new int[]{1};
+        Quicksort.sort(actual, 0, 0);
+        int[] expected = new int[]{1};
+        Assert.assertArrayEquals(expected, actual);
 
-		actual = new int[]{2, 1};
-		Quicksort.sort(actual, 0, 1);
-		expected = new int[]{1, 2};
-		Assert.assertArrayEquals(expected, actual);
+        actual = new int[]{2, 1};
+        Quicksort.sort(actual, 0, 1);
+        expected = new int[]{1, 2};
+        Assert.assertArrayEquals(expected, actual);
 
-		actual = new int[]{2, 1, 3};
-		Quicksort.sort(actual, 0, 2);
-		expected = new int[]{1, 2, 3};
-		Assert.assertArrayEquals(expected, actual);
+        actual = new int[]{2, 1, 3};
+        Quicksort.sort(actual, 0, 2);
+        expected = new int[]{1, 2, 3};
+        Assert.assertArrayEquals(expected, actual);
 
-		actual = new int[]{4, 2, 1, 3};
-		Quicksort.sort(actual, 0, 3);
-		expected = new int[]{1, 2, 3, 4};
-		Assert.assertArrayEquals(expected, actual);
-	}
+        actual = new int[]{4, 2, 1, 3};
+        Quicksort.sort(actual, 0, 3);
+        expected = new int[]{1, 2, 3, 4};
+        Assert.assertArrayEquals(expected, actual);
+
+        actual = new int[]{4, 2, 1, 3, 5};
+        Quicksort.sort(actual, 0, 4);
+        expected = new int[]{1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, actual);
+
+        actual = new int[]{4, 2, 1, 6, 3, 5};
+        Quicksort.sort(actual, 0, 5);
+        expected = new int[]{1, 2, 3, 4, 5, 6};
+        Assert.assertArrayEquals(expected, actual);
+
+        actual = new int[]{4, 3, 1, 6, 3, 5};
+        Quicksort.sort(actual, 0, 5);
+        expected = new int[]{1, 3, 3, 4, 5, 6};
+        Assert.assertArrayEquals(expected, actual);
+
+        actual = new int[]{4, 3, 1, 6, 3, 6};
+        Quicksort.sort(actual, 0, 5);
+        expected = new int[]{1, 3, 3, 4, 6, 6};
+        Assert.assertArrayEquals(expected, actual);
+
+        actual = new int[]{438, 74, 93, 33, 841, 64, 3, 4, 294, 272, 674};
+        Quicksort.sort(actual, 0, 10);
+        expected = new int[]{3, 4, 33, 64, 74, 93, 272, 294, 438, 674, 841};
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
