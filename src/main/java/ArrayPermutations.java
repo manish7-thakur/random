cc437b40a8e9db505236449b4a29cf422cc8b142
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-04-25 12:41:40 +0700

    format code

--- src/main/java/ArrayPermutations.java
+++ src/main/java/ArrayPermutations.java
@@ -1,22 +1,22 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class ArrayPermutations {
-	static ArrayList<String> permutate(char[] arr, int l) {
-		ArrayList<String> permutations = new ArrayList<>();
-		return find(arr, new char[l], 0, 0, l, permutations);
-	}
+    static ArrayList<String> permutate(char[] arr, int l) {
+        ArrayList<String> permutations = new ArrayList<>();
+        find(arr, new char[l], 0, 0, l, permutations);
+        return permutations;
+    }
 
-	static ArrayList<String> find(char[] arr, char[] per, int i, int p, int l, ArrayList<String> permutations) {
-		if(p >=l) {
-			permutations.add(new String(per));
-			return permutations;
-		}
-		if(i >= arr.length) {
-			return permutations;
-		}
-		per[p] = arr[i];
-		find(arr, per, i + 1, p+1, l, permutations);
-		find(arr, per, i + 1, p, l, permutations);
-		return permutations;
-	}
+    static void find(char[] arr, char[] per, int i, int p, int l, ArrayList<String> permutations) {
+        if (p >= l) {
+            permutations.add(new String(per));
+            return;
+        }
+        if (i >= arr.length) {
+            return;
+        }
+        per[p] = arr[i];
+        find(arr, per, i + 1, p + 1, l, permutations);
+        find(arr, per, i + 1, p, l, permutations);
+    }
 }
--- src/test/java/ArrayPermutationsTest.java
+++ src/test/java/ArrayPermutationsTest.java
@@ -1,26 +1,31 @@
-import org.junit.*;
-import java.util.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayPermutationsTest {
-	@Test
-	public void generatePermutations() {
-		ArrayList<String> actual = ArrayPermutations.permutate(new char[]{'a','b'}, 2);
-		List<String> expected = Arrays.asList("ab");
-		Assert.assertEquals(expected, actual);
-		
-		actual = ArrayPermutations.permutate(new char[]{'a','b', 'c'}, 2);
-		expected = Arrays.asList("ab", "ac", "bc");
-		Assert.assertEquals(expected, actual);
+    @Test
+    public void generatePermutations() {
+        ArrayList<String> actual = ArrayPermutations.permutate(new char[]{'a', 'b'}, 2);
+        List<String> expected = Arrays.asList("ab");
+        Assert.assertEquals(expected, actual);
 
-		actual = ArrayPermutations.permutate(new char[]{'a','b', 'c'}, 3);
-		expected = Arrays.asList("abc");
-		Assert.assertEquals(expected, actual);
+        actual = ArrayPermutations.permutate(new char[]{'a', 'b', 'c'}, 2);
+        expected = Arrays.asList("ab", "ac", "bc");
+        Assert.assertEquals(expected, actual);
 
-		actual = ArrayPermutations.permutate(new char[]{'a','b', 'c'}, 1);
-		expected = Arrays.asList("a", "b", "c");
-		Assert.assertEquals(expected, actual);
+        actual = ArrayPermutations.permutate(new char[]{'a', 'b', 'c'}, 3);
+        expected = Arrays.asList("abc");
+        Assert.assertEquals(expected, actual);
 
-		actual = ArrayPermutations.permutate(new char[]{'a','b', 'c', 'd'}, 3);
-		expected = Arrays.asList("abc", "abd", "acd", "bcd");
-		Assert.assertEquals(expected, actual);
-	}
+        actual = ArrayPermutations.permutate(new char[]{'a', 'b', 'c'}, 1);
+        expected = Arrays.asList("a", "b", "c");
+        Assert.assertEquals(expected, actual);
+
+        actual = ArrayPermutations.permutate(new char[]{'a', 'b', 'c', 'd'}, 3);
+        expected = Arrays.asList("abc", "abd", "acd", "bcd");
+        Assert.assertEquals(expected, actual);
+    }
 }
