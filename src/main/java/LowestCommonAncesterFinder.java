300f88bf47bbe4bda92e11e5087fbc732f50ccc0
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-05-01 12:28:05 +0700

    LCA finder for binary tree more test cases

--- src/main/java/LowestCommonAncesterFinder.java
+++ src/main/java/LowestCommonAncesterFinder.java
@@ -2,11 +2,9 @@ import java.util.ArrayList;
 
 public class LowestCommonAncesterFinder {
     static int forBST(TreeNode curr, int a, int b) {
-        //TreeNode traverser = curr;
         ArrayList<Integer> ancestersA = new ArrayList<>();
         if (findAncestors(curr, ancestersA, a)) return -1;
         ArrayList<Integer> ancestersB = new ArrayList<>();
-        //traverser = curr;
         if (findAncestors(curr, ancestersB, b)) return -1;
         int i = ancestersA.size() - 1;
         int j = ancestersB.size() - 1;
@@ -34,5 +32,38 @@ public class LowestCommonAncesterFinder {
             return true;
         }
         return false;
+    }
+
+
+    private static boolean findBTAncestors(TreeNode traverser, ArrayList<Integer> ancesters, int value) {
+        if (traverser == null) {
+            return false;
+        }
+        if (traverser.value == value) {
+            return true;
+        }
+        ancesters.add(traverser.value);
+        if (findBTAncestors(traverser.left, ancesters, value) || findBTAncestors(traverser.right, ancesters, value))
+            return true;
+        ancesters.remove(ancesters.size() - 1);
+        return false;
+    }
+
+    static int forBinaryTree(TreeNode curr, int a, int b) {
+        ArrayList<Integer> ancestersA = new ArrayList<>();
+        if (!findBTAncestors(curr, ancestersA, a)) return -1;
+        ArrayList<Integer> ancestersB = new ArrayList<>();
+        if (!findBTAncestors(curr, ancestersB, b)) return -1;
+        int i = ancestersA.size() - 1;
+        int j = ancestersB.size() - 1;
+        while (i >= 0 && j >= 0 && ancestersA.get(i) != ancestersB.get(j)) {
+            i--;
+            j--;
+        }
+        if (i >= 0) {
+            return ancestersA.get(i);
+        } else {
+            return ancestersB.get(j);
+        }
     }
 }
--- src/test/java/LowestCommonAncesterFinderTest.java
+++ src/test/java/LowestCommonAncesterFinderTest.java
@@ -40,4 +40,43 @@ public class LowestCommonAncesterFinderTest {
         expected = 4;
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void findForBinaryTree() {
+        TreeNode root = new TreeNode(4, null, null);
+        int actual = LowestCommonAncesterFinder.forBinaryTree(root, 1, 2);
+        int expected = -1;
+        Assert.assertEquals(expected, actual);
+
+        root = new TreeNode(3, new TreeNode(4, null, null), null);
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 4, 3);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+
+        root = new TreeNode(3, null, new TreeNode(4, null, null));
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 3, 4);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+
+        root = new TreeNode(3, new TreeNode(4, null, null), new TreeNode(5, null, null));
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 4, 5);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+
+        root = new TreeNode(3, new TreeNode(4, new TreeNode(7, null, null), null),
+                new TreeNode(1, null, new TreeNode(2, null, null)));
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 2, 7);
+        expected = 3;
+        Assert.assertEquals(expected, actual);
+
+        root = new TreeNode(1, new TreeNode(2, new TreeNode(4, null, null), new TreeNode(5, null, null)),
+                new TreeNode(3, null, new TreeNode(6, null, new TreeNode(7, null, null))));
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 4, 5);
+        expected = 2;
+        Assert.assertEquals(expected, actual);
+
+        actual = LowestCommonAncesterFinder.forBinaryTree(root, 7, 5);
+        expected = 1;
+        Assert.assertEquals(expected, actual);
+    }
 }
