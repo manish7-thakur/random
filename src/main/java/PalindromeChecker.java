b9e584aabe2d7a49b3bd4b9c738e632b4ba21ead
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-05-03 15:18:46 +0700

    recursive plaindrome

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -17,4 +17,31 @@ public class PalindromeChecker {
         }
         return true;
     }
+
+    /**
+     * Class to wrap a pointer, basically c language **CharNode syntax
+     */
+    static class P {
+        CharNode next;
+    }
+
+    static boolean isPalindromeRec(CharNode node) {
+        P head = new P();
+        head.next = node;
+        return isPalindrome(node, head);
+    }
+
+    private static boolean isPalindrome(CharNode head, P start) {
+        if (head == null) {
+            return true;
+        }
+        if (!isPalindrome(head.next, start)) {
+            return false;
+        }
+        if (head.data != start.next.data) {
+            return false;
+        }
+        start.next = start.next.next;
+        return true;
+    }
 }
--- src/test/java/PalindromeCheckerTest.java
+++ src/test/java/PalindromeCheckerTest.java
@@ -5,12 +5,11 @@ public class PalindromeCheckerTest {
     @Test
     public void testPalindrome() {
         CharNode head = new CharNode('a', new CharNode('a', null));
-        boolean actual = PalindromeChecker.isPalindrome(head);
+        boolean actual = PalindromeChecker.isPalindromeRec(head);
         Assert.assertTrue(actual);
 
-
         head = new CharNode('a', new CharNode('b', null));
-        actual = PalindromeChecker.isPalindrome(head);
+        actual = PalindromeChecker.isPalindromeRec(head);
         Assert.assertFalse(actual);
 
         head = new CharNode('a', new CharNode('b', new CharNode('a', null)));
@@ -18,11 +17,15 @@ public class PalindromeCheckerTest {
         Assert.assertTrue(actual);
 
         head = new CharNode('a', new CharNode('b', new CharNode('b', new CharNode('a', null))));
-        actual = PalindromeChecker.isPalindrome(head);
+        actual = PalindromeChecker.isPalindromeRec(head);
         Assert.assertTrue(actual);
 
         head = new CharNode('a', new CharNode('b', new CharNode('a', new CharNode('b', null))));
-        actual = PalindromeChecker.isPalindrome(head);
+        actual = PalindromeChecker.isPalindromeRec(head);
         Assert.assertFalse(actual);
+
+        head = new CharNode('a', new CharNode('b', new CharNode('c', new CharNode('b', new CharNode('a', null)))));
+        actual = PalindromeChecker.isPalindromeRec(head);
+        Assert.assertTrue(actual);
     }
 }
