3bdf14bb3fd75a872e84a6aa770b8e690b8f20f7
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-05-30 14:11:16 +0700

    Level order traversal for Bin tree

---
+++ src/main/java/LevelOrderTraverser.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class LevelOrderTraverser {
+    static List<Integer> traverse(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        List<Integer> res = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < queue.size(); i++) {
+                TreeNode curr = queue.poll();
+                res.add(curr.value);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+        return res;
+    }
+}
