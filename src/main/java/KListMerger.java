ffc8f3ff89b01c5e7724ab2af4b839499179d25e
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-06-13 11:13:04 +0700

    height restricted minheap for merging k sorted lists

--- src/main/java/KListMerger.java
+++ src/main/java/KListMerger.java
@@ -2,22 +2,29 @@ import java.util.*;
 
 public class KListMerger {
 	static Node merge(Node[] lists) {
-		Queue<Node> minHeap = new PriorityQueue<>(new Comparator<Node>() {
+		int capacity = lists.length > 0 ? lists.length : 1;
+		Queue<Node> minHeap = new PriorityQueue<>(capacity, new Comparator<Node>() {
 			@Override
 			public int compare(Node o1, Node o2) {
 				return o1.data - o2.data;
 			}
 		});
-		for(Node list: lists) {
-			while(list != null) {
-				minHeap.add(list);
-				list = list.next;
+		for(int i=0;i < lists.length; i++) {
+			if(lists[i] != null) {
+				minHeap.add(lists[i]);
 			}
 		}
 		Node newList = new Node(0, null);
 		Node head = newList;
+		//We take out & insert only one item in the heap to keep its size
+		//equal to number of list rather than total elements of all lists.
 		while(!minHeap.isEmpty()) {
-			newList.next = minHeap.remove();
+			Node curr = minHeap.remove();
+			newList.next = curr;
+			curr = curr.next;
+			if(curr != null){
+				minHeap.add(curr);
+			}
 			newList = newList.next;
 		}
 		newList.next = null;
