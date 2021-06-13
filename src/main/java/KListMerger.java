bb60e84599f0d7a81a986ec2f0abc19d3507febe
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-06-13 11:14:46 +0700

    enhanced for loop

--- src/main/java/KListMerger.java
+++ src/main/java/KListMerger.java
@@ -1,33 +1,35 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class KListMerger {
-	static Node merge(Node[] lists) {
-		int capacity = lists.length > 0 ? lists.length : 1;
-		Queue<Node> minHeap = new PriorityQueue<>(capacity, new Comparator<Node>() {
-			@Override
-			public int compare(Node o1, Node o2) {
-				return o1.data - o2.data;
-			}
-		});
-		for(Node curr: lists) {
-			if(curr != null) {
-				minHeap.add(curr);
-			}
-		}
-		Node newList = new Node(0, null);
-		Node head = newList;
-		//We take out & insert only one item in the heap to keep its size
-		//equal to number of list rather than total elements of all lists.
-		while(!minHeap.isEmpty()) {
-			Node curr = minHeap.remove();
-			newList.next = curr;
-			curr = curr.next;
-			if(curr != null){
-				minHeap.add(curr);
-			}
-			newList = newList.next;
-		}
-		newList.next = null;
-		return head.next;
-	}
+    static Node merge(Node[] lists) {
+        int capacity = lists.length > 0 ? lists.length : 1;
+        Queue<Node> minHeap = new PriorityQueue<>(capacity, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.data - o2.data;
+            }
+        });
+        for (Node curr : lists) {
+            if (curr != null) {
+                minHeap.add(curr);
+            }
+        }
+        Node newList = new Node(0, null);
+        Node head = newList;
+        //We take out & insert only one item in the heap to keep its size
+        //equal to number of list rather than total elements of all lists.
+        while (!minHeap.isEmpty()) {
+            Node curr = minHeap.remove();
+            newList.next = curr;
+            curr = curr.next;
+            if (curr != null) {
+                minHeap.add(curr);
+            }
+            newList = newList.next;
+        }
+        newList.next = null;
+        return head.next;
+    }
 }
--- src/test/java/KListMergerTest.java
+++ src/test/java/KListMergerTest.java
@@ -1,45 +1,46 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class KListMergerTest {
-	@Test
-	public void mergeSortedLists() {
-		Node actual = KListMerger.merge(new Node[]{});
-		Assert.assertNull(actual);
+    @Test
+    public void mergeSortedLists() {
+        Node actual = KListMerger.merge(new Node[]{});
+        Assert.assertNull(actual);
 
-		Node head1 = new Node(1, null);
-		Node head2 = new Node(1, null);
-		actual = KListMerger.merge(new Node[]{head1, head2});
-		Assert.assertEquals(1, actual.data);
-		Assert.assertEquals(1, actual.next.data);
-		Assert.assertNull(actual.next.next);
+        Node head1 = new Node(1, null);
+        Node head2 = new Node(1, null);
+        actual = KListMerger.merge(new Node[]{head1, head2});
+        Assert.assertEquals(1, actual.data);
+        Assert.assertEquals(1, actual.next.data);
+        Assert.assertNull(actual.next.next);
 
-		head1 = new Node(1, new Node(2, null));
-		head2 = new Node(1, new Node(3, null));
-		actual = KListMerger.merge(new Node[]{head1, head2});
-		Assert.assertEquals(1, actual.data);
-		Assert.assertEquals(1, actual.next.data);
-		Assert.assertEquals(2, actual.next.next.data);
-		Assert.assertEquals(3, actual.next.next.next.data);
-		Assert.assertNull(actual.next.next.next.next);
+        head1 = new Node(1, new Node(2, null));
+        head2 = new Node(1, new Node(3, null));
+        actual = KListMerger.merge(new Node[]{head1, head2});
+        Assert.assertEquals(1, actual.data);
+        Assert.assertEquals(1, actual.next.data);
+        Assert.assertEquals(2, actual.next.next.data);
+        Assert.assertEquals(3, actual.next.next.next.data);
+        Assert.assertNull(actual.next.next.next.next);
 
-		head1 = new Node(1, new Node(2, null));
-		head2 = new Node(1, new Node(3, null));
-		Node head3 = new Node(2, new Node(3, null));
-		actual = KListMerger.merge(new Node[]{head1, head2, head3});
-		Assert.assertEquals(1, actual.data);
-		Assert.assertEquals(1, actual.next.data);
-		Assert.assertEquals(2, actual.next.next.data);
-		Assert.assertEquals(2, actual.next.next.next.data);
-		Assert.assertEquals(3, actual.next.next.next.next.data);
-		Assert.assertEquals(3, actual.next.next.next.next.next.data);
-		Assert.assertNull(actual.next.next.next.next.next.next);
+        head1 = new Node(1, new Node(2, null));
+        head2 = new Node(1, new Node(3, null));
+        Node head3 = new Node(2, new Node(3, null));
+        actual = KListMerger.merge(new Node[]{head1, head2, head3});
+        Assert.assertEquals(1, actual.data);
+        Assert.assertEquals(1, actual.next.data);
+        Assert.assertEquals(2, actual.next.next.data);
+        Assert.assertEquals(2, actual.next.next.next.data);
+        Assert.assertEquals(3, actual.next.next.next.next.data);
+        Assert.assertEquals(3, actual.next.next.next.next.next.data);
+        Assert.assertNull(actual.next.next.next.next.next.next);
 
-		head1 = new Node(-2, new Node(-1, new Node(-1, new Node(-1, null))));
-		actual = KListMerger.merge(new Node[]{head1, null});
-		Assert.assertEquals(-2, actual.data);
-		Assert.assertEquals(-1, actual.next.data);
-		Assert.assertEquals(-1, actual.next.next.data);
-		Assert.assertEquals(-1, actual.next.next.next.data);
-		Assert.assertNull(actual.next.next.next.next);
-	}
+        head1 = new Node(-2, new Node(-1, new Node(-1, new Node(-1, null))));
+        actual = KListMerger.merge(new Node[]{head1, null});
+        Assert.assertEquals(-2, actual.data);
+        Assert.assertEquals(-1, actual.next.data);
+        Assert.assertEquals(-1, actual.next.next.data);
+        Assert.assertEquals(-1, actual.next.next.next.data);
+        Assert.assertNull(actual.next.next.next.next);
+    }
 }
