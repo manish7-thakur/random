57ff615cde58ab5fb263889e4e01147cee71978c
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-06-19 21:23:28 +0700

    add test cases with k =1

--- src/test/java/ReverseKNodeGroupTest.java
+++ src/test/java/ReverseKNodeGroupTest.java
@@ -4,10 +4,23 @@ public class ReverseKNodeGroupTest {
 	@Test
 	public void reverseKGroupLinkedList() {
 		Node head = new Node(1, new Node(2, null));
-		Node actual = ReverseKNodeGroup.reverse(head, 2);
+		Node actual = ReverseKNodeGroup.reverse(head, 1);
+		Assert.assertEquals(1, actual.data);
+		Assert.assertEquals(2, actual.next.data);
+		Assert.assertNull(actual.next.next);
+
+		head = new Node(1, new Node(2, null));
+		actual = ReverseKNodeGroup.reverse(head, 2);
 		Assert.assertEquals(2, actual.data);
 		Assert.assertEquals(1, actual.next.data);
 		Assert.assertNull(actual.next.next);
+
+		head = new Node(1, new Node(2, new Node(3, null)));
+		actual = ReverseKNodeGroup.reverse(head, 1);
+		Assert.assertEquals(1, actual.data);
+		Assert.assertEquals(2, actual.next.data);
+		Assert.assertEquals(3, actual.next.next.data);
+		Assert.assertNull(actual.next.next.next);
 
 		head = new Node(1, new Node(2, new Node(3, null)));
 		actual = ReverseKNodeGroup.reverse(head, 2);
