332fce899000801ed02a1c852d42b9b32c499641
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-06-21 23:01:11 +0700

    store common calls in variables

--- src/main/java/SubstringConcatFinder.java
+++ src/main/java/SubstringConcatFinder.java
@@ -4,37 +4,36 @@ import java.util.Map;
 
 public class SubstringConcatFinder {
     static ArrayList<Integer> find(String s, String[] words) {
-        int wordlength = words[0].length();
+        int wordLength = words[0].length();
         int wordCount = words.length;
         ArrayList<Integer> res = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>(wordCount);
-        createMap(map, words);
-        int endPoint = s.length() - wordlength * wordCount;
+        Map<String, Integer> tempMap = new HashMap<>(wordCount);
+        for (int i = 0; i < wordCount; i++) {
+            int count = map.getOrDefault(words[i], 0);
+            map.put(words[i], ++count);
+        }
+        int strLen = s.length();
+        int endPoint = strLen - wordLength * wordCount;
         int i = 0;
         while (i <= endPoint) {
             int j = i;
-            while (j + wordlength <= s.length() && map.getOrDefault(s.substring(j, j + wordlength), 0) > 0 && wordCount > 0) {
-                String currWord = s.substring(j, j + wordlength);
-                int count = map.get(currWord);
-                map.put(currWord, --count);
-                j += wordlength;
+            tempMap.clear();
+            while (j + wordLength <= strLen && wordCount > 0) {
+                String currWord = s.substring(j, j + wordLength);
+                int count = tempMap.getOrDefault(currWord, 0);
+                if (!(map.getOrDefault(currWord, 0) > count))
+                    break;
+                tempMap.put(currWord, ++count);
+                j += wordLength;
                 wordCount--;
-                if (wordCount == 0) {
-                    res.add(i);
-                }
             }
-            createMap(map, words);
+            if (wordCount == 0) {
+                res.add(i);
+            }
             wordCount = words.length;
             i++;
         }
         return res;
-    }
-
-    static void createMap(Map<String, Integer> map, String[] words) {
-        map.clear();
-        for (int i = 0; i < words.length; i++) {
-            int count = map.getOrDefault(words[i], 0);
-            map.put(words[i], ++count);
-        }
     }
 }
