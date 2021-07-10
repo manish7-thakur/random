f59b1a444625aee26f2feff51e7e5b79c7ba58b6
Thakur, Manish (Agoda) <manish.thakur@agoda.com>
2021-07-10 16:53:58 +0700

    rename arr to nums

--- src/main/java/FirstMissingPositiveFinder.java
+++ src/main/java/FirstMissingPositiveFinder.java
@@ -1,25 +1,25 @@
 import java.util.*;
 public class FirstMissingPositiveFinder {
-  public static int find(int[] arr) {
-    for(int i = 0; i < arr.length; i++) {
-      if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != i+1) {
-        int curr = arr[arr[i]-1];
-        arr[arr[i]-1] = arr[i];
-        arr[i] = 0;
-        while(curr > 0 && curr <= arr.length && arr[curr - 1] != curr) {
-          int temp = arr[curr - 1];
-          arr[curr - 1] = curr;
+  public static int find(int[] nums) {
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != i+1) {
+        int curr = nums[nums[i]-1];
+        nums[nums[i]-1] = nums[i];
+        nums[i] = 0;
+        while(curr > 0 && curr <= nums.length && nums[curr - 1] != curr) {
+          int temp = nums[curr - 1];
+          nums[curr - 1] = curr;
           curr = temp;
         }
-      } else if(arr[i] < 0 || arr[i] > arr.length) {
-        arr[i] = 0;
+      } else if(nums[i] < 0 || nums[i] > nums.length) {
+        nums[i] = 0;
       }
     }
-    for(int i = 0; i < arr.length; i++) {
-      if(arr[i] == 0) {
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] == 0) {
         return i + 1;
       }
     }
-    return arr.length + 1;
+    return nums.length + 1;
   }
 }
