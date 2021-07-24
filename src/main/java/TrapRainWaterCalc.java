2a7d93a1352962849214b8f81cdd08093a677f12
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-07-24 13:50:05 +0700

    trp water using 2 pointers

--- src/main/java/TrapRainWaterCalc.java
+++ src/main/java/TrapRainWaterCalc.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class TrapRainWaterCalc {
   public static int calc(int[] height) {
     int res = 0;
-    for(int i = 0; i < height.length; i++) {
+    for(int i = 1; i < height.length - 1; i++) {
       int leftMax = 0;
       int rightMax = 0;
       for(int j = i-1; j>= 0; j--) {
@@ -13,6 +14,54 @@ public class TrapRainWaterCalc {
       int value = Math.min(leftMax, rightMax) - height[i];
       if(value > 0) {
         res += value;
+      }
+    }
+    return res;
+  }
+
+  public static int calcDp(int[] height) {
+    int res = 0;
+    if(height.length == 0) {
+      return 0;
+    }
+    int[] leftArr = new int[height.length];
+    int[] rightArr = new int[height.length];
+    leftArr[0] = height[0];
+    for(int i = 1; i < height.length; i++) {
+      leftArr[i] = Math.max(height[i], leftArr[i-1]);
+    }
+    rightArr[height.length-1] = height[height.length-1];
+    for(int i = height.length - 2; i >= 0; i--) {
+      rightArr[i] = Math.max(height[i], rightArr[i+1]);
+    }
+    for(int i = 0; i < height.length; i++) {
+      res += Math.min(leftArr[i], rightArr[i]) - height[i];
+    }
+    return res;
+  }
+
+  public static int calc2Pointer(int[] height) {
+    if(height.length == 0) {
+      return 0;
+    }
+    int left = 0;
+    int right = height.length - 1;
+    int leftMax = height[left];
+    int rightMax = height[right];
+    int res = 0;
+    while(left < right) {
+      if(leftMax <= rightMax) {
+        res += leftMax - height[left];
+        left++;
+        if(height[left] > leftMax) {
+          leftMax = height[left];
+        }
+      } else {
+        res += rightMax - height[right];
+        right--;
+        if(height[right] > rightMax) {
+          rightMax = height[right];
+        }
       }
     }
     return res;
--- src/test/java/TrapRainWaterCalcTest.java
+++ src/test/java/TrapRainWaterCalcTest.java
@@ -3,12 +3,12 @@ import org.junit.*;
 public class TrapRainWaterCalcTest {
   @Test
   public void calcTrappedRainWater() {
-    int actual = TrapRainWaterCalc.calc(new int[] {});
-    int expected = 0;
-    Assert.assertEquals(expected, actual);
+   int actual = TrapRainWaterCalc.calc(new int[] {});
+   int expected = 0;
+   Assert.assertEquals(expected, actual);
 
-    actual = TrapRainWaterCalc.calc(new int[] {1, 0, 1});
-    expected = 1;
+   actual = TrapRainWaterCalc.calc(new int[] {1, 0, 1});
+   expected = 1;
     Assert.assertEquals(expected, actual);
 
     actual = TrapRainWaterCalc.calc(new int[] {2, 1, 0, 2});
@@ -20,6 +20,46 @@ public class TrapRainWaterCalcTest {
     Assert.assertEquals(expected, actual);
 
     actual = TrapRainWaterCalc.calc(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
+    expected = 6;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calcDp(new int[] {});
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calcDp(new int[] {1, 0, 1});
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calcDp(new int[] {2, 1, 0, 2});
+    expected = 3;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calcDp(new int[] {4,2,0,3,2,5});
+    expected = 9;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calcDp(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
+    expected = 6;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {});
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {1, 0, 1});
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {2, 1, 0, 2});
+    expected = 3;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {4,2,0,3,2,5});
+    expected = 9;
+    Assert.assertEquals(expected, actual);
+
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
     expected = 6;
     Assert.assertEquals(expected, actual);
   }
