a5f803457808f7d3a3412ad336ee4f591bed3095
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-07-28 13:31:01 +0700

    add test case

--- src/test/java/TrapRainWaterCalcTest.java
+++ src/test/java/TrapRainWaterCalcTest.java
@@ -59,6 +59,10 @@ public class TrapRainWaterCalcTest {
     expected = 9;
     Assert.assertEquals(expected, actual);
 
+    actual = TrapRainWaterCalc.calc2Pointer(new int[] {5,5,1,7,1,1,5,2,7,6});
+    expected = 23;
+    Assert.assertEquals(expected, actual);
+
     actual = TrapRainWaterCalc.calc2Pointer(new int[] {0,1,0,2,1,0,1,3,2,1,2,1});
     expected = 6;
     Assert.assertEquals(expected, actual);
