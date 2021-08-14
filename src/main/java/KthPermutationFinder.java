ac7c8e368dd450219b0a73c4dd5e2911cef7f3d1
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-14 22:41:15 +0700

    format code

--- src/main/java/KthPermutationFinder.java
+++ src/main/java/KthPermutationFinder.java
@@ -1,28 +1,28 @@
-import java.util.Arrays;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class KthPermutationFinder {
-  public static String getPermutation(int n, int k) {
-    int curr = 0;
-    StringBuilder sb = new StringBuilder(n);
-    for(int i = 1; i <= n; i++) {
-      sb.append(i);
+    public static String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 1; i <= n; i++) {
+            sb.append(i);
+        }
+        List<String> list = permutate(sb.toString(), k);
+        return list.get(k - 1);
     }
-    List<String> list = permutate(sb.toString(), k);
-    return list.get(k-1);
-  }
-  private static List<String> permutate(String s, int k) {
-    List<String> list = new ArrayList<>();
-    if(s.length() == 1) {
-      list.add(s);
-      return list;
+
+    private static List<String> permutate(String s, int k) {
+        List<String> list = new ArrayList<>();
+        if (s.length() == 1) {
+            list.add(s);
+            return list;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            List<String> curr = permutate(s.substring(0, i) + s.substring(i + 1), k);
+            for (String str : curr) {
+                list.add(s.charAt(i) + str);
+            }
+        }
+        return list;
     }
-    for(int i = 0; i < s.length(); i++) {
-      List<String> curr = permutate(s.substring(0, i) + s.substring(i+1), k);
-      for(String str: curr) {
-        list.add(s.charAt(i) + str);
-      }
-    }
-    return list;
-  }
 }
--- src/test/java/KthPermutationFinderTest.java
+++ src/test/java/KthPermutationFinderTest.java
@@ -1,34 +1,35 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class KthPermutationFinderTest {
-  @Test
-  public void findKthPermuationFor() {
-    String actual = KthPermutationFinder.getPermutation(1, 1);
-    String expected = "1";
-    Assert.assertEquals(expected, actual);
+    @Test
+    public void findKthPermuationFor() {
+        String actual = KthPermutationFinder.getPermutation(1, 1);
+        String expected = "1";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(2, 1);
-    expected = "12";
-    Assert.assertEquals(expected, actual);
+        actual = KthPermutationFinder.getPermutation(2, 1);
+        expected = "12";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(2, 2);
-    expected = "21";
-    Assert.assertEquals(expected, actual);
+        actual = KthPermutationFinder.getPermutation(2, 2);
+        expected = "21";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(3, 1);
-    expected = "123";
-    Assert.assertEquals(expected, actual);
+        actual = KthPermutationFinder.getPermutation(3, 1);
+        expected = "123";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(3, 3);
-    expected = "213";
-    Assert.assertEquals(expected, actual);
+        actual = KthPermutationFinder.getPermutation(3, 3);
+        expected = "213";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(3, 6);
-    expected = "321";
-    Assert.assertEquals(expected, actual);
+        actual = KthPermutationFinder.getPermutation(3, 6);
+        expected = "321";
+        Assert.assertEquals(expected, actual);
 
-    actual = KthPermutationFinder.getPermutation(4, 9);
-    expected = "2314";
-    Assert.assertEquals(expected, actual);
-  }
+        actual = KthPermutationFinder.getPermutation(4, 9);
+        expected = "2314";
+        Assert.assertEquals(expected, actual);
+    }
 }
