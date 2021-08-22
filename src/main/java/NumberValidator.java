826cc1d02cb6d7f4e9c3113d282100fb346d002d
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-22 13:50:55 +0700

    s.charAt in a var

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -6,13 +6,14 @@ public class NumberValidator {
         boolean containsDot = false;
         boolean isexp = false;
         for (int i = 0; i < s.length(); i++) {
-          char c = s.charAt(i);
+            char c = s.charAt(i);
             if (c == '+' || c == '-') {
                 if (i == 0) continue;
                 if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
             } else if (c == '.') {
                 if (isexp || containsDot) return false;
-                if ((i == 0 && !Character.isDigit(s.charAt(i + 1))) || (i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))) return false;
+                if (i == 0 && !Character.isDigit(s.charAt(i + 1)) || i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))
+                    return false;
                 containsDot = true;
             } else if (c == 'e' || c == 'E') {
                 if (i == 0 || isexp || i == s.length() - 1) return false;
