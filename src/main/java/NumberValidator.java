c08f593fa743aaaa17c44be57831282f1caaf213
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-22 13:46:44 +0700

    use character.isDigit

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -11,14 +11,13 @@ public class NumberValidator {
                 if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
             } else if (s.charAt(i) == '.') {
                 if (isexp || containsDot) return false;
-                if ((i == 0 && !(s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9'))
-                        || (i == s.length() - 1 && !(s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '9'))) return false;
+                if ((i == 0 && !Character.isDigit(s.charAt(i + 1))) || (i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))) return false;
                 containsDot = true;
             } else if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
                 if (i == 0 || isexp || i == s.length() - 1) return false;
-                if (!((s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '9') || s.charAt(i - 1) == '.')) return false;
+                if (!(Character.isDigit(s.charAt(i - 1)) || s.charAt(i - 1) == '.')) return false;
                 isexp = true;
-            } else if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+            } else if (!Character.isDigit(s.charAt(i))) {
                 return false;
             }
         }
