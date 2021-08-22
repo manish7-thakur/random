5b7aefedf5f36ca63eb3295e0ff6974294232899
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-22 13:35:44 +0700

    remove i !=0

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -1,30 +1,27 @@
 public class NumberValidator {
     public static boolean isNumber(String s) {
-      if(s.isEmpty() || (s.length() == 1 && !(s.charAt(0) >= '0' && s.charAt(0) <= '9'))) {
-        return false;
-      }
-      boolean containsDot = false;
-      boolean isexp = false;
-      for(int i = 0; i < s.length(); i++) {
-        if(s.charAt(i) == '+' || s.charAt(i) == '-') {
-          if(i == 0) continue;
-          if(!(s.charAt(i - 1) == 'e' || s.charAt(i-1) == 'E') || i == s.length() - 1) return false;
-        }
-        else if(s.charAt(i) == '.') {
-          if(isexp || containsDot) return false;
-          if((i == 0 && !(s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9'))
-          || (i == s.length() - 1 && !(s.charAt(i-1) >= '0' && s.charAt(i-1) <= '9'))) return false;
-          containsDot = true;
-        }
-        else if(s.charAt(i) == 'e' || s.charAt(i) == 'E') {
-          if(i == 0 || isexp || i == s.length() - 1) return false;
-          if(i != 0 && !((s.charAt(i-1) >= '0' && s.charAt(i-1) <= '9') || s.charAt(i - 1) == '.')) return false;
-          isexp = true;
-        }
-        else if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+        if (s.isEmpty() || (s.length() == 1 && !(s.charAt(0) >= '0' && s.charAt(0) <= '9'))) {
             return false;
         }
-      }
-      return true;
+        boolean containsDot = false;
+        boolean isexp = false;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                if (i == 0) continue;
+                if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
+            } else if (s.charAt(i) == '.') {
+                if (isexp || containsDot) return false;
+                if ((i == 0 && !(s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9'))
+                        || (i == s.length() - 1 && !(s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '9'))) return false;
+                containsDot = true;
+            } else if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
+                if (i == 0 || isexp || i == s.length() - 1) return false;
+                if (!((s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '9') || s.charAt(i - 1) == '.')) return false;
+                isexp = true;
+            } else if (!(s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/NumberValidatorTest.java
+++ src/test/java/NumberValidatorTest.java
@@ -4,97 +4,97 @@ import org.junit.Test;
 public class NumberValidatorTest {
     @Test
     public void validateNumber() {
-       boolean actual = NumberValidator.isNumber("");
-       Assert.assertFalse(actual);
+        boolean actual = NumberValidator.isNumber("");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber(".");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber(".");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("..");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("..");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("1");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("1");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("+");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("-");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("-");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("+.");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+.");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("+-1.");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+-1.");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("+1");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("+1");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("-1");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("-1");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("+-1");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+-1");
+        Assert.assertFalse(actual);
 
-       actual = NumberValidator.isNumber("+1.");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("+1.");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber(".2");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber(".2");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("+.2");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("+.2");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("-.2");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("-.2");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("+2.0");
-       Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("+2.0");
+        Assert.assertTrue(actual);
 
-       actual = NumberValidator.isNumber("+2.0.");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+2.0.");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("2e10");
-      Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("2e10");
+        Assert.assertTrue(actual);
 
-      actual = NumberValidator.isNumber("-2.e-10");
-      Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("-2.e-10");
+        Assert.assertTrue(actual);
 
-      actual = NumberValidator.isNumber("0e");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("0e");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber(".e1");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber(".e1");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("1e.");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("1e.");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("2e1e0");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("2e1e0");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("4e+");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("4e+");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("+E3");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("+E3");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("2e1.0");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("2e1.0");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("-2e+1.0");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("-2e+1.0");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("-2E+1");
-      Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("-2E+1");
+        Assert.assertTrue(actual);
 
-      actual = NumberValidator.isNumber("e3");
-      Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("e3");
+        Assert.assertFalse(actual);
 
-      actual = NumberValidator.isNumber("-123.456e789");
-      Assert.assertTrue(actual);
+        actual = NumberValidator.isNumber("-123.456e789");
+        Assert.assertTrue(actual);
 
-      actual = NumberValidator.isNumber("7e69e");
-       Assert.assertFalse(actual);
+        actual = NumberValidator.isNumber("7e69e");
+        Assert.assertFalse(actual);
     }
 }
