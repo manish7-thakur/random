54271dd8791fae2de3b17653d2924b3cfc6bd190
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-22 13:49:10 +0700

    s.charAt in a var

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -6,18 +6,19 @@ public class NumberValidator {
         boolean containsDot = false;
         boolean isexp = false;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+          char c = s.charAt(i);
+            if (c == '+' || c == '-') {
                 if (i == 0) continue;
                 if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
-            } else if (s.charAt(i) == '.') {
+            } else if (c == '.') {
                 if (isexp || containsDot) return false;
                 if ((i == 0 && !Character.isDigit(s.charAt(i + 1))) || (i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))) return false;
                 containsDot = true;
-            } else if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
+            } else if (c == 'e' || c == 'E') {
                 if (i == 0 || isexp || i == s.length() - 1) return false;
                 if (!(Character.isDigit(s.charAt(i - 1)) || s.charAt(i - 1) == '.')) return false;
                 isexp = true;
-            } else if (!Character.isDigit(s.charAt(i))) {
+            } else if (!Character.isDigit(c)) {
                 return false;
             }
         }
