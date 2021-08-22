d4dd9f3cc24f6bb615a7fa1ae0b2f8c89ecf869f
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-08-22 14:15:35 +0700

    convert if-else to swtich

--- src/main/java/NumberValidator.java
+++ src/main/java/NumberValidator.java
@@ -7,20 +7,27 @@ public class NumberValidator {
         boolean isexp = false;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '+' || c == '-') {
-                if (i == 0) continue;
-                if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
-            } else if (c == '.') {
-                if (isexp || containsDot) return false;
-                if (i == 0 && !Character.isDigit(s.charAt(i + 1)) || i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))
-                    return false;
-                containsDot = true;
-            } else if (c == 'e' || c == 'E') {
-                if (i == 0 || isexp || i == s.length() - 1) return false;
-                if (!(Character.isDigit(s.charAt(i - 1)) || s.charAt(i - 1) == '.')) return false;
-                isexp = true;
-            } else if (!Character.isDigit(c)) {
-                return false;
+            switch (c) {
+                case '+':
+                case '-':
+                    if (i == 0) continue;
+                    if (!(s.charAt(i - 1) == 'e' || s.charAt(i - 1) == 'E') || i == s.length() - 1) return false;
+                    break;
+                case '.':
+                    if (isexp || containsDot) return false;
+                    if (i == 0 && !Character.isDigit(s.charAt(i + 1)) || i == s.length() - 1 && !Character.isDigit(s.charAt(i - 1)))
+                        return false;
+                    containsDot = true;
+                    break;
+                case 'e':
+                case 'E':
+                    if (i == 0 || isexp || i == s.length() - 1) return false;
+                    if (!(Character.isDigit(s.charAt(i - 1)) || s.charAt(i - 1) == '.')) return false;
+                    isexp = true;
+                    break;
+                default:
+                    if (!Character.isDigit(c)) return false;
+                    break;
             }
         }
         return true;
