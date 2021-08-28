15228428bb274a0c1651c2c591bcb4dc12f52342
mthakur <manish.thakur@agoda.com>
2021-08-28 23:41:37 +0700

    space count evenly distributed

--- src/main/java/TextJustifier.java
+++ src/main/java/TextJustifier.java
@@ -5,10 +5,10 @@ public class TextJustifier {
         List<String> res = new ArrayList<>();
         int curr = 0;
         int i = 0, j;
-        int space = 0;
+        int spaceCount = 0;
         while (i < words.length) {
             for (j = i; j < words.length; j++) {
-                if (curr + words[j].length() + space == maxWidth) {
+                if (curr + words[j].length() + spaceCount == maxWidth) {
                     String str = "";
                     for (int k = i; k <= j; k++) {
                         if (k == j) str = str + words[k];
@@ -17,37 +17,38 @@ public class TextJustifier {
                     res.add(str);
                     i = j + 1;
                     break;
-                } else if (curr + words[j].length() + space > maxWidth) {
-                    int exSpc = maxWidth - (curr + space - 1);
-                    String str = justify(i, j - 1, maxWidth, exSpc, words);
+                } else if (curr + words[j].length() + spaceCount > maxWidth) {
+                    int spaces = maxWidth - (curr);
+                    int cSpc = spaceCount == 0 ? 0:  spaces / spaceCount;
+                    int exSpc = spaceCount == 0 ? spaces: spaces % spaceCount;
+                    String str = justify(i, j - 1, maxWidth, cSpc, exSpc, words);
                     res.add(str);
                     i = j;
                     break;
                 }
                 curr += words[j].length();
                 if (j + 1 == words.length) {
-                    int exSpc = maxWidth - (curr + space);
-                    String str = justify(i, j, maxWidth, exSpc, words);
+                    int spaces = (maxWidth - (curr));
+                    int cSpc = spaceCount == 0 ? 0:  spaces / spaceCount;
+                    int exSpc = spaceCount == 0 ? spaces: spaces % spaceCount;
+                    String str = justify(i, j, maxWidth, cSpc, exSpc, words);
                     res.add(str);
                     i = j + 1;
                 }
-                space++;
+                spaceCount++;
             }
             curr = 0;
-            space = 0;
+            spaceCount = 0;
         }
         return res;
     }
 
-    static String justify(int i, int j, int maxWidth, int exSpc, String[] words) {
-      if(i == j) {
-       return String.format("%-"+(words[i].length() + exSpc) + "s", words[i]);
-      }
+    static String justify(int i, int j, int maxWidth, int cSpc, int exSpc, String[] words) {
       String str = "";
       for (int k = i; k <= j; k++) {
         if(k == i) str = String.format("%-"+(words[k].length() + exSpc) + "s", words[k]);
         else str = str + words[k];
-          if(k != j) str = str + " ";
+          if(k != j) str = str + String.format("%"+ cSpc + "s", "");
       }
       return str;
     }
