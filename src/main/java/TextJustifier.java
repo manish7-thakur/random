4ea2987cc260d5866ce72bc5b4fa04f2365ff605
mthakur <manish.thakur@agoda.com>
2021-08-30 00:09:13 +0700

    separate left & right justify

--- src/main/java/TextJustifier.java
+++ src/main/java/TextJustifier.java
@@ -6,84 +6,73 @@ public class TextJustifier {
         List<String> res = new ArrayList<>();
         int curr = 0;
         int i = 0, j;
-            for (j = 0; j < words.length; j++) {
-               if (curr + words[j].length() + j - i > maxWidth) {
-                    String str = justify(i, j - 1, maxWidth, curr, words, false);
-                    res.add(str);
-                    i = j;
-                    curr = 0;
-                }
-                curr += words[j].length();
+        for (j = 0; j < words.length; j++) {
+            if (curr + words[j].length() + j - i > maxWidth) {
+                String str = justify(i, j - 1, maxWidth, curr, words);
+                res.add(str);
+                i = j;
+                curr = 0;
             }
-            String str = leftJustify(i, j - 1, maxWidth, curr, words, true);
-            res.add(str);
+            curr += words[j].length();
+        }
+        String str = leftJustify(i, j - 1, maxWidth, curr, words);
+        res.add(str);
         return res;
     }
-    static String leftJustify(int i, int j, int maxWidth, int textLen, String[] words, boolean lastLine) {
-      StringBuilder sb = new StringBuilder(maxWidth);
-      if(i == j) {
-        sb.append(words[i]);
-        for(int k = 0; k < maxWidth - textLen; k++) {
-          sb.append(' ');
+
+    static String leftJustify(int i, int j, int maxWidth, int textLen, String[] words) {
+        StringBuilder sb = new StringBuilder(maxWidth);
+        if (i == j) {
+            sb.append(words[i]);
+            for (int k = 0; k < maxWidth - textLen; k++) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+        for (int k = i; k <= j; k++) {
+            sb.append(words[k]);
+            if (k != j) {
+                sb.append(' ');
+            }
+        }
+        int sp = maxWidth - sb.length();
+        for (int k = 0; k < sp; k++) {
+            sb.append(' ');
         }
         return sb.toString();
-      }
-      for(int k = i; k <= j; k++) {
-        sb.append(words[k]);
-        if(k != j) {
-          sb.append(' ');
-        }
-      }
-      int sp = maxWidth - sb.length();
-      for(int k = 0; k < sp; k++) {
-        sb.append(' ');
-      }
-      return sb.toString();
     }
-    static String justify(int i, int j, int maxWidth, int textLen, String[] words, boolean lastLine) {
-      StringBuilder sb = new StringBuilder(maxWidth);
-      if(i == j) {
-        sb.append(words[i]);
-        for(int k = 0; k < maxWidth - textLen; k++) {
-          sb.append(' ');
+
+    static String justify(int i, int j, int maxWidth, int textLen, String[] words) {
+        StringBuilder sb = new StringBuilder(maxWidth);
+        if (i == j) {
+            sb.append(words[i]);
+            for (int k = 0; k < maxWidth - textLen; k++) {
+                sb.append(' ');
+            }
+            return sb.toString();
         }
-        return sb.toString();
-      }
-      int exSpc;
-      int cSpc;
-      if(lastLine) {
-        cSpc = 1;
-        exSpc = maxWidth - (textLen + j - i);
-      } else {
         int spaceCount = j - i;
         int spaces = maxWidth - textLen;
-        exSpc = spaces % spaceCount;
-        cSpc = spaces / spaceCount;
-      }
-      StringBuilder spcB = new StringBuilder(cSpc);
-      for(int k = 0; k < cSpc; k++) {
-        spcB.append(' ');
-      }
-      //String cSpcStr = String.format("%-" + cSpc + "s", ""); Using lib method slowed the algo to 13 ms
-      String cSpcStr = spcB.toString();
-      sb.append(words[i]);
-      sb.append(cSpcStr);
-      for(int k = i + 1; k <= j; k++) {
-        if(!lastLine && exSpc > 0) {
-          sb.append(" ");
-          exSpc--;
+        int exSpc = spaces % spaceCount;
+        int cSpc = spaces / spaceCount;
+        StringBuilder spcB = new StringBuilder(cSpc);
+        for (int k = 0; k < cSpc; k++) {
+            spcB.append(' ');
         }
-        sb.append(words[k]);
-        if(k != j) {
-          sb.append(cSpcStr);
+        //String cSpcStr = String.format("%-" + cSpc + "s", ""); Using lib method slowed the algo to 13 ms
+        String cSpcStr = spcB.toString();
+        sb.append(words[i]);
+        sb.append(cSpcStr);
+        for (int k = i + 1; k <= j; k++) {
+            if (exSpc > 0) {
+                sb.append(' ');
+                exSpc--;
+            }
+            sb.append(words[k]);
+            if (k != j) {
+                sb.append(cSpcStr);
+            }
         }
-      }
-      if(lastLine && exSpc > 0) {
-        int sp = maxWidth - sb.length();
-        for(int k = 0; k < sp; k++) {
-          sb.append(' ');
-        }
-      }
-      return sb.toString();
+        return sb.toString();
     }
 }
--- src/test/java/TextJustifierTest.java
+++ src/test/java/TextJustifierTest.java
@@ -5,129 +5,129 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TextJustifierTest {
-  @Test
-  public void justifyString() {
-    String actual = TextJustifier.justify(0 , 0, 4, 4, new String[] {"what"}, false);
-    String expected = "what";
-    Assert.assertEquals(expected, actual);
+    @Test
+    public void justifyString() {
+        String actual = TextJustifier.justify(0, 0, 4, 4, new String[]{"what"});
+        String expected = "what";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 0, 8, 4, new String[] {"what"}, false);
-    expected = "what    ";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 0, 8, 4, new String[]{"what"});
+        expected = "what    ";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 7, 6, new String[] {"what", "is"}, false);
-    expected = "what is";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 1, 7, 6, new String[]{"what", "is"});
+        expected = "what is";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 7, 6, new String[] {"what", "is"}, true);
-    expected = "what is";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.leftJustify(0, 1, 7, 6, new String[]{"what", "is"});
+        expected = "what is";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 8, 6, new String[] {"what", "is"}, true);
-    expected = "what is ";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.leftJustify(0, 1, 8, 6, new String[]{"what", "is"});
+        expected = "what is ";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 8, 6, new String[] {"what", "is"}, false);
-    expected = "what  is";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 1, 8, 6, new String[]{"what", "is"});
+        expected = "what  is";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 10, 6, new String[] {"what", "is"}, false);
-    expected = "what    is";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 1, 10, 6, new String[]{"what", "is"});
+        expected = "what    is";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 1, 10, 6, new String[] {"what", "is"}, true);
-    expected = "what is   ";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.leftJustify(0, 1, 10, 6, new String[]{"what", "is"});
+        expected = "what is   ";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 2, 11, 9, new String[] {"what", "is", "the"}, false);
-    expected = "what is the";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 2, 11, 9, new String[]{"what", "is", "the"});
+        expected = "what is the";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 2, 12, 9, new String[] {"what", "is", "the"}, false);
-    expected = "what  is the";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 2, 12, 9, new String[]{"what", "is", "the"});
+        expected = "what  is the";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 2, 14, 9, new String[] {"what", "is", "the"}, false);
-    expected = "what   is  the";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 2, 14, 9, new String[]{"what", "is", "the"});
+        expected = "what   is  the";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 16, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what is the time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 16, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what is the time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 17, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what  is the time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 17, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what  is the time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 18, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what  is  the time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 18, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what  is  the time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 19, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what  is  the  time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 19, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what  is  the  time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 20, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what   is  the  time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 20, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what   is  the  time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 21, 13, new String[] {"what", "is", "the", "time"}, false);
-    expected = "what   is   the  time";
-    Assert.assertEquals(expected, actual);
+        actual = TextJustifier.justify(0, 3, 21, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what   is   the  time";
+        Assert.assertEquals(expected, actual);
 
-    actual = TextJustifier.justify(0 , 3, 19, 13, new String[] {"what", "is", "the", "time"}, true);
-    expected = "what is the time   ";
-    Assert.assertEquals(expected, actual);
-  }
+        actual = TextJustifier.leftJustify(0, 3, 19, 13, new String[]{"what", "is", "the", "time"});
+        expected = "what is the time   ";
+        Assert.assertEquals(expected, actual);
+    }
 
-  @Test
-  public void justifyText() {
-      List<String> actual = TextJustifier.fullJustify(new String[]{"what"}, 4);
-      List<String> expected = Arrays.asList("what");
-      Assert.assertEquals(expected, actual);
+    @Test
+    public void justifyText() {
+        List<String> actual = TextJustifier.fullJustify(new String[]{"what"}, 4);
+        List<String> expected = Arrays.asList("what");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what"}, 5);
-      expected = Arrays.asList("what ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what"}, 5);
+        expected = Arrays.asList("what ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what"}, 10);
-      expected = Arrays.asList("what      ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what"}, 10);
+        expected = Arrays.asList("what      ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 7);
-      expected = Arrays.asList("what is");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 7);
+        expected = Arrays.asList("what is");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 8);
-      expected = Arrays.asList("what is ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 8);
+        expected = Arrays.asList("what is ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 6);
-      expected = Arrays.asList("what  ", "is the");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 6);
+        expected = Arrays.asList("what  ", "is the");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 7);
-      expected = Arrays.asList("what is", "the    ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 7);
+        expected = Arrays.asList("what is", "the    ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 11);
-      expected = Arrays.asList("what is the");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 11);
+        expected = Arrays.asList("what is the");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 16);
-      expected = Arrays.asList("what is the     ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 16);
+        expected = Arrays.asList("what is the     ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16);
-      expected = Arrays.asList("This    is    an", "example  of text", "justification.  ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16);
+        expected = Arrays.asList("This    is    an", "example  of text", "justification.  ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"What","must","be","acknowledgment","shall","be"}, 16);
-      expected = Arrays.asList("What   must   be", "acknowledgment  ", "shall be        ");
-      Assert.assertEquals(expected, actual);
+        actual = TextJustifier.fullJustify(new String[]{"What", "must", "be", "acknowledgment", "shall", "be"}, 16);
+        expected = Arrays.asList("What   must   be", "acknowledgment  ", "shall be        ");
+        Assert.assertEquals(expected, actual);
 
-      actual = TextJustifier.fullJustify(new String[]{"Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"}, 20);
-      expected = Arrays.asList("Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  ");
-      Assert.assertEquals(expected, actual);
-  }
+        actual = TextJustifier.fullJustify(new String[]{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"}, 20);
+        expected = Arrays.asList("Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  ");
+        Assert.assertEquals(expected, actual);
+    }
 }
