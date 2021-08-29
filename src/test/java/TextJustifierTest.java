e5fc185f1bc0a80fcce7cf8278007ac529b7bba1
mthakur <manish.thakur@agoda.com>
2021-08-29 14:00:49 +0700

    fix existing text as per line logic

--- src/test/java/TextJustifierTest.java
+++ src/test/java/TextJustifierTest.java
@@ -68,46 +68,46 @@ public class TextJustifierTest {
     Assert.assertEquals(expected, actual);
   }
 
-  // @Test
-  // public void justifyText() {
-  //     List<String> actual = TextJustifier.fullJustify(new String[]{"what"}, 4);
-  //     List<String> expected = Arrays.asList("what");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what"}, 5);
-  //     expected = Arrays.asList("what ");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what"}, 10);
-  //     expected = Arrays.asList("what      ");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 7);
-  //     expected = Arrays.asList("what is");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 8);
-  //     expected = Arrays.asList("what  is");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 6);
-  //     expected = Arrays.asList("what  ", "is the");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 7);
-  //     expected = Arrays.asList("what is", "the    ");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 11);
-  //     expected = Arrays.asList("what is the");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 16);
-  //     expected = Arrays.asList("what    is   the");
-  //     Assert.assertEquals(expected, actual);
-  //
-  //     actual = TextJustifier.fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16);
-  //     expected = Arrays.asList("This    is    an", "example  of text", "justification.  ");
-  //     Assert.assertEquals(expected, actual);
-  // }
+  @Test
+  public void justifyText() {
+      List<String> actual = TextJustifier.fullJustify(new String[]{"what"}, 4);
+      List<String> expected = Arrays.asList("what");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what"}, 5);
+      expected = Arrays.asList("what ");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what"}, 10);
+      expected = Arrays.asList("what      ");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 7);
+      expected = Arrays.asList("what is");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is"}, 8);
+      expected = Arrays.asList("what is ");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 6);
+      expected = Arrays.asList("what  ", "is the");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 7);
+      expected = Arrays.asList("what is", "the    ");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 11);
+      expected = Arrays.asList("what is the");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"what", "is", "the"}, 16);
+      expected = Arrays.asList("what is the     ");
+      Assert.assertEquals(expected, actual);
+
+      actual = TextJustifier.fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16);
+      expected = Arrays.asList("This    is    an", "example  of text", "justification.  ");
+      Assert.assertEquals(expected, actual);
+  }
 }
