fed0dfc79baf1d3e4bdd256df7303dd035f18fc5
mthakur <manish.thakur@agoda.com>
2021-08-30 00:01:53 +0700

    left justify method

--- src/main/java/TextJustifier.java
+++ src/main/java/TextJustifier.java
@@ -15,11 +15,31 @@ public class TextJustifier {
                 }
                 curr += words[j].length();
             }
-            String str = justify(i, j - 1, maxWidth, curr, words, true);
+            String str = leftJustify(i, j - 1, maxWidth, curr, words, true);
             res.add(str);
         return res;
     }
-
+    static String leftJustify(int i, int j, int maxWidth, int textLen, String[] words, boolean lastLine) {
+      StringBuilder sb = new StringBuilder(maxWidth);
+      if(i == j) {
+        sb.append(words[i]);
+        for(int k = 0; k < maxWidth - textLen; k++) {
+          sb.append(' ');
+        }
+        return sb.toString();
+      }
+      for(int k = i; k <= j; k++) {
+        sb.append(words[k]);
+        if(k != j) {
+          sb.append(' ');
+        }
+      }
+      int sp = maxWidth - sb.length();
+      for(int k = 0; k < sp; k++) {
+        sb.append(' ');
+      }
+      return sb.toString();
+    }
     static String justify(int i, int j, int maxWidth, int textLen, String[] words, boolean lastLine) {
       StringBuilder sb = new StringBuilder(maxWidth);
       if(i == j) {
