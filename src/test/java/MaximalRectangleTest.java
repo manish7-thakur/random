b8eb37b69673abefb98cac818c03127e083735c7
mthakur <manish.thakur@agoda.com>
2021-09-08 22:25:29 +0700

    monotonic maximal rect with 4x4

--- src/test/java/MaximalRectangleTest.java
+++ src/test/java/MaximalRectangleTest.java
@@ -34,5 +34,25 @@ public class MaximalRectangleTest {
     actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '1'}, {'1', '1'}});
     expected = 4;
     Assert.assertEquals(expected, actual);
+
+    actual = MaximalRectangle.maximalRectangle(new char[][]{{'0', '0'}, {'1', '1'}});
+    expected = 2;
+    Assert.assertEquals(expected, actual);
+
+    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '0', '0'}, {'1', '1', '0'}});
+    expected = 3;
+    Assert.assertEquals(expected, actual);
+
+    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '1', '0'}, {'1', '1', '0'}});
+    expected = 4;
+    Assert.assertEquals(expected, actual);
+
+    actual = MaximalRectangle.maximalRectangle(new char[][]
+    {{'1','0','1','0','0'},
+    {'1','0','1','1','1'},
+    {'1','1','1','1','1'},
+    {'1','0','0','1','0'}});
+    expected = 6;
+    Assert.assertEquals(expected, actual);
   }
 }
