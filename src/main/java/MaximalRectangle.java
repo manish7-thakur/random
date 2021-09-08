cf728d7c17fb10bcfe076d8dd2ac318bc0971628
mthakur <manish.thakur@agoda.com>
2021-09-08 22:33:00 +0700

    don't add the item to stack if stack.peek height is same

--- src/main/java/MaximalRectangle.java
+++ src/main/java/MaximalRectangle.java
@@ -1,30 +1,32 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class MaximalRectangle {
-  static public int maximalRectangle(char[][] matrix) {
-    int res = 0;
-    char[][] extended = new char[matrix.length][];
-    for(int i = 0; i < extended.length; i++) {
-      extended[i] = Arrays.copyOf(matrix[i], matrix[i].length + 1);
-    }
-    Stack<Integer[]> stack = new Stack<>();
-    for(int i = 0; i < extended.length; i++) {
-      for(int j = 0; j < extended[0].length; j++) {
-          int height = 0;
-          for(int k = i; k >=0 && extended[k][j] == '1'; k--) {
-            height++;
-          }
-          int curr = j;
-          while(!stack.isEmpty() && stack.peek()[1] > height) {
-            Integer[] pair = stack.pop();
-            int idx = pair[0];
-            int h = pair[1];
-            res = Math.max(res, (j - idx) * h);
-            curr = idx;
-          }
-          stack.push(new Integer[]{curr, height});
+    static public int maximalRectangle(char[][] matrix) {
+        int res = 0;
+        char[][] extended = new char[matrix.length][];
+        for (int i = 0; i < extended.length; i++) {
+            extended[i] = Arrays.copyOf(matrix[i], matrix[i].length + 1);
         }
+        Stack<Integer[]> stack = new Stack<>();
+        for (int i = 0; i < extended.length; i++) {
+            for (int j = 0; j < extended[0].length; j++) {
+                int height = 0;
+                for (int k = i; k >= 0 && extended[k][j] == '1'; k--) {
+                    height++;
+                }
+                int curr = j;
+                while (!stack.isEmpty() && stack.peek()[1] > height) {
+                    Integer[] pair = stack.pop();
+                    int idx = pair[0];
+                    int h = pair[1];
+                    res = Math.max(res, (j - idx) * h);
+                    curr = idx;
+                }
+                if (stack.isEmpty() || stack.peek()[1] != height)
+                    stack.push(new Integer[]{curr, height});
+            }
+        }
+        return res;
     }
-    return res;
-  }
 }
--- src/test/java/MaximalRectangleTest.java
+++ src/test/java/MaximalRectangleTest.java
@@ -1,58 +1,59 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximalRectangleTest {
-  @Test
-  public void calcMaxArea() {
-    int actual = MaximalRectangle.maximalRectangle(new char[][]{});
-    int expected = 0;
-    Assert.assertEquals(expected, actual);
+    @Test
+    public void calcMaxArea() {
+        int actual = MaximalRectangle.maximalRectangle(new char[][]{});
+        int expected = 0;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'0'}});
-    expected = 0;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'0'}});
+        expected = 0;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1'}});
-    expected = 1;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1'}});
+        expected = 1;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'0', '0'}});
-    expected = 0;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'0', '0'}});
+        expected = 0;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '1'}});
-    expected = 2;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '1'}});
+        expected = 2;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0'}, {'1', '0'}});
-    expected = 2;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0'}, {'1', '0'}});
+        expected = 2;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0'}, {'1', '1'}});
-    expected = 2;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0'}, {'1', '1'}});
+        expected = 2;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '1'}, {'1', '1'}});
-    expected = 4;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '1'}, {'1', '1'}});
+        expected = 4;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'0', '0'}, {'1', '1'}});
-    expected = 2;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'0', '0'}, {'1', '1'}});
+        expected = 2;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '0', '0'}, {'1', '1', '0'}});
-    expected = 3;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '0', '0'}, {'1', '1', '0'}});
+        expected = 3;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '1', '0'}, {'1', '1', '0'}});
-    expected = 4;
-    Assert.assertEquals(expected, actual);
+        actual = MaximalRectangle.maximalRectangle(new char[][]{{'1', '0', '0'}, {'1', '1', '0'}, {'1', '1', '0'}});
+        expected = 4;
+        Assert.assertEquals(expected, actual);
 
-    actual = MaximalRectangle.maximalRectangle(new char[][]
-    {{'1','0','1','0','0'},
-    {'1','0','1','1','1'},
-    {'1','1','1','1','1'},
-    {'1','0','0','1','0'}});
-    expected = 6;
-    Assert.assertEquals(expected, actual);
-  }
+        actual = MaximalRectangle.maximalRectangle(new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}});
+        expected = 6;
+        Assert.assertEquals(expected, actual);
+    }
 }
