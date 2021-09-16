ac0d961706f52702967479fbc549aae81b43781f
mthakur <manish.thakur@agoda.com>
2021-09-16 22:01:55 +0700

    Expression evaluator

---
+++ src/main/java/ExpressionEvaluator.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class ExpressionEvaluator {
+  public static int evaluate(String exp) {
+    Stack<Character> opStack = new Stack<>();
+    Stack<Integer> nStack = new Stack<>();
+    Set<Character> ops = new HashSet<>();
+    ops.add('+');
+    ops.add('-');
+    ops.add('*');
+    ops.add('/');
+    int i = 0;
+    int j = 0;
+    while(!ops.contains(exp.charAt(i))) {
+      i++;
+    }
+    Integer num = Integer.parseInt(exp.substring(j, i));
+    nStack.push(num);
+    while(i < exp.length()) {
+      char c = exp.charAt(i);
+      j = ++i;
+      while(i < exp.length() && !ops.contains(exp.charAt(i))) {
+        i++;
+      }
+      num = Integer.parseInt(exp.substring(j, i));
+      switch (c) {
+        case '*' :
+          nStack.push(num * nStack.pop());
+          break;
+        case '/' :
+          nStack.push(nStack.pop() / num);
+          break;
+        case '+' :
+        case '-' :
+          nStack.push(num);
+          opStack.push(c);
+          break;
+      }
+    }
+    while(!opStack.isEmpty()) {
+      char op = opStack.pop();
+      int opd1 = nStack.pop();
+      int opd2 = nStack.pop();
+      if(op == '+') {
+        nStack.push(opd1 + opd2);
+      } else {
+        nStack.push(opd2 - opd1);
+      }
+    }
+    return nStack.pop();
+  }
+}
