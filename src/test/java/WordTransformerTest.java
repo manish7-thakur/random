90a1661a9e794e5a96c41db3132d2708f89a7633
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-09-30 22:56:35 +0700

    word transformer for multiple paths

--- src/test/java/WordTransformerTest.java
+++ src/test/java/WordTransformerTest.java
@@ -15,5 +15,17 @@ public class WordTransformerTest {
    actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag","bog", "koi"));
    expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"), Arrays.asList("dog", "bog", "bag"));
    Assert.assertEquals(expected, actual);
+
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag","bog", "koi"));
+   expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"), Arrays.asList("dog", "bog", "bag"));
+   Assert.assertEquals(expected, actual);
+
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag", "dol", "koi"));
+   expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"));
+   Assert.assertEquals(expected, actual);
+
+   actual = WordTransformer.findLadders("dog", "sac", Arrays.asList("dag", "koi", "dac", "sog", "poi", "sag"));
+   expected = Arrays.asList(Arrays.asList("dog", "sog", "sag", "sac"), Arrays.asList("dog", "dag", "dac", "sac"), Arrays.asList("dog", "dag", "sag", "sac"));
+   Assert.assertEquals(expected, actual);
   }
 }
