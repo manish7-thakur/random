80a7c0d5290750fec6c83ace3b442952eb14c518
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-02 11:44:18 +0700

    use priorityqueue to add shart transformations

--- src/main/java/WordTransformer.java
+++ src/main/java/WordTransformer.java
@@ -6,30 +6,42 @@ public class WordTransformer {
     List<String> input = new ArrayList<>();
     input.add(beginWord);
     Map<String, Boolean> map = new HashMap<>();
+    Queue<List<String>> queue = new PriorityQueue<>(new Comparator<List<String>>(){
+      @Override
+      public int compare(List<String> l1, List<String> l2) {
+        return l1.size() - l2.size();
+      }
+    });
     for(int i = 0; i < wordList.size(); i++) {
       map.put(wordList.get(i), false);
     }
-    find(beginWord, endWord, input, result, map);
+    find(beginWord, endWord, input, queue, map);
+    int minSize = 0;
+    if(!queue.isEmpty()) {
+      List<String> l = queue.remove();
+      minSize = l.size();
+      result.add(l);
+    }
+    while(!queue.isEmpty() && queue.peek().size() <= minSize) {
+      result.add(queue.remove());
+    }
     return result;
   }
-  static boolean find(String beginWord, String endWord, List<String> input, List<List<String>> result, Map<String, Boolean> map) {
-    if(isClose(beginWord, endWord)) {
-      List<String> res = new ArrayList<>(input);
-      res.add(endWord);
-      result.add(res);
-      return true;
+  static void find(String beginWord, String endWord, List<String> input, Queue<List<String>> queue, Map<String, Boolean> map) {
+    if(beginWord.equals(endWord)) {
+      queue.add(new ArrayList<>(input));
+      return;
     }
     for(Map.Entry<String, Boolean> e: map.entrySet()) {
       String s = e.getKey();
       if(!e.getValue() && isClose(beginWord, s)) {
         input.add(s);
         map.put(s, true);
-        find(s, endWord, input, result, map);
+        find(s, endWord, input, queue, map);
         input.remove(s);
         map.put(s, false);
       }
     }
-    return false;
   }
   static boolean isClose(String word1, String word2) {
     int count = 0;
--- src/test/java/WordTransformerTest.java
+++ src/test/java/WordTransformerTest.java
@@ -4,28 +4,30 @@ import java.util.*;
 public class WordTransformerTest {
   @Test
   public void findShortestTransformations() {
-   List<List<String>> actual = WordTransformer.findLadders("dog", "cog", Arrays.asList("hoi", "koi"));
+   List<List<String>> actual = WordTransformer.findLadders("dog", "cog", Arrays.asList("hoi", "cog"));
    List<List<String>> expected = Arrays.asList(Arrays.asList("dog", "cog"));
    Assert.assertEquals(expected, actual);
 
-   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("bog", "koi"));
+   actual = WordTransformer.findLadders("dog", "cog", Arrays.asList("hoi", "koi"));
+   Assert.assertTrue(actual.isEmpty());
+
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("bog", "koi", "bag"));
    expected = Arrays.asList(Arrays.asList("dog", "bog", "bag"));
    Assert.assertEquals(expected, actual);
 
-   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag","bog", "koi"));
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("bog", "koi"));
+   Assert.assertTrue(actual.isEmpty());
+
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag", "bog", "koi", "bag"));
    expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"), Arrays.asList("dog", "bog", "bag"));
    Assert.assertEquals(expected, actual);
 
-   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag","bog", "koi"));
-   expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"), Arrays.asList("dog", "bog", "bag"));
-   Assert.assertEquals(expected, actual);
-
-   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag", "dol", "koi"));
+   actual = WordTransformer.findLadders("dog", "bag", Arrays.asList("dag", "dol", "koi", "bag"));
    expected = Arrays.asList(Arrays.asList("dog", "dag", "bag"));
    Assert.assertEquals(expected, actual);
 
-   actual = WordTransformer.findLadders("dog", "sac", Arrays.asList("dag", "koi", "dac", "sog", "poi", "sag"));
-   expected = Arrays.asList(Arrays.asList("dog", "sog", "sag", "sac"), Arrays.asList("dog", "dag", "dac", "sac"), Arrays.asList("dog", "dag", "sag", "sac"));
+   actual = WordTransformer.findLadders("dog", "sac", Arrays.asList("dag", "koi", "dac", "sog", "poi", "sag", "sac"));
+   expected = Arrays.asList(Arrays.asList("dog", "sog", "sag", "sac"), Arrays.asList("dog", "dag", "sag", "sac"), Arrays.asList("dog", "dag", "dac", "sac"));
    Assert.assertEquals(expected, actual);
   }
 }
