eb6c91101a083d05de39c72bbf683128ca333165
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-03 13:38:02 +0700

    remove queue

--- src/main/java/WordTransformer.java
+++ src/main/java/WordTransformer.java
@@ -7,12 +7,6 @@ public class WordTransformer {
     input.add(beginWord);
     Map<String, Set<String>> map = new HashMap<>();
     Set<String> visited = new HashSet<>();
-    Queue<List<String>> queue = new PriorityQueue<>(new Comparator<List<String>>(){
-      @Override
-      public int compare(List<String> l1, List<String> l2) {
-        return l1.size() - l2.size();
-      }
-    });
     map.put(beginWord, new HashSet<String>());
     Queue<String> wordQueue = new LinkedList<>();
     Set<String> wordSet = new HashSet<>(wordList);
@@ -39,32 +33,20 @@ public class WordTransformer {
       }
         wordSet.removeAll(currList);
     }
-
-    find(beginWord, endWord, input, queue, map, visited);
-    int minSize = 0;
-    if(!queue.isEmpty()) {
-      List<String> l = queue.remove();
-      minSize = l.size();
-      result.add(l);
-    }
-    while(!queue.isEmpty() && queue.peek().size() <= minSize) {
-      result.add(queue.remove());
-    }
+    find(beginWord, endWord, input, result, map);
     return result;
   }
-  static void find(String beginWord, String endWord, List<String> input, Queue<List<String>> queue, Map<String, Set<String>> map, Set<String> visited) {
+  static void find(String beginWord, String endWord, List<String> input, List<List<String>> result, Map<String, Set<String>> map) {
     if(map.get(beginWord).contains(endWord)) {
       List<String> l = new ArrayList<>(input);
       l.add(endWord);
-      queue.add(l);
+      result.add(l);
       return;
     }
     for(String s : map.get(beginWord)) {
         input.add(s);
-        visited.add(s);
-        find(s, endWord, input, queue, map, visited);
+        find(s, endWord, input, result, map);
         input.remove(input.size() - 1);
-        visited.remove(s);
     }
   }
   static boolean isClose(String word1, String word2) {
--- src/test/java/WordTransformerTest.java
+++ src/test/java/WordTransformerTest.java
@@ -27,7 +27,7 @@ public class WordTransformerTest {
    Assert.assertEquals(expected, actual);
 
    actual = WordTransformer.findLadders("dog", "sac", Arrays.asList("dag", "koi", "dac", "sog", "poi", "sag", "sac"));
-   expected = Arrays.asList(Arrays.asList("dog", "sog", "sag", "sac"), Arrays.asList("dog", "dag", "sag", "sac"), Arrays.asList("dog", "dag", "dac", "sac"));
+   expected = Arrays.asList(Arrays.asList("dog", "sog", "sag", "sac"), Arrays.asList("dog", "dag", "dac", "sac"), Arrays.asList("dog", "dag", "sag", "sac"));
    Assert.assertEquals(expected, actual);
 
    actual = WordTransformer.findLadders("hit", "cog", Arrays.asList("hot","dot","dog","lot","log","cog"));
@@ -38,23 +38,24 @@ public class WordTransformerTest {
    Assert.assertTrue(actual.isEmpty());
 
    actual = WordTransformer.findLadders("qa", "sq",  Arrays.asList("si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"));
-   expected = Arrays.asList(Arrays.asList("qa", "pa", "pb", "sb", "sq"), Arrays.asList("qa", "ba", "bi", "si", "sq"), Arrays.asList("qa", "ba", "be", "se", "sq"),
-           Arrays.asList("qa", "ba", "br", "sr", "sq"), Arrays.asList("qa", "ra", "rn", "sn", "sq"), Arrays.asList("qa", "ra", "rh", "sh", "sq"),
-           Arrays.asList("qa", "ra", "re", "se", "sq"), Arrays.asList("qa", "ra", "rb", "sb", "sq"), Arrays.asList("qa", "ca", "cr", "sr", "sq"),
-           Arrays.asList("qa", "ca", "co", "so", "sq"), Arrays.asList("qa", "ca", "cm", "sm", "sq"), Arrays.asList("qa", "ca", "ci", "si", "sq"),
-           Arrays.asList("qa", "ta", "tc", "sc", "sq"), Arrays.asList("qa", "ta", "tb", "sb", "sq"), Arrays.asList("qa", "ta", "to", "so", "sq"),
-           Arrays.asList("qa", "ta", "tm", "sm", "sq"), Arrays.asList("qa", "ta", "ti", "si", "sq"), Arrays.asList("qa", "ta", "th", "sh", "sq"),
-           Arrays.asList("qa", "fa", "fe", "se", "sq"), Arrays.asList("qa", "fa", "fr", "sr", "sq"), Arrays.asList("qa", "fa", "fm", "sm", "sq"),
-           Arrays.asList("qa", "ga", "ge", "se", "sq"), Arrays.asList("qa", "ga", "go", "so", "sq"), Arrays.asList("qa", "ha", "he", "se", "sq"),
-           Arrays.asList("qa", "ha", "ho", "so", "sq"), Arrays.asList("qa", "ha", "hi", "si", "sq"), Arrays.asList("qa", "ya", "ye", "se", "sq"),
-           Arrays.asList("qa", "ya", "yb", "sb", "sq"), Arrays.asList("qa", "ya", "yo", "so", "sq"), Arrays.asList("qa", "la", "li", "si", "sq"),
-           Arrays.asList("qa", "la", "le", "se", "sq"), Arrays.asList("qa", "la", "lt", "st", "sq"), Arrays.asList("qa", "la", "lr", "sr", "sq"),
-           Arrays.asList("qa", "la", "lo", "so", "sq"), Arrays.asList("qa", "la", "ln", "sn", "sq"), Arrays.asList("qa", "ma", "mi", "si", "sq"),
-           Arrays.asList("qa", "ma", "me", "se", "sq"), Arrays.asList("qa", "ma", "mt", "st", "sq"), Arrays.asList("qa", "ma", "mr", "sr", "sq"),
-           Arrays.asList("qa", "ma", "mb", "sb", "sq"), Arrays.asList("qa", "ma", "mo", "so", "sq"), Arrays.asList("qa", "ma", "mn", "sn", "sq"),
-           Arrays.asList("qa", "na", "ni", "si", "sq"), Arrays.asList("qa", "na", "ne", "se", "sq"), Arrays.asList("qa", "na", "nb", "sb", "sq"),
-           Arrays.asList("qa", "na", "no", "so", "sq"), Arrays.asList("qa", "pa", "po", "so", "sq"), Arrays.asList("qa", "pa", "pm", "sm", "sq"),
-           Arrays.asList("qa", "pa", "pi", "si", "sq"), Arrays.asList("qa", "pa", "ph", "sh", "sq"), Arrays.asList("qa", "pa", "pt", "st", "sq"));
+   expected = Arrays.asList(
+           Arrays.asList("qa", "pa", "pb", "sb", "sq"), Arrays.asList("qa", "pa", "pt", "st", "sq"), Arrays.asList("qa", "pa", "ph", "sh", "sq"),
+           Arrays.asList("qa", "pa", "pi", "si", "sq"), Arrays.asList("qa", "pa", "pm", "sm", "sq"), Arrays.asList("qa", "pa", "po", "so", "sq"),
+           Arrays.asList("qa", "na", "no", "so", "sq"), Arrays.asList("qa", "na", "nb", "sb", "sq"), Arrays.asList("qa", "na", "ne", "se", "sq"),
+           Arrays.asList("qa", "na", "ni", "si", "sq"), Arrays.asList("qa", "ma", "mn", "sn", "sq"), Arrays.asList("qa", "ma", "mo", "so", "sq"),
+           Arrays.asList("qa", "ma", "mb", "sb", "sq"), Arrays.asList("qa", "ma", "mr", "sr", "sq"), Arrays.asList("qa", "ma", "mt", "st", "sq"),
+           Arrays.asList("qa", "ma", "me", "se", "sq"), Arrays.asList("qa", "ma", "mi", "si", "sq"), Arrays.asList("qa", "la", "ln", "sn", "sq"),
+           Arrays.asList("qa", "la", "lo", "so", "sq"), Arrays.asList("qa", "la", "lr", "sr", "sq"), Arrays.asList("qa", "la", "lt", "st", "sq"),
+           Arrays.asList("qa", "la", "le", "se", "sq"), Arrays.asList("qa", "la", "li", "si", "sq"), Arrays.asList("qa", "ya", "yo", "so", "sq"),
+           Arrays.asList("qa", "ya", "yb", "sb", "sq"), Arrays.asList("qa", "ya", "ye", "se", "sq"), Arrays.asList("qa", "ha", "hi", "si", "sq"),
+           Arrays.asList("qa", "ha", "ho", "so", "sq"), Arrays.asList("qa", "ha", "he", "se", "sq"), Arrays.asList("qa", "ga", "go", "so", "sq"),
+           Arrays.asList("qa", "ga", "ge", "se", "sq"), Arrays.asList("qa", "fa", "fm", "sm", "sq"), Arrays.asList("qa", "fa", "fr", "sr", "sq"),
+           Arrays.asList("qa", "fa", "fe", "se", "sq"), Arrays.asList("qa", "ta", "th", "sh", "sq"), Arrays.asList("qa", "ta", "ti", "si", "sq"),
+           Arrays.asList("qa", "ta", "tm", "sm", "sq"), Arrays.asList("qa", "ta", "to", "so", "sq"), Arrays.asList("qa", "ta", "tb", "sb", "sq"),
+           Arrays.asList("qa", "ta", "tc", "sc", "sq"), Arrays.asList("qa", "ca", "ci", "si", "sq"), Arrays.asList("qa", "ca", "cm", "sm", "sq"),
+           Arrays.asList("qa", "ca", "co", "so", "sq"), Arrays.asList("qa", "ca", "cr", "sr", "sq"), Arrays.asList("qa", "ra", "rb", "sb", "sq"),
+           Arrays.asList("qa", "ra", "re", "se", "sq"), Arrays.asList("qa", "ra", "rh", "sh", "sq"), Arrays.asList("qa", "ra", "rn", "sn", "sq"),
+           Arrays.asList("qa", "ba", "br", "sr", "sq"), Arrays.asList("qa", "ba", "be", "se", "sq"), Arrays.asList("qa", "ba", "bi", "si", "sq"));
    Assert.assertEquals(expected, actual);
   }
 }
