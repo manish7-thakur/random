18f8b78efcf74750776786224aaefaa7b406c335
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-08 17:35:39 +0700

    palindrome partitiioner

---
+++ src/main/java/PalindromePartitioner.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class PalindromePartitioner {
+  static public int minCut(String s) {
+    return findMinCut(s, 0, s.length() - 1);
+  }
+  static int findMinCut(String s, int start, int end) {
+    if(start >= end) {
+      return 0;
+    }
+    if(isPalindrome(s, start, end)) {
+      return 0;
+    }
+    int min = Integer.MAX_VALUE;
+    for(int i = start; i < end; i++) {
+      min = Math.min(min, 1 + findMinCut(s, start, i) + findMinCut(s, i + 1, end));
+    }
+    return min;
+  }
+  static boolean isPalindrome(String s, int start, int end) {
+    while(start < end) {
+      if(s.charAt(start) != s.charAt(end)) {
+        return false;
+      }
+      start++;
+      end--;
+    }
+    return true;
+  }
+}
---
+++ src/test/java/PalindromePartitionerTest.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+
+public class PalindromePartitionerTest {
+  @Test
+  public void findMinCuts() {
+    int actual = PalindromePartitioner.minCut("a");
+    int expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("aa");
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("ab");
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("abc");
+    expected = 2;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("abbc");
+    expected = 2;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("abbccccddd");
+    expected = 3;
+    Assert.assertEquals(expected, actual);
+
+    actual = PalindromePartitioner.minCut("dddccccbbabbccccddd");
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+  }
+}
