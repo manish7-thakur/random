31979ca9da2bb14bb5b53c0e2d7048bea1b6cd61
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-13 13:39:13 +0700

    backtrack with 2 diff words of wordbreak

--- src/main/java/WordBreakFinder.java
+++ src/main/java/WordBreakFinder.java
@@ -4,13 +4,35 @@ public class WordBreakFinder {
   static public List<String> wordBreak(String s, List<String> wordDict) {
     List<String> res = new ArrayList<>();
     Set<String> set = new HashSet<>(wordDict);
-    findWords(s, 0, s.length(), res, set);
+    List<String> curr = new ArrayList();
+    findWords(s, 0, s.length(), curr, res, set);
     return res;
   }
 
-  static void findWords(String s, int l, int h, List<String> res, Set<String> wordSet) {
-    if(wordSet.contains(s.substring(l, h))) {
-      res.add(s.substring(l, h));
+  static boolean findWords(String s, int l, int h, List<String> curr, List<String> res, Set<String> wordSet) {
+    if(l >= h) {
+      return true;
     }
+    for(int i = l + 1; i <= h; i++) {
+      if(wordSet.contains(s.substring(l, i))) {
+        curr.add(s.substring(l, i));
+        if(findWords(s, i, h, curr, res, wordSet)) {
+          if(!s.substring(i, h).isEmpty()) curr.add(s.substring(i, h));
+          addResult(res, curr);
+        } else {
+          curr.remove(curr.size() - 1);
+        }
+        curr.remove(s.substring(l, i));
+      }
+    }
+    return false;
+  }
+  static void addResult(List<String> res, List<String> curr) {
+    StringBuilder b = new StringBuilder();
+    for(int i = 0; i < curr.size(); i++) {
+      b.append(curr.get(i));
+      if(i + 1 < curr.size()) b.append(" ");
+    }
+    res.add(b.toString());
   }
 }
--- src/test/java/WordBreakFinderTest.java
+++ src/test/java/WordBreakFinderTest.java
@@ -7,5 +7,9 @@ public class WordBreakFinderTest {
     List<String> actual = WordBreakFinder.wordBreak("cat", Arrays.asList("cat"));
     List<String> expected = Arrays.asList("cat");
     Assert.assertEquals(expected, actual);
+
+    actual = WordBreakFinder.wordBreak("cat", Arrays.asList("cat", "c", "at"));
+    expected = Arrays.asList("c at", "cat");
+    Assert.assertEquals(expected, actual);
   }
 }
