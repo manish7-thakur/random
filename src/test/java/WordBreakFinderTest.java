9e2000881b2ee9cd5d8c8bf28b61e114b9e2a178
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-13 20:51:06 +0700

    assert empty if word are not in the dictionary

--- src/test/java/WordBreakFinderTest.java
+++ src/test/java/WordBreakFinderTest.java
@@ -8,6 +8,9 @@ public class WordBreakFinderTest {
     List<String> expected = Arrays.asList("cat");
     Assert.assertEquals(expected, actual);
 
+    actual = WordBreakFinder.wordBreak("cat", Arrays.asList("ca", "at", "ct"));
+    Assert.assertTrue(actual.isEmpty());
+
     actual = WordBreakFinder.wordBreak("cat", Arrays.asList("cat", "c", "at"));
     expected = Arrays.asList("c at", "cat");
     Assert.assertEquals(expected, actual);
@@ -19,5 +22,6 @@ public class WordBreakFinderTest {
     actual = WordBreakFinder.wordBreak("cat", Arrays.asList("cat", "c", "at", "ca", "t", "a"));
     expected = Arrays.asList("c a t", "c at", "ca t", "cat");
     Assert.assertEquals(expected, actual);
+
   }
 }
