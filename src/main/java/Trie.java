6fe5294b88c8aa6055f1be1d7aa9a5c10cfe5b83
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-16 22:54:48 +0700

    refactor code

--- src/main/java/Trie.java
+++ src/main/java/Trie.java
@@ -11,20 +11,18 @@ public class Trie {
     TrieNode getChild(char c) {
       return map.get(c);
     }
-    void addChild(char c, TrieNode node) {
-      map.put(c, node);
+    void addChild(char c) {
+      map.put(c, new TrieNode(c));
     }
   }
   private TrieNode root = new TrieNode(' ');
   public void insert(String str) {
     TrieNode current = root;
     for(char c : str.toCharArray()) {
-      TrieNode node = current.getChild(c);
-      if(node == null) {
-        node = new TrieNode(c);
-        current.addChild(c, node);
+      if(current.getChild(c) == null) {
+        current.addChild(c);
       }
-      current = node;
+      current = current.getChild(c);
     }
   }
 
