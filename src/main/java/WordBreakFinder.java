fc18977ba356715d4de145e65a4e0be6df3aac1c
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-16 12:35:43 +0700

    add memoization for word break

--- src/main/java/WordBreakFinder.java
+++ src/main/java/WordBreakFinder.java
@@ -4,8 +4,8 @@ public class WordBreakFinder {
   static public List<String> wordBreak(String s, List<String> wordDict) {
     List<String> res = new ArrayList<>();
     Set<String> set = new HashSet<>(wordDict);
-    int[][] mem = new int[s.length() + 1][s.length() + 1];
-    findWords(s, 0, s.length(), "", res, set, mem);
+    ArrayList<String>[][] mem = new ArrayList[s.length() + 1][s.length() + 1];
+    res = findWords(s, 0, s.length(), res, set, mem);
     return res;
   }
 
@@ -20,5 +20,26 @@ public class WordBreakFinder {
         findWords(s, i, h, curr + " " + prefix, res, wordSet, mem);
       }
     }
+  }
+
+  static List<String> findWords(String s, int l, int h, List<String> res, Set<String> wordSet, List<String>[][] mem) {
+    if(l >= h) {
+      return Arrays.asList("");
+    }
+    if(mem[l][h] != null) {
+      return mem[l][h];
+    }
+    List<String> list = new ArrayList<>();
+    for(int i = l + 1; i <= h; i++) {
+      String prefix = s.substring(l, i);
+      if(wordSet.contains(prefix)) {
+        List<String> clist = findWords(s, i, h, res, wordSet, mem);
+        for(String str: clist) {
+          list.add((prefix + " " + str).trim());
+        }
+      }
+    }
+    mem[l][h] = list;
+    return list;
   }
 }
