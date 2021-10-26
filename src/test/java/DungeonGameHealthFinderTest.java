6f0a684638fc96790953164dfd73692658cf034d
IT-GitlabBuildServer <IT-GitlabBuildServer@agoda.com>
2021-10-26 12:48:12 +0700

    dunguin gmae with 2 X2

--- src/test/java/DungeonGameHealthFinderTest.java
+++ src/test/java/DungeonGameHealthFinderTest.java
@@ -46,5 +46,12 @@ public class DungeonGameHealthFinderTest {
     actual = DungeonGameHealthFinder.calculateMinimumHP(new int[][]{{-2, 3}, {-3, -2}});
     expected = 3;
     Assert.assertEquals(expected, actual);
+
+    actual = DungeonGameHealthFinder.calculateMinimumHP(new int[][]
+    {{-2, 3, -1},
+     {-3, -8, -3},
+     {1, 2, -2}});
+    expected = 6;
+    Assert.assertEquals(expected, actual);
   }
 }
