ff3860243e673e9dc2d4f39beac0f0b340908107
mthakur <manish.thakur@agoda.com>
2021-10-29 10:41:25 +0700

    combine stock trader and max profit finder

--- src/main/java/MaxStockProfitFinder.java
+++ src/main/java/MaxStockProfitFinder.java
@@ -6,4 +6,27 @@ public class MaxStockProfitFinder {
         }
         return MaximumSubarrayFinder.maxSubarraySum(priceChange).sum;
     }
+    static public int maxProfit(int[] prices) {
+      int[] profits = new int[prices.length];
+      int maxProfit = 0, maxLeftProfit = 0, maxRightProfit = 0;
+      int min = prices[0];
+      int max = prices[prices.length - 1];
+      int l = 0, r = prices.length - 1;
+      while(l < prices.length && r >= 0) {
+        maxLeftProfit = Math.max(maxLeftProfit, prices[l] - min);
+        profits[l] += maxLeftProfit;
+        if(prices[l] < min) min = prices[l];
+
+        maxRightProfit = Math.max(maxRightProfit, max - prices[r]);
+        profits[r] += maxRightProfit;
+        if(prices[r] > max) max = prices[r];
+
+        maxProfit = Math.max(Math.max(maxProfit, profits[l]), profits[r]);
+
+        l++;
+        r--;
+      }
+
+      return maxProfit;
+    }
 }
