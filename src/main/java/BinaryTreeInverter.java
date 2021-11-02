5ea073efd7d89138a65217ada0ebd37b5faa9954
mthakur <manish.thakur@agoda.com>
2021-11-02 22:51:46 +0700

    Binary Tree right side finder

--- src/main/java/BinaryTreeInverter.java
+++ src/main/java/BinaryTreeInverter.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTreeInverter {
     public static void invert(TreeNode root) {
         if (root == null) {
@@ -8,5 +10,23 @@ public class BinaryTreeInverter {
         root.right = left;
         invert(root.left);
         invert(root.right);
+    }
+    
+    public static List<Integer> rightSideView(TreeNode root) {
+      List<Integer> res = new ArrayList<>();
+      if(root == null) return res;
+      LinkedList<TreeNode> queue = new LinkedList<>();
+      queue.add(root);
+      while(!queue.isEmpty()) {
+        int count = queue.size();
+        while(count > 0) {
+          TreeNode item = queue.poll();
+          if(item.left != null) queue.add(item.left);
+          if(item.right != null) queue.add(item.right);
+          count--;
+          if(count == 0) res.add(item.val);
+        }
+      }
+      return res;
     }
 }
--- src/test/java/BinaryTreeInverterTest.java
+++ src/test/java/BinaryTreeInverterTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.*;
 
 public class BinaryTreeInverterTest {
     @Test
@@ -33,5 +34,31 @@ public class BinaryTreeInverterTest {
         Assert.assertEquals(8, root.left.right.val);
         Assert.assertEquals(3, root.right.right.val);
         Assert.assertEquals(2, root.right.left.val);
+    }
+
+    @Test
+    public void findRightSideView() {
+      List<Integer> actual = BinaryTreeInverter.rightSideView(null);
+      Assert.assertTrue(actual.isEmpty());
+
+      TreeNode root = new TreeNode(1, null, null);
+      actual = BinaryTreeInverter.rightSideView(root);
+      List<Integer> expected = Arrays.asList(1);
+      Assert.assertEquals(expected, actual);
+
+      root = new TreeNode(1, null, new TreeNode(2));
+      actual = BinaryTreeInverter.rightSideView(root);
+      expected = Arrays.asList(1, 2);
+      Assert.assertEquals(expected, actual);
+
+      root = new TreeNode(1, new TreeNode(3), new TreeNode(2));
+      actual = BinaryTreeInverter.rightSideView(root);
+      expected = Arrays.asList(1, 2);
+      Assert.assertEquals(expected, actual);
+      
+      root = new TreeNode(1, new TreeNode(3, null, new TreeNode(4, new TreeNode(5),null)), null);
+      actual = BinaryTreeInverter.rightSideView(root);
+      expected = Arrays.asList(1, 3, 4, 5);
+      Assert.assertEquals(expected, actual);
     }
 }
