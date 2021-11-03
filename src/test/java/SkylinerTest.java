d4ab95aaf55ee1153165bea0bb37b4ad52a35262
mthakur <manish.thakur@agoda.com>
2021-11-03 17:26:34 +0700

    add cases

--- src/test/java/SkylinerTest.java
+++ src/test/java/SkylinerTest.java
@@ -23,5 +23,13 @@ public class SkylinerTest {
     actual = Skyliner.getSkyline(new int[][]{{6, 7, 2}, {7, 8, 3}});
     expected = Arrays.asList(Arrays.asList(6,2), Arrays.asList(7,3), Arrays.asList(8,0));
     Assert.assertEquals(expected, actual);
+
+    actual = Skyliner.getSkyline(new int[][]{{0, 4, 3}, {1, 3, 2}});
+    expected = Arrays.asList(Arrays.asList(0,3), Arrays.asList(4,0));
+    Assert.assertEquals(expected, actual);
+
+    actual = Skyliner.getSkyline(new int[][]{{2,9,10},{3,7,15},{5,12,12},{15,20,10},{19,24,8}});
+    expected = Arrays.asList(Arrays.asList(2,10), Arrays.asList(3,15), Arrays.asList(7,12), Arrays.asList(12,0), Arrays.asList(15,10), Arrays.asList(20,8), Arrays.asList(24,0));
+    Assert.assertEquals(expected, actual);
   }
 }
