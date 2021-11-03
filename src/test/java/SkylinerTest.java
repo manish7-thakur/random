26de606d797fa306b85365d8f67d838cc4332ea4
mthakur <manish.thakur@agoda.com>
2021-11-03 17:21:20 +0700

    skyline with special merged bulding cases

--- src/test/java/SkylinerTest.java
+++ src/test/java/SkylinerTest.java
@@ -11,5 +11,17 @@ public class SkylinerTest {
     actual = Skyliner.getSkyline(new int[][]{{0, 2, 3}, {2, 5, 3}});
     expected = Arrays.asList(Arrays.asList(0,3), Arrays.asList(5,0));
     Assert.assertEquals(expected, actual);
+
+    actual = Skyliner.getSkyline(new int[][]{{0, 2, 3}, {0, 1, 2}});
+    expected = Arrays.asList(Arrays.asList(0,3), Arrays.asList(2,0));
+    Assert.assertEquals(expected, actual);
+
+    actual = Skyliner.getSkyline(new int[][]{{3, 5, 3}, {4, 5, 2}});
+    expected = Arrays.asList(Arrays.asList(3,3), Arrays.asList(5,0));
+    Assert.assertEquals(expected, actual);
+
+    actual = Skyliner.getSkyline(new int[][]{{6, 7, 2}, {7, 8, 3}});
+    expected = Arrays.asList(Arrays.asList(6,2), Arrays.asList(7,3), Arrays.asList(8,0));
+    Assert.assertEquals(expected, actual);
   }
 }
