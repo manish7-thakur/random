30bba5382a026997d453732e8438a3ad5ffebc78
mthakur <manish.thakur@agoda.com>
2021-11-08 17:09:43 +0700

    use stream for array traversal

--- src/main/java/ArrayIntersectionFinder.java
+++ src/main/java/ArrayIntersectionFinder.java
@@ -6,22 +6,16 @@ public class ArrayIntersectionFinder {
     Map<Integer, Integer> occMap = new HashMap<>();
     if(a.length > b.length) {
       arr = a;
-      for(int i = 0; i < b.length; i++) {
-        occMap.merge(b[i], 1, Integer::sum);
-      }
+      Arrays.stream(b).forEach(e -> occMap.merge(e, 1, Integer::sum));
     } else {
       arr = b;
-      for(int i = 0; i < a.length; i++) {
-        occMap.merge(a[i], 1, Integer::sum);
-      }
+      Arrays.stream(a).forEach(e -> occMap.merge(e, 1, Integer::sum));
     }
     List<Integer> interList = new ArrayList<>();
-    for(Integer elem: arr) {
-      occMap.computeIfPresent(elem, (k, v) -> {
-        interList.add(elem);
+    Arrays.stream(arr).forEach(e -> occMap.computeIfPresent(e, (k, v) -> {
+        interList.add(e);
         return --v == 0 ? null: v;
-      });
-    }
+      }));
     return interList;
   }
 }
