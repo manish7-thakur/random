2cef1b60d8f11551d930e0d1dc246e8054b7b324
mthakur <manish.thakur@agoda.com>
2021-11-15 16:08:53 +0700

    don't do i+1 at each iteration

--- src/main/java/ExpressionEvaluator.java
+++ src/main/java/ExpressionEvaluator.java
@@ -53,9 +53,10 @@ public class ExpressionEvaluator {
     while(i < s.length()) {
       char c = s.charAt(i);
       if(Character.isDigit(c)) {
-        j = i;
-        while(i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) i++;
-        acc += Integer.parseInt(s.substring(j, i + 1)) * sign;
+        j = i++;
+        while(i < s.length() && Character.isDigit(s.charAt(i))) i++;
+        acc += Integer.parseInt(s.substring(j, i)) * sign;
+        i--;
       } else if('+' == c) {
         sign = 1;
       } else if('-' == c){
