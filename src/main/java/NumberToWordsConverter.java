d4e848e426fa49f6701ccff82b7464e142f92cf5
mthakur <manish.thakur@agoda.com>
2021-11-25 14:41:00 +0700

    words upto 99

--- src/main/java/NumberToWordsConverter.java
+++ src/main/java/NumberToWordsConverter.java
@@ -1,5 +1,20 @@
 public class NumberToWordsConverter {
+  static String[] ones = new String[]{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+  static String[] tens = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  static String[] tys = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Eighteen"};
+
   static public String numberToWords(int num) {
-    return "One";
+    StringBuilder b = new StringBuilder();
+    if(num < 10) b.append(ones[num]);
+    else if(num >= 10  && num < 20) b.append(tens[num % 10]);
+    else if(num >= 20 && num < 100) {
+      b.append(tys[num / 10]);
+      int one = num % 10;
+      if(one > 0) {
+        b. append(" ");
+        b.append(ones[one]);
+      }
+    }
+    return b.toString();
   }
 }
--- src/test/java/NumberToWordsConverterTest.java
+++ src/test/java/NumberToWordsConverterTest.java
@@ -3,8 +3,32 @@ import org.junit.*;
 public class NumberToWordsConverterTest {
   @Test
   public void testIntegerToWordsConversion() {
-    String actual = NumberToWordsConverter.numberToWords(1);
-    String expected = "One";
+    String actual = NumberToWordsConverter.numberToWords(0);
+    String expected = "Zero";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(1);
+    expected = "One";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(9);
+    expected = "Nine";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(10);
+    expected = "Ten";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(20);
+    expected = "Twenty";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(45);
+    expected = "Forty Five";
+    Assert.assertEquals(expected, actual);
+
+    actual = NumberToWordsConverter.numberToWords(99);
+    expected = "Ninety Nine";
     Assert.assertEquals(expected, actual);
   }
 }
