ef52008538b751773bfaa7f5acede95e4f467727
mthakur <manish.thakur@agoda.com>
2021-11-26 18:05:22 +0700

    add test case

--- src/test/java/SlidingWindowMaxTest.java
+++ src/test/java/SlidingWindowMaxTest.java
@@ -55,6 +55,10 @@ public class SlidingWindowMaxTest {
     expected = new int[]{2, 2, 2, -2, -1};
     Assert.assertArrayEquals(expected, actual);
 
+    actual = SlidingWindowMax.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3);
+    expected = new int[]{3, 3, 5, 5, 6, 7};
+    Assert.assertArrayEquals(expected, actual);
+
     actual = SlidingWindowMax.maxSlidingWindow(new int[]{1, 0, 2, -3, -2, -3, -1, -1, 7, 4, 2, 0, 2, 1, 4, -4, -3, -2, -3, 4, 0, 0, -3, -3, 3, 0, 4, 2, -3, -3}, 3);
     expected = new int[]{2, 2, 2, -2, -1, -1, 7, 7, 7, 4, 2, 2, 4, 4, 4, -2, -2, 4, 4, 4, 0, 0, 3, 3, 4, 4, 4, 2};
     Assert.assertArrayEquals(expected, actual);
