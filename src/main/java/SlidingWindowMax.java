83633b642d9b1ebb8304e4414d044b1231725b39
mthakur <manish.thakur@agoda.com>
2021-11-27 11:29:57 +0700

    simplifycode

--- src/main/java/SlidingWindowMax.java
+++ src/main/java/SlidingWindowMax.java
@@ -4,13 +4,12 @@ public class SlidingWindowMax {
   static public int[] maxSlidingWindow(int[] nums, int k) {
     int resultLen = nums.length - (k - 1);
     int[] result = new int[resultLen];
-    int r = 0, l = 0;
-    int h;
+    int r = 0, l = 0, h;
     Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
     Queue<Integer> removalQueue = new PriorityQueue<>(Collections.reverseOrder());
-    for(h = 0; h < k; h++) {
-      queue.add(nums[h]);
-    }
+
+    for(h = 0; h < k; h++) queue.add(nums[h]);
+
     result[r] = queue.peek();
     while(h < nums.length) {
       if(nums[l] == queue.peek()) queue.remove();
