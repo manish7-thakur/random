442257596ca25a129f81a12fb6ed67e02eb7c739
mthakur <manish.thakur@agoda.com>
2021-11-29 14:34:41 +0700

    simplify code for matrix traverser

--- src/main/java/MatrixTraverser.java
+++ src/main/java/MatrixTraverser.java
@@ -1,21 +1,12 @@
 public class MatrixTraverser {
   static public boolean searchMatrix(int[][] matrix, int target) {
-    boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-    return visit(matrix, 0, 0, target, visited);
-  }
-  static boolean visit(int[][] matrix, int i, int j, int target, boolean[][] visited) {
-    if(!safe(matrix, i , j, visited)) return false;
-    visited[i][j] = true;
-    if(matrix[i][j] == target) return true;
-    else if(matrix[i][j] < target && visit(matrix, i, j + 1, target, visited)) {
-      return true;
-    } else if(matrix[i][j] > target && visit(matrix, i, j - 1, target, visited)) {
-      return true;
+    int r = 0;
+    int c = matrix[0].length - 1;
+    while(c >= 0 && r < matrix.length) {
+      if(matrix[r][c] == target) return true;
+      else if(matrix[r][c] < target) r++;
+      else c--;
     }
-    if(visit(matrix, i + 1, j, target, visited)) return true;
     return false;
-  }
-  static boolean safe(int[][] matrix, int i, int j, boolean[][] visited) {
-    return i < matrix.length && i >= 0 && j < matrix[0].length && j >= 0 && !visited[i][j];
   }
 }
--- src/test/java/MatrixTraverserTest.java
+++ src/test/java/MatrixTraverserTest.java
@@ -31,11 +31,19 @@ public class MatrixTraverserTest {
     Assert.assertTrue(actual);
 
     actual = MatrixTraverser.searchMatrix(new int[][]{
-      {1,4,7,11,15},
-      {2,5,8,12,19},
-      {3,6,9,16,22},
+      {1, 4, 7,11,15},
+      {2, 5, 8,12,19},
+      {3, 6, 9,16,22},
       {10,13,14,17,24},
-      {18,21,23,26,30}}, 5);
+      {18,21,23,26,30}}, 18);
     Assert.assertTrue(actual);
+
+    actual = MatrixTraverser.searchMatrix(new int[][]{
+      {1, 4, 7,11,15},
+      {2, 5, 8,12,19},
+      {3, 6, 9,16,22},
+      {10,13,14,17,24},
+      {18,21,23,26,30}}, 20);
+    Assert.assertFalse(actual);
   }
 }
