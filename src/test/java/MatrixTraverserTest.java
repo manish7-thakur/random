85511ceafe488481e4fbe506d44042d12e339383
mthakur <manish.thakur@agoda.com>
2021-11-29 10:41:19 +0700

    search instant

--- src/test/java/MatrixTraverserTest.java
+++ src/test/java/MatrixTraverserTest.java
@@ -30,7 +30,12 @@ public class MatrixTraverserTest {
     actual = MatrixTraverser.searchMatrix(new int[][]{{1, 2, 4}, {5, 6, 8}, {7, 8, 9}}, 7);
     Assert.assertTrue(actual);
 
-    actual = MatrixTraverser.searchMatrix(new int[][]{{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}}, 5);
+    actual = MatrixTraverser.searchMatrix(new int[][]{
+      {1,4,7,11,15},
+      {2,5,8,12,19},
+      {3,6,9,16,22},
+      {10,13,14,17,24},
+      {18,21,23,26,30}}, 5);
     Assert.assertTrue(actual);
   }
 }
