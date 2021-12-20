bc725f79503d249fa723f662ea420878cdba5ee7
mthakur <manish.thakur@agoda.com>
2021-12-20 13:37:56 +0700

    headseen & tail seen varibale names

--- src/main/java/FlipCoins.java
+++ src/main/java/FlipCoins.java
@@ -1,16 +1,16 @@
 public class FlipCoins {
     public static int minimumCoinFlips(String coins) {
-        boolean tail = false;
+        boolean tailSeen = false;
         int headFlips = 0;
         for (int i = 0; i < coins.length(); i++) {
-            if (tail && coins.charAt(i) == 'H') headFlips++;
-            if (coins.charAt(i) == 'T') tail = true;
+            if (tailSeen && coins.charAt(i) == 'H') headFlips++;
+            if (coins.charAt(i) == 'T') tailSeen = true;
         }
-        boolean head = false;
+        boolean headSeen = false;
         int tailFlips = 0;
         for(int i = coins.length() - 1; i >= 0; i--) {
-          if(head && coins.charAt(i) == 'T') tailFlips++;
-          if(coins.charAt(i) == 'H') head = true;
+          if(headSeen && coins.charAt(i) == 'T') tailFlips++;
+          if(coins.charAt(i) == 'H') headSeen = true;
         }
         return Math.min(headFlips, tailFlips);
     }
