f573503a6ba971e267c661f786e245c252eadd79
mthakur <manish.thakur@agoda.com>
2021-12-21 10:09:53 +0700

    password strength

--- src/main/java/PassStrength.java
+++ src/main/java/PassStrength.java
@@ -3,24 +3,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PassStrength {
-    public static int findPasswordStrength(String password) {
-        HashMap<String, Integer> mem = new HashMap<>();
-        int res = 0;
-        int iter = 0;
-        while(iter < password.length()) {
-            for (int i = 0; i < password.length(); i++) {
-                int count = mem.getOrDefault(password.substring(i, iter+1), 0);
-                if(count== 0) {
-                    Set<Character> set = new HashSet<>();
-                    for (int j = i; j <= iter; j++) {
-                        set.add(password.charAt(j));
-                    }
-                    res += set.size();
-                } else res+=count;
-
-            }
-            iter++;
+    public static int findsStrength(String s) {
+      int res = 0;
+      for(int j = 0; j < s.length(); j++) {
+        int curr = 0;
+        Set<Character> set = new HashSet<>();
+        for(int i = j; i < s.length(); i++) {
+          char c = s.charAt(i);
+          if(!set.contains(c)) curr++;
+          set.add(c);
+          res += curr;
         }
-        return res;
+      }
+      return res;
     }
 }
--- src/test/java/PassStrengthTest.java
+++ src/test/java/PassStrengthTest.java
@@ -1,16 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class PassStrengthTest {
     @Test
     public void testStreagth() {
-        int actual = PassStrength.findPasswordStrength("good");
-        Assert.assertEquals(16, actual);
+      int actual = PassStrength.findsStrength("GOOD");
+      Assert.assertEquals(16, actual);
 
-        actual = PassStrength.findPasswordStrength("test");
-        Assert.assertEquals(19, actual);
+      actual = PassStrength.findsStrength("GOODO");
+      Assert.assertEquals(26, actual);
 
+      actual = PassStrength.findsStrength("TEST");
+      Assert.assertEquals(19, actual);
     }
-
 }
