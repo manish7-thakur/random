cc9c8dc807f94c7d34d3a60fe4b690ccb8210793
mthakur <manish.thakur@agoda.com>
2022-01-18 10:50:44 +0700

    combine binary search test methods

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -8,26 +8,17 @@ public class BinarySearchTest {
         String[] arr = {};
         int i = BinarySearch.binarySearch(arr, "1", 0, arr.length - 1);
         assertEquals(-1, i);
-    }
 
-    @Test
-    public void SingleElemArraySearch() {
-        String[] arr = {"a"};
-        int i = BinarySearch.binarySearch(arr, "a", 0, arr.length - 1);
+        arr = new String[]{"a"};
+        i = BinarySearch.binarySearch(arr, "a", 0, arr.length - 1);
         assertEquals(0, i);
-    }
 
-    @Test
-    public void DoubleElemArraySearch() {
-        String[] arr = {"a", "b"};
-        int i = BinarySearch.binarySearch(arr, "b", 0, arr.length - 1);
+        arr = new String[]{"a", "b"};
+        i = BinarySearch.binarySearch(arr, "b", 0, arr.length - 1);
         assertEquals(1, i);
-    }
 
-    @Test
-    public void MultiElemArraySearch() {
-        String[] arr = {"a", "b", "c", "d", "e"};
-        int i = BinarySearch.binarySearch(arr, "d", 0, arr.length - 1);
+        arr = new String[]{"a", "b", "c", "d", "e"};
+        i = BinarySearch.binarySearch(arr, "d", 0, arr.length - 1);
         assertEquals(3, i);
     }
 }
