859bf6bde9c0e187a5cd7a45b69f0c24cef6a51d
mthakur <manish.thakur@agoda.com>
2022-01-20 12:09:32 +0700

    combine mergesort tests

--- src/test/java/MergeSortTest.java
+++ src/test/java/MergeSortTest.java
@@ -13,13 +13,10 @@ public class MergeSortTest {
         MergeSort.sort(actual);
         expected = new int[]{1, 2};
         Assert.assertArrayEquals(expected, actual);
-    }
 
-    @Test
-    public void threeElemMergeSort() {
-        int[] actual = new int[]{1, 3, 2};
+        actual = new int[]{1, 3, 2};
         MergeSort.sort(actual);
-        int[] expected = new int[]{1, 2, 3};
+        expected = new int[]{1, 2, 3};
         Assert.assertArrayEquals(expected, actual);
 
         actual = new int[]{3, 2, 2};
@@ -31,13 +28,10 @@ public class MergeSortTest {
         MergeSort.sort(actual);
         expected = new int[]{1, 2, 3};
         Assert.assertArrayEquals(expected, actual);
-    }
 
-    @Test
-    public void fourElemMergeSort() {
-        int[] actual = new int[]{1, 3, 2, 4};
+        actual = new int[]{1, 3, 2, 4};
         MergeSort.sort(actual);
-        int[] expected = new int[]{1, 2, 3, 4};
+        expected = new int[]{1, 2, 3, 4};
         Assert.assertArrayEquals(expected, actual);
 
         actual = new int[]{4, 3, 2, 2};
@@ -49,13 +43,10 @@ public class MergeSortTest {
         MergeSort.sort(actual);
         expected = new int[]{1, 2, 3, 4};
         Assert.assertArrayEquals(expected, actual);
-    }
 
-    @Test
-    public void multiElemMergeSort() {
-        int[] actual = new int[]{1, 3, 2, 4, 36, 256, 23, 42, 53, 6, 2, 52, 32, 432, 32};
+        actual = new int[]{1, 3, 2, 4, 36, 256, 23, 42, 53, 6, 2, 52, 32, 432, 32};
         MergeSort.sort(actual);
-        int[] expected = new int[]{1, 2, 2, 3, 4, 6, 23, 32, 32, 36, 42, 52, 53, 256, 432};
+        expected = new int[]{1, 2, 2, 3, 4, 6, 23, 32, 32, 36, 42, 52, 53, 256, 432};
         Assert.assertArrayEquals(expected, actual);
     }
 }
