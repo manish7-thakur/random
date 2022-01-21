af3c253a6ffd0e792966fcf436450ad70b626eca
mthakur <manish.thakur@agoda.com>
2022-01-21 12:07:12 +0700

    Fix tests for Arranger

--- src/main/java/ArrayArranger.java
+++ src/main/java/ArrayArranger.java
@@ -1,9 +1,9 @@
 
-/**
- * Arrange elements in such a way that in any pair of 3 the middle element greater than both left & right
- * 7,5,3,6,2,4 -> 5<=7>=3<=6>=2<=4
- */
 public class ArrayArranger {
+  /**
+   * Arrange elements in such a way that in any pair of 3 the middle element greater than both left & right
+   * 7,5,3,6,2,4 -> 5<=7>=3<=6>=2<=4
+   */
     public static int[] arrange(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             if (i % 2 != 0) {
