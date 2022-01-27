e70fff4df6bc18c75150f63503f5e088134d26a7
mthakur <manish.thakur@agoda.com>
2022-01-27 15:47:21 +0700

    flip statmenst to getkadane algo

--- src/main/java/ArrayArranger.java
+++ src/main/java/ArrayArranger.java
@@ -45,8 +45,8 @@ public class ArrayArranger {
 
       for(int i = 0; i < nums.length; i++) {
         currSum += nums[i];
-        if(nums[i] > currSum) currSum = nums[i];
         if(currSum > res) res = currSum;
+        if(currSum < 0) currSum = 0;
       }
       return res;
     }
