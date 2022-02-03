370ecff09c13c165bffd68fdb4f352de0274a434
mthakur <manish.thakur@agoda.com>
2022-02-03 12:26:03 +0700

    merge array intersection

--- src/main/java/ArrayArranger.java
+++ src/main/java/ArrayArranger.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class ArrayArranger {
   /**
@@ -67,5 +68,22 @@ public class ArrayArranger {
         k--;
         j--;
       }
+    }
+    public static List<Integer> intersect(int[] a, int[] b) {
+      int[] arr;
+      Map<Integer, Integer> occMap = new HashMap<>();
+      if(a.length > b.length) {
+        arr = a;
+        Arrays.stream(b).forEach(e -> occMap.merge(e, 1, Integer::sum));
+      } else {
+        arr = b;
+        Arrays.stream(a).forEach(e -> occMap.merge(e, 1, Integer::sum));
+      }
+      List<Integer> interList = new ArrayList<>();
+      Arrays.stream(arr).forEach(e -> occMap.computeIfPresent(e, (k, v) -> {
+          interList.add(e);
+          return --v == 0 ? null: v;
+        }));
+      return interList;
     }
 }
