642ed0f3b96cdeb8f278dc1402b9a30b7da4d68a
mthakur <manish.thakur@agoda.com>
2022-03-04 11:26:55 +0700

    Bit manipulation and adding without using plus

---
+++ src/main/java/BitMania.java
@@ -0,0 +1,11 @@
+public class BitMania {
+  static public int getSum(int a, int b) {
+    int sum = 0;
+    while(b != 0) {
+      sum = a ^ b;
+      b = (a & b) << 1;
+      a = sum;
+    }
+    return sum;
+  }
+}
---
+++ src/test/java/BitManiaTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+public class BitManiaTest {
+  @Test
+  public void addTwoNumsWithoutusingPlus() {
+    int actual = BitMania.getSum(0, 1);
+    int expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = BitMania.getSum(-1, 1);
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = BitMania.getSum(-1, -3);
+    expected = -4;
+    Assert.assertEquals(expected, actual);
+
+    actual = BitMania.getSum(4, 4);
+    expected = 8;
+    Assert.assertEquals(expected, actual);
+  }
+}
