b9c84c60c5669638c47ed155cfb89bc8763bf0c1
mthakur <manish.thakur@agoda.com>
2022-03-25 14:38:08 +0700

    clean up code avoiding repeated overwritings

--- src/main/java/ArrayMania.java
+++ src/main/java/ArrayMania.java
@@ -164,13 +164,11 @@ public class ArrayMania {
       }
     }
     for(int c = 1; c < matrix[0].length; c++) {
-      if(matrix[0][c] == 0) {
-        for(int r = 1; r < matrix.length; r++) matrix[r][c] = 0;
+      for(int r = 1; r < matrix.length; r++) {
+        if(matrix[r][0] == 0 || matrix[0][c] == 0) matrix[r][c] = 0;
       }
     }
-    for(int r = 0; r < matrix.length; r++) {
-      if(matrix[r][0] == 0) Arrays.fill(matrix[r], 0);
-    }
+    if(matrix[0][0] == 0) Arrays.fill(matrix[0], 0);
     if(firstColumnZero) {
       for(int r = 0; r < matrix.length; r++) matrix[r][0] = 0;
     }
