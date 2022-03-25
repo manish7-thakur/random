cc0d2acc8c6f0eda510096689d89c5e41be17ffe
mthakur <manish.thakur@agoda.com>
2022-03-25 13:12:38 +0700

    fix edge cases where col was hardcode zero

--- src/test/java/ArrayManiaTest.java
+++ src/test/java/ArrayManiaTest.java
@@ -325,17 +325,27 @@ public class ArrayManiaTest {
 
     actual = new int[][]{{1, 1, 3}, {3, 1, 0}, {0, 1, 0}};
     ArrayMania.setZeroes(actual);
-    expected = new int[][]{{0, 1, 0}, {0, 0, 0}, {0 , 0, 0}};
+    expected = new int[][]{{0, 1, 0}, {0, 0, 0}, {0, 0, 0}};
     Assert.assertArrayEquals(expected, actual);
 
     actual = new int[][]{{1, 1, 0}, {3, 1, 3}, {0, 1, 2}};
     ArrayMania.setZeroes(actual);
-    expected = new int[][]{{0, 0, 0}, {0, 1, 0}, {0 , 0, 0}};
+    expected = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
     Assert.assertArrayEquals(expected, actual);
 
     actual = new int[][]{{0, 1, 1}, {3, 1, 3}, {1, 1, 2}};
     ArrayMania.setZeroes(actual);
-    expected = new int[][]{{0, 0, 0}, {0, 1, 3}, {0 , 1, 2}};
+    expected = new int[][]{{0, 0, 0}, {0, 1, 3}, {0, 1, 2}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    ArrayMania.setZeroes(actual);
+    expected = new int[][]{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    ArrayMania.setZeroes(actual);
+    expected = new int[][]{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
     Assert.assertArrayEquals(expected, actual);
   }
 }
