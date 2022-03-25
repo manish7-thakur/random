fc104d6cee225ca34ad642ecf434c2e4176cf327
mthakur <manish.thakur@agoda.com>
2022-03-25 17:22:13 +0700

    break when size is equal to total

--- src/test/java/ArrayManiaTest.java
+++ src/test/java/ArrayManiaTest.java
@@ -377,5 +377,9 @@ public class ArrayManiaTest {
     actual = ArrayMania.spiralOrder(new int[][]{{1, 3, 4, 9}, {2, 5, 6, 3}, {8, 9, 2, 6}, {3, 5, 6, 7}});
     expected = List.of(1, 3, 4, 9, 3, 6, 7, 6, 5, 3, 8, 2, 5, 6, 2, 9);
     Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania.spiralOrder(new int[][]{{1, 2, 4, 6, 8, 9}, {2, 1, 4, 8, 9, 5}, {3, 1, 4, 6, 8, 9}, {8, 1, 7, 6, 5, 4}});
+    expected = List.of(1, 2, 4, 6, 8, 9, 5, 9, 4, 5, 6, 7, 1, 8, 3, 2, 1, 4, 8, 9, 8, 6, 4, 1);
+    Assert.assertEquals(expected, actual);
   }
 }
