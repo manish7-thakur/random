e3e1d661bec27cc3fc7bad26537bda24d7719b68
mthakur <manish.thakur@agoda.com>
2022-03-31 12:16:48 +0700

    use for loop than while

--- src/main/java/StringMania.java
+++ src/main/java/StringMania.java
@@ -20,18 +20,17 @@ public class StringMania {
     return res;
   }
   public static int characterReplacement(String s, int k) {
-    int l = 0, r = 0, res = 0, highestFrequency = 0;
+    int l = 0, res = 0, highestFrequency = 0;
     Map<Character, Integer> map = new HashMap<>();
-    while(r < s.length()) {
+    for(int r = 0; r < s.length(); r++) {
       int count = map.merge(s.charAt(r), 1, Integer::sum);
       if(count > highestFrequency) highestFrequency = count;
       int otherChars = (r - l + 1) - highestFrequency;
-      r++;
       if(otherChars > k) {
         map.compute(s.charAt(l), (c, v) -> --v);
         l++;
       } else {
-        int newlen = r - l;
+        int newlen = r - l + 1;
         if(newlen > res) res = newlen;
       }
     }
