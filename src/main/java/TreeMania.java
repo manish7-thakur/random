8cafba8150198e45a6e5009a4219694a0112cabf
mthakur <manish.thakur@agoda.com>
2022-04-04 12:00:45 +0700

    invert tree recursion

--- src/main/java/TreeMania.java
+++ src/main/java/TreeMania.java
@@ -45,4 +45,11 @@ public class TreeMania {
     }
     return true;
   }
+  static TreeNode invertTree(TreeNode root) {
+    if(root == null) return root;
+    TreeNode temp = invertTree(root.right);
+    root.right = invertTree(root.left);
+    root.left = temp;
+    return root;
+  }
 }
--- src/test/java/TreeManiaTest.java
+++ src/test/java/TreeManiaTest.java
@@ -52,4 +52,37 @@ public class TreeManiaTest {
     actual = TreeMania.isSameTreeIter(root, root);
     Assert.assertTrue(actual);
   }
+  @Test
+  public void invertBinaryTree() {
+    TreeNode actual = TreeMania.invertTree(null);
+    Assert.assertNull(actual);
+
+    TreeNode root = new TreeNode(1, new TreeNode(2));
+    actual = TreeMania.invertTree(root);
+    Assert.assertEquals(1, actual.val);
+    Assert.assertEquals(2, actual.right.val);
+
+    root = new TreeNode(1, null, new TreeNode(2));
+    actual = TreeMania.invertTree(root);
+    Assert.assertEquals(1, actual.val);
+    Assert.assertEquals(2, actual.left.val);
+
+    root = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2, new TreeNode(6)));
+    actual = TreeMania.invertTree(root);
+    Assert.assertEquals(1, actual.val);
+    Assert.assertEquals(2, actual.left.val);
+    Assert.assertEquals(6, actual.left.right.val);
+    Assert.assertEquals(3, actual.right.val);
+    Assert.assertEquals(5, actual.right.right.val);
+
+    root = new TreeNode(1, new TreeNode(3, new TreeNode(5), new TreeNode(4)), new TreeNode(2, new TreeNode(6), new TreeNode(7)));
+    actual = TreeMania.invertTree(root);
+    Assert.assertEquals(1, actual.val);
+    Assert.assertEquals(2, actual.left.val);
+    Assert.assertEquals(6, actual.left.right.val);
+    Assert.assertEquals(7, actual.left.left.val);
+    Assert.assertEquals(3, actual.right.val);
+    Assert.assertEquals(5, actual.right.right.val);
+    Assert.assertEquals(4, actual.right.left.val);
+  }
 }
