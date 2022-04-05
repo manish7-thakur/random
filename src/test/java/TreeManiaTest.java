2891c38abeb762652968e9faf56a34302ad60a2c
mthakur <manish.thakur@agoda.com>
2022-04-05 11:53:05 +0700

    isSubtree test cases

--- src/test/java/TreeManiaTest.java
+++ src/test/java/TreeManiaTest.java
@@ -128,5 +128,17 @@ public class TreeManiaTest {
 
     actual = TreeMania.isSubtree(new TreeNode(2, new TreeNode(2)), new TreeNode(2));
     Assert.assertTrue(actual);
+
+    actual = TreeMania.isSubtree(new TreeNode(2, new TreeNode(1, new TreeNode(3, new TreeNode(4)))), new TreeNode(3, new TreeNode(4)));
+    Assert.assertTrue(actual);
+
+    actual = TreeMania.isSubtree(new TreeNode(2, new TreeNode(1, new TreeNode(3, null, new TreeNode(4)))), new TreeNode(3, new TreeNode(4)));
+    Assert.assertFalse(actual);
+
+    actual = TreeMania.isSubtree(new TreeNode(2, new TreeNode(1, new TreeNode(3), new TreeNode(3, new TreeNode(4), new TreeNode(2)))), new TreeNode(3, new TreeNode(4)));
+    Assert.assertFalse(actual);
+
+    actual = TreeMania.isSubtree(new TreeNode(2, new TreeNode(1, new TreeNode(3, new TreeNode(4)), new TreeNode(3, new TreeNode(4, new TreeNode(2))))), new TreeNode(3, new TreeNode(4)));
+    Assert.assertTrue(actual);
   }
 }
