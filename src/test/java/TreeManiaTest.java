73d7df378f8c1663ae9ec9539a31535d62e72429
mthakur <manish.thakur@agoda.com>
2022-04-08 10:39:13 +0700

    more testcases

--- src/test/java/TreeManiaTest.java
+++ src/test/java/TreeManiaTest.java
@@ -260,5 +260,12 @@ public class TreeManiaTest {
     Assert.assertEquals(7, actual.left.right.val);
     Assert.assertEquals(4, actual.right.val);
     Assert.assertEquals(3, actual.right.left.val);
+
+    actual = TreeMania.buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7});
+    Assert.assertEquals(3, actual.val);
+    Assert.assertEquals(9, actual.left.val);
+    Assert.assertEquals(20, actual.right.val);
+    Assert.assertEquals(15, actual.right.left.val);
+    Assert.assertEquals(7, actual.right.right.val);
   }
 }
