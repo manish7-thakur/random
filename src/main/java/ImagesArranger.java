6bd3961b30ed40968ffbc069f7cdbb0953960846
mthakur <manish.thakur@agoda.com>
2022-04-28 10:29:45 +0700

    Images arranger code improved

--- src/main/java/ImagesArranger.java
+++ src/main/java/ImagesArranger.java
@@ -7,18 +7,21 @@ public class ImagesArranger {
     String[] imgStr = images.split("\n");
     String[][] imgInfo = new String[imgStr.length][3];
     for(int i = 0; i < imgStr.length; i++) {
-      imgInfo[i] = imgStr[i].split(",");
+      String[] info = imgStr[i].split(",");
+      imgInfo[i][0] = info[0].substring(info[0].indexOf("."));
+      imgInfo[i][1] = info[1].trim();
+      imgInfo[i][2] = info[2].trim().replace(' ', 'T');
     }
-    Map<String, Map<LocalDateTime, String>> map = Arrays.stream(imgInfo).collect(groupingBy(arr -> arr[1].trim(),
-            toMap(arr -> LocalDateTime.parse(arr[2].trim().replace(' ', 'T')), str -> "",
+    Map<String, Map<LocalDateTime, String>> map = Arrays.stream(imgInfo).collect(groupingBy(arr -> arr[1],
+            toMap(arr -> LocalDateTime.parse(arr[2]), str -> "",
                     (str1, str2) -> str1 /*executed to resolve duplicate key, how to merge previous & newer */, TreeMap::new)));
     assignSeq(map);
     StringBuilder b = new StringBuilder();
     for(String[] info: imgInfo) {
-      String city = info[1].trim();
-      LocalDateTime dateTime = LocalDateTime.parse(info[2].trim().replace(' ', 'T'));
+      String city = info[1];
+      LocalDateTime dateTime = LocalDateTime.parse(info[2]);
       String seq = map.get(city).get(dateTime);
-      b.append(city).append(seq).append(info[0].substring(info[0].indexOf(".")));
+      b.append(city).append(seq).append(info[0]);
       b.append('\n');
     }
     return b.toString();
