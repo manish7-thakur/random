2b24a84a7fe061be2b4002c3f10da357632bd706
mthakur <manish.thakur@agoda.com>
2022-05-10 12:57:29 +0700

    return when k < 0

--- src/main/java/ArrayMania2.java
+++ src/main/java/ArrayMania2.java
@@ -263,7 +263,7 @@ public class ArrayMania2 {
         res.add(new ArrayList<>(curr));
         return;
       }
-      if(k < 0) return;
+      if(k < 0 || start > n) return; // to make it more efficient
       for(int i = start; i < 10; i++) {
         if(i <= n) {
           curr.add(i);
