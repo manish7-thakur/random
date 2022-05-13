ec46ec9674d8939e43e463e9b9a71dd7622a726b
mthakur <manish.thakur@agoda.com>
2022-05-13 14:55:45 +0700

    majority form 5 elems

--- src/test/java/ArrayMania3Test.java
+++ src/test/java/ArrayMania3Test.java
@@ -54,6 +54,10 @@ public class ArrayMania3Test {
     actual = ArrayMania3.majorityElement3(new int[]{3, 1, 2, 3, 2});
     expected = List.of(3, 2);
     Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania3.majorityElement3(new int[]{2, 1, 4, 2, 3, 2, 8});
+    expected = List.of(2);
+    Assert.assertEquals(expected, actual);
   }
 
 }
