45abc1861f16eb9af18566e465ecd34eadb78224
mthakur <manish.thakur@agoda.com>
2022-05-13 14:47:04 +0700

    majority elem of 1/3

--- src/main/java/ArrayMania3.java
+++ src/main/java/ArrayMania3.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class ArrayMania3 {
   static int majorityElement(int[] nums) {
     int res = 0, count = 0;
@@ -8,9 +10,33 @@ public class ArrayMania3 {
     }
     return res;
   }
-  static int majorityElement(int[] nums) {
+  static List<Integer> majorityElement3(int[] nums) {
     List<Integer> res = new ArrayList<>();
-    res.add(nums[0]);
+    int num1 = 0, num2 = 0, c1 = 0, c2 = 0;
+    for(int n : nums) {
+      if(num1 == n) c1++;
+      else if(num2 == n) c2++;
+      if(c1 == 0) {
+        num1 = n;
+        c1++;
+      }
+      else if(c2 == 0) {
+        num2 = n;
+        c2++;
+      } else {
+        c1--;
+        c2--;
+      }
+    }
+    int majMark = nums.length / 3;
+    c1 = 0;
+    c2 = 0;
+    for(int n : nums) {
+      if(num1 == n) c1++;
+      if(num2 == n) c2++;
+    }
+    if(c1 > majMark) res.add(num1);
+    if(c2 > majMark) res.add(num2);
     return res;
   }
 }
--- src/test/java/ArrayMania3Test.java
+++ src/test/java/ArrayMania3Test.java
@@ -1,4 +1,5 @@
 import org.junit.*;
+import java.util.*;
 
 public class ArrayMania3Test {
   @Test
@@ -28,6 +29,22 @@ public class ArrayMania3Test {
   public void findOneThirdMajorityElement() {
     List<Integer> actual = ArrayMania3.majorityElement3(new int[]{1});
     List<Integer> expected = List.of(1);
+    Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania3.majorityElement3(new int[]{2, 1});
+    expected = List.of(2, 1);
+    Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania3.majorityElement3(new int[]{2, 1, 3});
+    expected = List.of();
+    Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania3.majorityElement3(new int[]{2, 1, 2});
+    expected = List.of(2);
+    Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania3.majorityElement3(new int[]{3, 1, 2, 2});
+    expected = List.of(2);
     Assert.assertEquals(expected, actual);
   }
 
