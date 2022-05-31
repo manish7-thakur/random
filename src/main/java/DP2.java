bf47bcd1f39e2f3a266848a86729c657ed0c0352
mthakur <manish.thakur@agoda.com>
2022-05-31 11:33:41 +0700

    return sum partition

---
+++ src/main/java/DP2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DP2 {
+  static boolean canPartition(int[] nums) {
+    int sum = Arrays.stream(nums).sum();
+    return canPartitionRec(nums, sum, nums.length - 1, 0);
+  }
+  static boolean canPartitionRec(int[] nums, int sum, int i, int otherSum) {
+    if(i < 0) return false;
+    if(sum == otherSum) return true;
+    return canPartitionRec(nums, sum - nums[i], i - 1, otherSum + nums[i]);
+  }
+}
---
+++ src/test/java/DP2Test.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+
+public class DP2Test {
+  @Test
+  public void partionWith2EqualSubsetSum() {
+    boolean actual = DP2.canPartition(new int[]{2});
+    Assert.assertFalse(actual);
+
+    actual = DP2.canPartition(new int[]{2, 2});
+    Assert.assertTrue(actual);
+  }
+}
