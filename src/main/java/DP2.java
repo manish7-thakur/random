e36c0e5ee366f07be2d72c775174bb3a5e277112
mthakur <manish.thakur@agoda.com>
2022-05-31 11:56:05 +0700

    add meoization

--- src/main/java/DP2.java
+++ src/main/java/DP2.java
@@ -2,11 +2,19 @@ import java.util.*;
 public class DP2 {
   static boolean canPartition(int[] nums) {
     int sum = Arrays.stream(nums).sum();
-    return canPartitionRec(nums, sum, nums.length - 1, 0);
+    Map<String, Boolean> mem = new HashMap<>();
+    return canPartitionRec(nums, sum, nums.length - 1, 0, mem);
   }
-  static boolean canPartitionRec(int[] nums, int sum, int i, int otherSum) {
+  static boolean canPartitionRec(int[] nums, int sum, int i, int otherSum, Map<String, Boolean> mem) {
     if(i < 0) return false;
+    String key = getKey(i, otherSum);
+    if(mem.containsKey(key)) return mem.get(key);
     if(sum == otherSum) return true;
-    return canPartitionRec(nums, sum - nums[i], i - 1, otherSum + nums[i]) || canPartitionRec(nums, sum, i - 1, otherSum);
+    boolean res = canPartitionRec(nums, sum - nums[i], i - 1, otherSum + nums[i], mem) || canPartitionRec(nums, sum, i - 1, otherSum, mem);
+    mem.put(key, res);
+    return res;
+  }
+  static String getKey(int i, int j) {
+    return i + "," + j;
   }
 }
--- src/test/java/DP2Test.java
+++ src/test/java/DP2Test.java
@@ -11,5 +11,20 @@ public class DP2Test {
 
     actual = DP2.canPartition(new int[]{1, 2, 1});
     Assert.assertTrue(actual);
+
+    actual = DP2.canPartition(new int[]{1, 3, 2, 1});
+    Assert.assertFalse(actual);
+
+    actual = DP2.canPartition(new int[]{1, 5, 11, 5});
+    Assert.assertTrue(actual);
+
+    actual = DP2.canPartition(new int[]{3, 1, 2, 1, 3});
+    Assert.assertTrue(actual);
+
+    actual = DP2.canPartition(new int[]{35, 12, 22, 11, 34, 5, 6, 13, 22, 3, 21, 72, 12, 33, 53, 23, 12, 34, 34, 86, 46, 32, 24, 32, 42, 23, 24, 52, 67, 63});
+    Assert.assertTrue(actual);
+
+    actual = DP2.canPartition(new int[]{35, 1, 22, 11, 34, 5, 6, 13, 22, 3, 21, 72, 12, 33, 53, 23, 12, 34, 34, 86, 46, 32, 24, 32, 42, 23, 24, 52, 67, 63});
+    Assert.assertFalse(actual);
   }
 }
