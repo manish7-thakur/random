5c6384cd0988416be4a82c4b0340704c639f1b35
mthakur <manish.thakur@agoda.com>
2022-05-31 16:49:20 +0700

    more test cases

--- src/main/java/StringMania2.java
+++ src/main/java/StringMania2.java
@@ -1,15 +1,23 @@
 public class StringMania2 {
   static int atoi(String s) {
-    int res = 0, sign = 1;
-    for(int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      if(Character.isDigit(c)) {
-        int digit = Character.getNumericValue(c);
-        if(res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
-          return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        res = 10 * res + digit;
-      } else if(c == '-' && res == 0) sign = -sign;
+    int res = 0, sign = 1, index = 0;
+    int n = s.length();
+    while (index < n && s.charAt(index) == ' ') {
+      index++;
+    }
+    if (index < n && s.charAt(index) == '+') {
+      index++;
+    } else if (index < n && s.charAt(index) == '-') {
+      sign = -1;
+      index++;
+    }
+    while(index < n && Character.isDigit(s.charAt(index))) {
+      int digit = Character.getNumericValue(s.charAt(index));
+      if(res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+        return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+      }
+      res = 10 * res + digit;
+      index++;
     }
     return res * sign;
   }
--- src/test/java/StringMania2Test.java
+++ src/test/java/StringMania2Test.java
@@ -20,19 +20,35 @@ public class StringMania2Test {
     Assert.assertEquals(expected, actual);
 
     actual = StringMania2.atoi("- 326 ");
-    expected = -326;
+    expected = 0;
     Assert.assertEquals(expected, actual);
 
     actual = StringMania2.atoi("- 34326 with words ");
-    expected = -34326;
+    expected = 0;
     Assert.assertEquals(expected, actual);
 
     actual = StringMania2.atoi(" 423645236423644352 with words ");
     expected = 2147483647;
     Assert.assertEquals(expected, actual);
 
-    actual = StringMania2.atoi("- 423645236423644352 with words ");
+    actual = StringMania2.atoi("-423645236423644352 with words ");
     expected = -2147483648;
+    Assert.assertEquals(expected, actual);
+
+    actual = StringMania2.atoi("words and 987");
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = StringMania2.atoi("-63 words and - 987");
+    expected = -63;
+    Assert.assertEquals(expected, actual);
+
+    actual = StringMania2.atoi("+1");
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = StringMania2.atoi("+-1");
+    expected = 0;
     Assert.assertEquals(expected, actual);
   }
 }
