701d0fbecf20072014f4ff75791ffc9749789660
mthakur <manish.thakur@agoda.com>
2022-05-31 12:17:21 +0700

    chekc half the sum

--- src/main/java/DP2.java
+++ src/main/java/DP2.java
@@ -3,14 +3,15 @@ public class DP2 {
   static boolean canPartition(int[] nums) {
     int sum = Arrays.stream(nums).sum();
     Map<String, Boolean> mem = new HashMap<>();
-    return canPartitionRec(nums, sum, nums.length - 1, 0, mem);
+    if(sum % 2 != 0) return false;
+    return canPartitionRec(nums, sum / 2, nums.length - 1, mem);
   }
-  static boolean canPartitionRec(int[] nums, int sum, int i, int otherSum, Map<String, Boolean> mem) {
+  static boolean canPartitionRec(int[] nums, int sum, int i, Map<String, Boolean> mem) {
     if(i < 0) return false;
-    String key = getKey(i, otherSum);
+    String key = getKey(i, sum);
     if(mem.containsKey(key)) return mem.get(key);
-    if(sum == otherSum) return true;
-    boolean res = canPartitionRec(nums, sum - nums[i], i - 1, otherSum + nums[i], mem) || canPartitionRec(nums, sum, i - 1, otherSum, mem);
+    if(sum == 0) return true;
+    boolean res = canPartitionRec(nums, sum - nums[i], i - 1, mem) || canPartitionRec(nums, sum, i - 1, mem);
     mem.put(key, res);
     return res;
   }
