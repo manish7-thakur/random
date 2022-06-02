3b3cc693cdc66dbbff67e4f43585cfc95d408abf
mthakur <manish.thakur@agoda.com>
2022-06-02 12:12:41 +0700

    mark visited as fasle

--- src/main/java/MatrixMania.java
+++ src/main/java/MatrixMania.java
@@ -1,6 +1,7 @@
 public class MatrixMania {
   static int[][] nearestZeroCell(int[][] mat) {
     boolean[][] visited = new boolean[mat.length][mat[0].length];
+    Integer[][] mem = new Integer[mat.length][mat[0].length];
     for(int i = 0; i < mat.length; i++) {
       for(int j = 0; j < mat[0].length; j++) {
         if(mat[i][j] == 1) nearestZeroCellRec(mat, i, j, visited);
@@ -10,6 +11,7 @@ public class MatrixMania {
   }
   static int nearestZeroCellRec(int[][] mat, int i, int j, boolean[][] visited) {
     if(!safe(mat, i, j, visited)) return Integer.MAX_VALUE;
+    if(mem[i][j] != null) return mem[i][j];
     if(visited[i][j]) return mat[i][j];
     visited[i][j] = true;
     if(mat[i][j] == 0) return 0;
@@ -17,6 +19,7 @@ public class MatrixMania {
     int min2 = Math.min(nearestZeroCellRec(mat, i + 1, j, visited), nearestZeroCellRec(mat, i, j + 1, visited));
     int min = Math.min(min1, min2);
     mat[i][j] = 1 + min;
+    mem[i][j] = mat[i][j];
     visited[i][j] = false;
     return mat[i][j];
   }
