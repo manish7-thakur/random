e917872c4b6d5ce150c7c4aaa328c184c3c60bf1
mthakur <manish.thakur@agoda.com>
2022-06-02 11:22:47 +0700

    dfs with matrix

---
+++ src/main/java/MatrixMania.java
@@ -0,0 +1,25 @@
+public class MatrixMania {
+  static int[][] nearestZeroCell(int[][] mat) {
+    boolean[][] visited = new boolean[mat.length][mat[0].length];
+    for(int i = 0; i < mat.length; i++) {
+      for(int j = 0; j < mat[0].length; j++) {
+        if(mat[i][j] == 1) nearestZeroCellRec(mat, i, j, visited);
+      }
+    }
+    return mat;
+  }
+  static int nearestZeroCellRec(int[][] mat, int i, int j, boolean[][] visited) {
+    if(!safe(mat, i, j, visited)) return Integer.MAX_VALUE;
+    if(visited[i][j]) return mat[i][j];
+    visited[i][j] = true;
+    if(mat[i][j] == 0) return 0;
+    int min1 = Math.min(nearestZeroCellRec(mat, i - 1, j, visited), nearestZeroCellRec(mat, i, j - 1, visited));
+    int min2 = Math.min(nearestZeroCellRec(mat, i + 1, j, visited), nearestZeroCellRec(mat, i, j + 1, visited));
+    int min = Math.min(min1, min2);
+    mat[i][j] = 1 + min;
+    return mat[i][j];
+  }
+  static boolean safe(int[][] mat, int i, int j, boolean[][] visited) {
+    return i >= 0 && i < mat.length && j >= 0 && j < mat[0].length;
+  }
+}
---
+++ src/test/java/MatrixManiaTest.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+
+public class MatrixManiaTest {
+  @Test
+  public void nearestDistanceToZeroForEachCell() {
+    int[][] actual = new int[][]{{0}};
+    MatrixMania.nearestZeroCell(actual);
+    int[][] expected = new int[][]{{0}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 1, 0}};
+    MatrixMania.nearestZeroCell(actual);
+    expected = new int[][]{{2, 1, 0}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 1, 0}, {0, 1, 0}};
+    MatrixMania.nearestZeroCell(actual);
+    expected = new int[][]{{1, 1, 0}, {0, 1, 0}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 1, 0}, {1, 1, 0}};
+    MatrixMania.nearestZeroCell(actual);
+    expected = new int[][]{{2, 1, 0}, {2, 1, 0}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    MatrixMania.nearestZeroCell(actual);
+    expected = new int[][]{{2, 1, 2}, {1, 0, 1}, {2, 1, 2}};
+    Assert.assertArrayEquals(expected, actual);
+
+    actual = new int[][]{{1, 0, 1}, {1, 0, 1}, {0, 1, 0}};
+    MatrixMania.nearestZeroCell(actual);
+    expected = new int[][]{{1, 0, 1}, {1, 0, 1}, {0, 1, 0}};
+    Assert.assertArrayEquals(expected, actual);
+  }
+}
