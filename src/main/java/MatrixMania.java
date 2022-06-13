a9bfb10d4c7c748b199c53bd469ec3a2740213ac
mthakur <manish.thakur@agoda.com>
2022-06-13 12:02:30 +0700

    use only changed cells

--- src/main/java/MatrixMania.java
+++ src/main/java/MatrixMania.java
@@ -1,31 +1,36 @@
+import java.util.*;
+
 public class MatrixMania {
   static int[][] nearestZeroCell(int[][] mat) {
+    record Pair(int i, int j){};
+    Queue<Pair> queue = new LinkedList<>();
     for(int i = 0; i < mat.length; i++) {
       for(int j = 0; j < mat[0].length; j++) {
         if(mat[i][j] == 1) mat[i][j] = Integer.MAX_VALUE;
+        else if(mat[i][j] == 0) queue.add(new Pair(i, j));
       }
     }
-    boolean goAgain = true;
-    while(goAgain) {
-      goAgain = false;
-      for(int i = 0; i < mat.length; i++) {
-        for(int j = 0; j < mat[0].length; j++) {
-          int min = getMin(mat, i, j);
-          if(min != Integer.MAX_VALUE && 1 + min < mat[i][j]) {
-            goAgain = true;
-            mat[i][j] = 1 + min;
-          }
-        }
+    while(!queue.isEmpty()) {
+      Pair p = queue.remove();
+      int i = p.i;
+      int j = p.j;
+      if(i - 1 >= 0 && 1 + mat[i][j] < mat[i - 1][j]) {
+        mat[i - 1][j] = 1 + mat[i][j];
+        queue.add(new Pair(i - 1, j));
+      }
+      if(i + 1 < mat.length && 1 + mat[i][j] < mat[i + 1][j]) {
+        mat[i + 1][j] = 1 + mat[i][j];
+        queue.add(new Pair(i + 1, j));
+      }
+      if(j - 1 >= 0 && 1 + mat[i][j] < mat[i][j - 1]) {
+        mat[i][j - 1] = 1 + mat[i][j];
+        queue.add(new Pair(i, j - 1));
+      }
+      if(j + 1 < mat[0].length && 1 + mat[i][j] < mat[i][j + 1]) {
+        mat[i][j + 1] = 1 + mat[i][j];
+        queue.add(new Pair(i, j + 1));
       }
     }
     return mat;
-  }
-  static int getMin(int[][] mat, int i , int j) {
-    int min = Integer.MAX_VALUE;
-    if(i - 1 >= 0) min = Math.min(min, mat[i - 1][j]);
-    if(j - 1 >= 0) min = Math.min(min, mat[i][j - 1]);
-    if(i + 1 < mat.length) min = Math.min(min, mat[i + 1][j]);
-    if(j + 1 < mat[0].length) min = Math.min(min, mat[i][j + 1]);
-    return min;
   }
 }
