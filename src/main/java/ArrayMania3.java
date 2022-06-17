4de6b49308830caa601ad3c69fc5bdbe54080a8f
mthakur <manish.thakur@agoda.com>
2022-06-17 12:05:46 +0700

    conatins duplicate

--- src/main/java/ArrayMania3.java
+++ src/main/java/ArrayMania3.java
@@ -101,7 +101,7 @@ public class ArrayMania3 {
   static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
     for(int i = 0; i < nums.length; i++) {
       for(int j = i + 1; j <= i + k && j < nums.length; j++) {
-        if(Math.abs(nums[i] - nums[j]) <= t) return true;
+        if(nums[i] - nums[j] <= t) return true;
       }
     }
     return false;
