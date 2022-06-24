542a5a55aff937a0985bc605fdac3d48bc34c899
mthakur <manish.thakur@agoda.com>
2022-06-24 17:49:06 +0700

    cleane soluition

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -132,7 +132,7 @@ public class BinarySearchTest {
       expected = new int[]{1, 3};
       assertArrayEquals(expected, actual);
 
-      actual = BinarySearch.searchRange(new int[]{2, 2, 2, 4, 4, 9}, 4);
+      actual = BinarySearch.searchRange(new int[]{2, 2, 2, 4, 4, 9}, 9);
       expected = new int[]{5, 5};
       assertArrayEquals(expected, actual);
     }
