66b1ae87147a8d298acc0a526a32c76a8aeebf0c
mthakur <manish.thakur@agoda.com>
2022-07-04 15:33:53 +0700

    sliding window improved

--- src/main/java/SlidingWindowMax.java
+++ src/main/java/SlidingWindowMax.java
@@ -4,25 +4,21 @@ public class SlidingWindowMax {
   static public int[] maxSlidingWindow(int[] nums, int k) {
     int resultLen = nums.length - (k - 1);
     int[] result = new int[resultLen];
-    int r = 0, l = 0, h;
-    Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-    Queue<Integer> removalQueue = new PriorityQueue<>(Collections.reverseOrder());
-
-    for(h = 0; h < k; h++) queue.add(nums[h]);
-
-    result[r] = queue.peek();
-    while(h < nums.length) {
-      if(nums[l] == queue.peek()) queue.remove();
-      else removalQueue.add(nums[l]);
-      while(!removalQueue.isEmpty() && queue.peek().equals(removalQueue.peek())) {
-        queue.remove();
-        removalQueue.remove();
+    int i, r = 0;
+    Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+    Queue<Integer> rq = new PriorityQueue<>(Comparator.reverseOrder());
+    for(i = 0; i < k; i++) pq.add(nums[i]);
+    while(r < result.length) {
+      int top = pq.peek();
+      result[r] = top;
+      rq.add(nums[i - k]);
+      if(i < nums.length) pq.add(nums[i]);
+      while(!rq.isEmpty() && pq.peek().intValue() == rq.peek().intValue()) {
+        pq.remove();
+        rq.remove();
       }
-      l++;
-      queue.add(nums[h]);
-      h++;
+      i++;
       r++;
-      result[r] = queue.peek();
     }
     return result;
   }
