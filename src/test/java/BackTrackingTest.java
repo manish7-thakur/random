0cd4a970b8fcff8194e0811f9e737e3c80d43ed5
mthakur <manish.thakur@agoda.com>
2022-07-05 14:57:30 +0700

    parenthesis update until length 3

--- src/test/java/BackTrackingTest.java
+++ src/test/java/BackTrackingTest.java
@@ -7,5 +7,13 @@ public class BackTrackingTest {
     List<String> actual = BackTracking.generateParenthesis(1);
     List<String> expected = List.of("()");
     Assert.assertEquals(expected, actual);
+
+    actual = BackTracking.generateParenthesis(2);
+    expected = List.of("(())", "()()");
+    Assert.assertEquals(expected, actual);
+
+    actual = BackTracking.generateParenthesis(3);
+    expected = List.of("((()))", "(()())", "(())()", "()(())", "()()()");
+    Assert.assertEquals(expected, actual);
   }
 }
