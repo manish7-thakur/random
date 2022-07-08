14564430c2e804d1bbadb9969ee46fed82aa058f
mthakur <manish.thakur@agoda.com>
2022-07-08 14:10:49 +0700

    BST iterator with bigger tree

--- src/test/java/BSTIteratorTest.java
+++ src/test/java/BSTIteratorTest.java
@@ -14,5 +14,23 @@ public class BSTIteratorTest {
     Assert.assertTrue(iter.hasNext());
     Assert.assertEquals(8, iter.next());
     Assert.assertEquals(9, iter.next());
+
+    iter = new BSTIterator(new TreeNode(7, new TreeNode(2, new TreeNode(1), new TreeNode(5, new TreeNode(3), new TreeNode(6))), new TreeNode(9, new TreeNode(8), new TreeNode(10))));
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(1, iter.next());
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(2, iter.next());
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(3, iter.next());
+    Assert.assertEquals(5, iter.next());
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(6, iter.next());
+    Assert.assertEquals(7, iter.next());
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(8, iter.next());
+    Assert.assertTrue(iter.hasNext());
+    Assert.assertEquals(9, iter.next());
+    Assert.assertEquals(10, iter.next());
+    Assert.assertFalse(iter.hasNext());
   }
 }
