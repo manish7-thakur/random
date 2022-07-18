8483843ea4cde23bf9b4f1c505d7e8af0bd96e49
mthakur <manish.thakur@agoda.com>
2022-07-18 13:52:58 +0700

    NAry node

---
+++ src/main/java/NaryNode.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class NaryNode {
+    public int val;
+    public List<NaryNode> children;
+
+    public NaryNode() {}
+
+    public NaryNode(int _val) {
+        val = _val;
+    }
+
+    public NaryNode(int _val, List<NaryNode> _children) {
+        val = _val;
+        children = _children;
+    }
+}
