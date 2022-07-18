42bbd509ec9a41b978b2dab436f8e017fdbe63db
mthakur <manish.thakur@agoda.com>
2022-07-18 11:34:18 +0700

    simplify condition

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -202,8 +202,6 @@ public class BinaryTree {
       if(p == null && q == null) return true;
       if(p == null || q == null) return false;
       if(p.val != q.val) return false;
-      if(!isSameTree(p.left, q.left)) return false;
-      if(!isSameTree(p.right, q.right)) return false;
-      return true;
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
