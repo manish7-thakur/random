ae4e320fa38129a2df8e46e027faef0383eff9d2
mthakur <manish.thakur@agoda.com>
2022-07-19 13:15:04 +0700

    merge conditions

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -220,8 +220,7 @@ public class BinaryTree {
 
     static boolean isSymmetricRec(TreeNode node1, TreeNode node2) {
       if(node1 == null && node2 == null) return true;
-      if(node1 == null || node2 == null) return false;
-      if(node1.val != node2.val) return false;
+      if(node1 == null || node2 == null || node1.val != node2.val) return false;
       return isSymmetricRec(node1.left, node2.right) && isSymmetricRec(node1.right, node2.left);
     }
 }
