b5fbcf1172ea8552e5b2b89ce21a90ffd5f36f06
mthakur <manish.thakur@agoda.com>
2022-09-08 17:48:48 +0700

    math divide test

--- src/main/java/Maths.java
+++ src/main/java/Maths.java
@@ -1,13 +1,16 @@
 public class Maths {
   static int divide(int dividend, int divisor) {
+    if(divisor == Integer.MIN_VALUE && dividend != Integer.MIN_VALUE) return 0; 
     int count = 0;
-    boolean negative = dividend < 0 || divisor < 0;
-    dividend = Math.abs(dividend);
-    divisor = Math.abs(divisor);
+    boolean positive = dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0;
+    if(dividend == Integer.MIN_VALUE) dividend = Integer.MAX_VALUE;
+    else dividend = Math.abs(dividend);
+    if(divisor == Integer.MIN_VALUE) divisor = Integer.MAX_VALUE;
+    else divisor = Math.abs(divisor);
     while(dividend >= divisor) {
       dividend -= divisor;
       count++;
     }
-    return negative ? -count : count;
+    return positive ? count : -count;
   }
 }
--- src/test/java/MathsTest.java
+++ src/test/java/MathsTest.java
@@ -14,5 +14,37 @@ public class MathsTest {
     actual = Maths.divide(7, -3);
     expected = -2;
     Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(-17, -3);
+    expected = 5;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MAX_VALUE, 1);
+    expected = Integer.MAX_VALUE;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MAX_VALUE, -Integer.MAX_VALUE);
+    expected = -1;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MIN_VALUE, -Integer.MAX_VALUE);
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    expected = -1;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MIN_VALUE, Integer.MIN_VALUE);
+    expected = 1;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MAX_VALUE, Integer.MIN_VALUE);
+    expected = 0;
+    Assert.assertEquals(expected, actual);
+
+    actual = Maths.divide(Integer.MIN_VALUE, -Integer.MIN_VALUE);
+    expected = 1;
+    Assert.assertEquals(expected, actual);
   }
 }
