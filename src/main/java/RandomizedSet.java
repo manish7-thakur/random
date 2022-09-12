9300da86e1420532ca96833dc37e4fce6268e74d
mthakur <manish.thakur@agoda.com>
2022-09-12 13:55:06 +0700

    remove from last

--- src/main/java/RandomizedSet.java
+++ src/main/java/RandomizedSet.java
@@ -1,18 +1,39 @@
 import java.util.*;
 
 public class RandomizedSet {
-  Map<Integer, Object> map;
-  private static final Object PRESENT = new Object();
+  Map<Integer, Integer> map;
+  List<Integer> list;
 
   public RandomizedSet() {
     map = new HashMap<>();
+    list = new ArrayList<>();
   }
 
   public boolean insert(int val) {
-    return map.put(val, PRESENT) == null;
+    if(!map.containsKey(val)) {
+      list.add(val);
+      map.put(val, list.size() - 1);
+      return true;
+    }
+    return false;
   }
 
   public boolean remove(int val) {
-    return map.remove(val) == PRESENT;
+    Integer index = map.remove(val);
+    if(index != null) {
+      removeFromList(index);
+      return true;
+    }
+    return false;
+  }
+
+  private void removeFromList(int index) {
+    list.set(index, list.get(list.size() - 1));
+    list.remove(list.size() - 1);
+  }
+
+  public Integer getRandom() {
+    Random r = new Random();
+    return list.get(r.nextInt(list.size()));
   }
 }
--- src/test/java/RandomizedSetTest.java
+++ src/test/java/RandomizedSetTest.java
@@ -9,5 +9,15 @@ public class RandomizedSetTest {
 
     actual = set.remove(7);
     Assert.assertFalse(actual);
+
+    actual = set.remove(6);
+    Assert.assertTrue(actual);
+
+    set.insert(8);
+    set.insert(9);
+    Assert.assertNotNull(set.getRandom());
+
+    set.remove(8);
+    Assert.assertEquals(9, set.getRandom().intValue());
   }
 }
