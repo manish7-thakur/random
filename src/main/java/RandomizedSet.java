72fafc6f2a4cbc9dc80ac683efce46eb1fc19566
mthakur <manish.thakur@agoda.com>
2022-09-12 13:19:12 +0700

    randomized set using hash map

---
+++ src/main/java/RandomizedSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class RandomizedSet {
+  Map<Integer, Object> map;
+  private static final Object PRESENT = new Object();
+  public RandomizedSet() {
+    map = new HashMap<>();
+  }
+  public boolean insert(int val) {
+    return map.put(val, PRESENT) == null;
+  }
+}
---
+++ src/test/java/RandomizedSetTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+
+public class RandomizedSetTest {
+  @Test
+  public void radomizedSetWithConstantTimeOps() {
+    RandomizedSet set = new RandomizedSet();
+    boolean actual = set.insert(6);
+    Assert.assertTrue(actual);
+  }
+}
