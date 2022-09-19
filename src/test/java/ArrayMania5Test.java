b1af8686001d77e37bc77fd2d76be3bc8f3449e1
mthakur <manish.thakur@agoda.com>
2022-09-19 13:20:59 +0700

    file name with no common data

--- src/test/java/ArrayMania5Test.java
+++ src/test/java/ArrayMania5Test.java
@@ -19,5 +19,9 @@ public class ArrayMania5Test {
     actual = ArrayMania5.findDuplicate(new String[] {"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"});
     expected = List.of(List.of("root/a/1.txt","root/c/3.txt"), List.of("root/a/2.txt","root/c/d/4.txt","root/4.txt"));
     Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania5.findDuplicate(new String[] {"root/a 1.txt(abced) 2.txt(efgoh)","root/c 3.txt(abcd)","root/c/d 4.txt(efigh)","root 4.txt(efgh)"});
+    expected = List.of();
+    Assert.assertEquals(expected, actual);
   }
 }
