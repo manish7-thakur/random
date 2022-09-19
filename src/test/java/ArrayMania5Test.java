f671bacd51e4f3979b0c6e02265b5d96d45e4602
mthakur <manish.thakur@agoda.com>
2022-09-19 13:19:47 +0700

    file name with multiple conetent

--- src/test/java/ArrayMania5Test.java
+++ src/test/java/ArrayMania5Test.java
@@ -15,5 +15,9 @@ public class ArrayMania5Test {
     actual = ArrayMania5.findDuplicate(new String[] {"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)"});
     expected = List.of(List.of("root/a/1.txt","root/c/3.txt"), List.of("root/a/2.txt", "root/c/d/4.txt"));
     Assert.assertEquals(expected, actual);
+
+    actual = ArrayMania5.findDuplicate(new String[] {"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"});
+    expected = List.of(List.of("root/a/1.txt","root/c/3.txt"), List.of("root/a/2.txt","root/c/d/4.txt","root/4.txt"));
+    Assert.assertEquals(expected, actual);
   }
 }
