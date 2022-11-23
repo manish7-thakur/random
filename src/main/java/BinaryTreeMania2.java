27b04155c2e386e0cb4bfe2043dcdc3951ebe2f8
mthakur <manish.thakur@agoda.com>
2022-11-23 13:12:35 +0700

    check for leaves and values as well

--- src/main/java/BinaryTreeMania2.java
+++ src/main/java/BinaryTreeMania2.java
@@ -1,5 +1,14 @@
 public class BinaryTreeMania2 {
   static boolean hasPathSum(TreeNode root, int targetSum) {
+    int currSum = 0;
+    if(root == null) return false;
+    return hasPathSumRec(root, currSum, targetSum);
+  }
+  static boolean hasPathSumRec(TreeNode root, int currSum, int targetSum) {
+    if(root == null) return false;
+    if(root.left == null && root.right == null && currSum + root.val == targetSum) return true;
+    if(hasPathSumRec(root.left, currSum + root.val, targetSum)) return true;
+    if(hasPathSumRec(root.right, currSum + root.val, targetSum)) return true;
     return false;
   }
 }
--- src/test/java/BinaryTreeMania2Test.java
+++ src/test/java/BinaryTreeMania2Test.java
@@ -5,5 +5,14 @@ public class BinaryTreeMania2Test {
   public void pathSumExists() {
     boolean actual = BinaryTreeMania2.hasPathSum(null, 0);
     Assert.assertFalse(actual);
+
+    actual = BinaryTreeMania2.hasPathSum(new TreeNode(1), 1);
+    Assert.assertTrue(actual);
+
+    actual = BinaryTreeMania2.hasPathSum(new TreeNode(1, null, new TreeNode(-2)), -1);
+    Assert.assertTrue(actual);
+
+    actual = BinaryTreeMania2.hasPathSum(new TreeNode(1, null, new TreeNode(1, new TreeNode(2))), 2);
+    Assert.assertFalse(actual);
   }
 }
