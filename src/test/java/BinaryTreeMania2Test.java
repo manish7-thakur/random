a9ffa7d7c85ccad4ed59cd37cf7479ab11833b00
mthakur <manish.thakur@agoda.com>
2022-11-23 13:16:16 +0700

    zigzag test case

--- src/test/java/BinaryTreeMania2Test.java
+++ src/test/java/BinaryTreeMania2Test.java
@@ -14,5 +14,8 @@ public class BinaryTreeMania2Test {
 
     actual = BinaryTreeMania2.hasPathSum(new TreeNode(1, null, new TreeNode(1, new TreeNode(2))), 2);
     Assert.assertFalse(actual);
+
+    actual = BinaryTreeMania2.hasPathSum(new TreeNode(1, new TreeNode(7, new TreeNode(-4), new TreeNode(-5, new TreeNode(3)))), 6);
+    Assert.assertTrue(actual);
   }
 }
