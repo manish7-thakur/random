05e9cea58899da5c1160926938f5a19eaf60dcc6
mthakur <manish.thakur@agoda.com>
2022-12-20 14:55:25 +0700

    binary tree path with special logic

--- src/main/java/BinaryTreeMania2.java
+++ src/main/java/BinaryTreeMania2.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTreeMania2 {
   static boolean hasPathSum(TreeNode root, int targetSum) {
     int currSum = 0;
@@ -10,5 +12,21 @@ public class BinaryTreeMania2 {
     if(hasPathSumRec(root.left, currSum + root.val, targetSum)) return true;
     if(hasPathSumRec(root.right, currSum + root.val, targetSum)) return true;
     return false;
+  }
+
+  static List<String> binaryTreePaths(TreeNode root) {
+    List<String> res = new ArrayList<>();
+    binaryTreePathsRec(root, "", res);
+    return res;
+  }
+
+  static void binaryTreePathsRec(TreeNode root, String currPath, List<String> res) {
+    if(root == null) return;
+    if(root.left == null && root.right == null) {
+      res.add(currPath + root.val);
+      return;
+    }
+    binaryTreePathsRec(root.left, currPath + root.val + "->", res);
+    binaryTreePathsRec(root.right, currPath + root.val + "->", res);
   }
 }
--- src/test/java/BinaryTreeMania2Test.java
+++ src/test/java/BinaryTreeMania2Test.java
@@ -1,4 +1,5 @@
 import org.junit.*;
+import java.util.*;
 
 public class BinaryTreeMania2Test {
   @Test
@@ -17,5 +18,19 @@ public class BinaryTreeMania2Test {
 
     actual = BinaryTreeMania2.hasPathSum(new TreeNode(1, new TreeNode(7, new TreeNode(-4), new TreeNode(-5, new TreeNode(3)))), 6);
     Assert.assertTrue(actual);
+  }
+  @Test
+  public void binaryTreePathsFromRootToLeaves() {
+    List<String> actual = BinaryTreeMania2.binaryTreePaths(new TreeNode(3));
+    List<String> expected = List.of("3");
+    Assert.assertEquals(expected, actual);
+
+    actual = BinaryTreeMania2.binaryTreePaths(new TreeNode(3, new TreeNode(4)));
+    expected = List.of("3->4");
+    Assert.assertEquals(expected, actual);
+
+    actual = BinaryTreeMania2.binaryTreePaths(new TreeNode(3, new TreeNode(4, new TreeNode(2), new TreeNode(1))));
+    expected = List.of("3->4->2", "3->4->1");
+    Assert.assertEquals(expected, actual);
   }
 }
