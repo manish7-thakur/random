e9a5e4de484be723882bb71765ecd52b94250de8
mthakur <manish.thakur@agoda.com>
2023-02-01 14:40:46 +0700

    new soln

--- src/main/java/StringMania2.java
+++ src/main/java/StringMania2.java
@@ -105,7 +105,7 @@ public class StringMania2 {
     Map<Character, Integer> map1 = new HashMap<>();
     Map<Character, Integer> map2 = new HashMap<>();
     for(int i = 0; i < s.length(); i++) {
-      if(map1.put(s.charAt(i), i) != map2.put(t.charAt(i), i))
+      if(!Objects.equals(map1.put(s.charAt(i), i), map2.put(t.charAt(i), i)))
       return false;
     }
     return true;
